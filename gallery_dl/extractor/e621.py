1c4df37d6bf357b93869ff884c71cad808f080cb
Mike Fährmann <mike_faehrmann@web.de>
2015-04-11 00:39:26 +0200

    [e621] update to new extractor interface

--- gallery_dl/extractor/e621.py
+++ gallery_dl/extractor/e621.py
@@ -1,8 +1,28 @@
-from .danbooru import JSONBooruExtractor
+# -*- coding: utf-8 -*-
 
-class Extractor(JSONBooruExtractor):
+# Copyright 2014, 2015 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract image-urls from https://e621.net/"""
+
+from .booru import JSONBooruExtractor
+
+info = {
+    "category": "e621",
+    "extractor": "E621Extractor",
+    "directory": ["{category}", "{tags}"],
+    "filename": "{category}_{name}",
+    "pattern": [
+        r"(?:https?://)?(?:www\.)?e621\.net/post/index/\d+/([^?]+)",
+        r"(?:https?://)?(?:www\.)?e621\.net/post\?tags=([^&]+).*"
+    ],
+}
+
+class E621Extractor(JSONBooruExtractor):
 
     def __init__(self, match, config):
-        JSONBooruExtractor.__init__(self, match, config)
-        self.category = "e621"
+        JSONBooruExtractor.__init__(self, match, config, info)
         self.api_url  = "https://e621.net/post/index.json"
