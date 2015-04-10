598aa0dccdd3efaa570d09d6525374777685c1fe
Mike Fährmann <mike_faehrmann@web.de>
2015-04-11 00:17:43 +0200

    [gelbooru] update to new extractor interface

--- gallery_dl/extractor/gelbooru.py
+++ gallery_dl/extractor/gelbooru.py
@@ -1,10 +1,29 @@
-from .danbooru import XMLBooruExtractor
+# -*- coding: utf-8 -*-
 
-class Extractor(XMLBooruExtractor):
+# Copyright 2014, 2015 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract image-urls from http://gelbooru.com/"""
+
+from .booru import XMLBooruExtractor
+
+info = {
+    "category": "gelbooru",
+    "extractor": "GelbooruExtractor",
+    "directory": ["{category}", "{tags}"],
+    "filename": "{category}_{name}",
+    "pattern": [
+        r"(?:https?://)?(?:www\.)?gelbooru\.com/(?:index\.php)?\?page=post&s=list&tags=([^&]+).*",
+    ],
+}
+
+class GelbooruExtractor(XMLBooruExtractor):
 
     def __init__(self, match, config):
-        XMLBooruExtractor.__init__(self, match, config)
-        self.category = "gelbooru"
+        XMLBooruExtractor.__init__(self, match, config, info)
         self.api_url  = "http://gelbooru.com/"
         self.params   = {"page":"dapi", "s":"post", "q":"index", "tags":self.tags}
 
