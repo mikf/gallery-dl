6a9b3a24afc5d603646a2fd06b4adcf89cb366b4
Mike Fährmann <mike_faehrmann@web.de>
2015-04-10 21:45:41 +0200

    docstrings and other small fixes for downloaders

--- gallery_dl/downloader/text.py
+++ gallery_dl/downloader/text.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2014, 2015 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Downloader module for text urls"""
+
 from .common import BasicDownloader
 
 class Downloader(BasicDownloader):
