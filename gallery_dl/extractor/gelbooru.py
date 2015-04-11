3c6161eea76a19506f6d993500821b36f037bad6
Mike Fährmann <mike_faehrmann@web.de>
2015-04-11 16:22:15 +0200

    small whitespace fixes

--- gallery_dl/extractor/gelbooru.py
+++ gallery_dl/extractor/gelbooru.py
@@ -24,11 +24,11 @@ class GelbooruExtractor(XMLBooruExtractor):
 
     def __init__(self, match, config):
         XMLBooruExtractor.__init__(self, match, config, info)
-        self.api_url  = "http://gelbooru.com/"
-        self.params   = {"page":"dapi", "s":"post", "q":"index", "tags":self.tags}
+        self.api_url = "http://gelbooru.com/"
+        self.params = {"page":"dapi", "s":"post", "q":"index", "tags":self.tags}
 
     def update_page(self, reset=False):
         if reset is False:
             self.params["pid"] += 1
         else:
-            self.params["pid"]  = 0
+            self.params["pid"] = 0
