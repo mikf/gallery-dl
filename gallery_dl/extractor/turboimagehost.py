2cb6f4300c326dfa35c4e8e56679328aed2f63f7
Mike Fährmann <mike_faehrmann@web.de>
2015-12-03 00:25:15 +0100

    [turboimagehost] add extractor

---
+++ gallery_dl/extractor/turboimagehost.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images from http://www.turboimagehost.com"""
+
+from .common import Extractor, Message
+from .. import text
+
+class TurboimagehostExtractor(Extractor):
+
+    category = "turboimagehost"
+    directory_fmt = ["{category}"]
+    filename_fmt = "{category}_{index}_{filename}"
+    pattern = [r"(?:https?://)?(?:www\.)?turboimagehost\.com/p/((\d+)/[^/]+\.html)"]
+
+    def __init__(self, match):
+        Extractor.__init__(self)
+        self.part, self.index = match.groups()
+
+    def items(self):
+        page = self.request("http://www.turboimagehost.com/p/" + self.part).text
+        data = {
+            "category": self.category,
+            "index": self.index,
+        }
+        text.extract_all(page, (
+            ('width' , 'var imWidth = ', ';'),
+            ('height', 'var imHeight = ', ';'),
+            ('url'   , '<a href="http://www.turboimagehost.com"><img src="', '"'),
+        ), values=data)
+        text.nameext_from_url(data["url"], data)
+        yield Message.Version, 1
+        yield Message.Directory, data
+        yield Message.Url, data["url"], data
