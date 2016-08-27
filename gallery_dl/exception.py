5b1adb72c8028bbca1b27a1cbdc98fd40fb80b00
Mike Fährmann <mike_faehrmann@web.de>
2016-08-27 17:39:45 +0200

    new exception + docstrings

--- gallery_dl/exception.py
+++ gallery_dl/exception.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Mike Fährmann
+# Copyright 2015, 2016 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
 class NoExtractorError(Exception):
-    pass
+    """No extractor can handle the given URL"""
 
 class AuthenticationError(Exception):
-    pass
+    """Invalid or missing login information"""
+
+class NotFoundError(Exception):
+    """Requested resource (gallery/image) does not exist"""
