76da3f7cf6b0f36b32c29d272f30d72dac4e9d79
Mike Fährmann <mike_faehrmann@web.de>
2016-09-08 07:56:52 +0200

    [imgcandy] add extractor

--- gallery_dl/extractor/__init__.py
+++ gallery_dl/extractor/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Mike Fährmann
+# Copyright 2015,2016 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -33,6 +33,7 @@ modules = [
     "imagefap",
     "imagetwist",
     "imgbox",
+    "imgcandy",
     "imgchili",
     "imgth",
     "imgur",
---
+++ gallery_dl/extractor/imgcandy.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2016 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images from http://imgcandy.net/"""
+
+from .common import Extractor, Message
+from .. import text
+
+class ImgcandyImageExtractor(Extractor):
+    """Extractor for single images from imgcandy.net"""
+    category = "imgcandy"
+    directory_fmt = ["{category}"]
+    filename_fmt = "{filename}"
+    pattern = [(r"(?:https?://)?(?:www\.)?imgcandy\.net/img-([a-z0-9]+)"
+                r"(?:_(.+))?\.html")]
+    test = [("http://imgcandy.net/img-57d02527efee8_test-テスト.png.html", {
+        "url": "bc3c9207b10dbfe8e65ccef5b9e3194a7427b4fa",
+        "keyword": "381e036374742a091cac7dd7a3eca90ee725afa3",
+        "content": "0c8768055e4e20e7c7259608b67799171b691140",
+    })]
+
+    def __init__(self, match):
+        Extractor.__init__(self)
+        self.token, self.filename = match.groups()
+
+    def items(self):
+        data = {"category": self.category, "token": self.token}
+        params = {"imgContinue": "Continue+to+image+...+"}
+        page = self.request("http://imgcandy.net/img-" + self.token + ".html",
+                            method="post", data=params).text
+        url = text.extract(page, "<img class='centred' src='", "'")[0]
+        text.nameext_from_url(self.filename or url, data)
+        yield Message.Version, 1
+        yield Message.Directory, data
+        yield Message.Url, url, data
