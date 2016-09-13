26a9b7beeeab1f707b1395e39d8cc3fc4a5b4360
Mike Fährmann <mike_faehrmann@web.de>
2016-09-13 08:01:00 +0200

    [imagetwist] small fixes + tests

--- gallery_dl/extractor/imagetwist.py
+++ gallery_dl/extractor/imagetwist.py
@@ -18,6 +18,11 @@ class ImagetwistImageExtractor(Extractor):
     directory_fmt = ["{category}"]
     filename_fmt = "{category}_{user}_{filename}"
     pattern = [r"(?:https?://)?(?:www\.)?imagetwist\.com/([a-z0-9]{12})"]
+    test = [("http://imagetwist.com/4e46hv31tu0q/test.jpg", {
+        "url": "6b3fc0bd1105b698d2d5844658ca674d66b1e2e7",
+        "keyword": "d599a540ed233bb7b66e4abec30affbad2e44af1",
+        "content": "96b1fd099b06faad5879fce23a7e4eb8290d8810",
+    })]
 
     def __init__(self, match):
         Extractor.__init__(self)
@@ -25,7 +30,7 @@ class ImagetwistImageExtractor(Extractor):
 
     def items(self):
         page = self.request("http://imagetwist.com/" + self.token).text
-        url     , pos = text.extract(page, '<img src="', '"')
+        url     , pos = text.extract(page, 'center;"><img src="', '"')
         filename, pos = text.extract(page, ' alt="', '"', pos)
         userid  , pos = text.extract(url , '/', '/', 29)
         data = {
