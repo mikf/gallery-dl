828aedd5712f0a2dfa4d6235407490dfa0ef1e8d
Mike Fährmann <mike_faehrmann@web.de>
2016-10-25 15:26:32 +0200

    [khinsider] unescape soundtrack title

--- gallery_dl/extractor/khinsider.py
+++ gallery_dl/extractor/khinsider.py
@@ -39,13 +39,15 @@ class KhinsiderSoundtrackExtractor(AsynchronousExtractor):
 
     def get_job_metadata(self, page):
         """Collect metadata for extractor-job"""
-        return text.extract_all(page, (
+        data = text.extract_all(page, (
             ("album", "Album name: <b>", "</b>"),
             ("count", "Number of Files: <b>", "</b>"),
             ("size" , "Total Filesize: <b>", "</b>"),
             ("date" , "Date added: <b>", "</b>"),
             ("type" , "Album type: <b>", "</b>"),
         ))[0]
+        data["album"] = text.unescape(data["album"])
+        return data
 
     def get_album_tracks(self, page):
         """Collect url and metadata for all tracks of a soundtrack"""
