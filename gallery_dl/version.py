4cb49cfd767e83be25f22d75e2c1e92723c0a377
Mike Fährmann <mike_faehrmann@web.de>
2017-02-13 00:55:01 +0100

    bump version to 0.6.4

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2016, 2017 Mike Fährmann
+# Copyright 2016-2017 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
