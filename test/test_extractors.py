7b86e157e31adc81b2ebb7e190b58ffae346b526
Mike Fährmann <mike_faehrmann@web.de>
2017-05-03 15:17:08 +0200

    add '--abort-on-skip' option and ability to control skip behavior
    the 'skip' config option controls skipping behavior:
        true    - skip download if file already exist (default)
        false   - download and overwrite files even if it exists
        "abort" - abort extractor run if a download would be skipped
                  (same as '--abort-on-skip')

--- test/test_extractors.py
+++ test/test_extractors.py
@@ -51,7 +51,7 @@ skip = [
     # dont work on travis-ci
     "exhentai", "kissmanga", "mangafox", "dynastyscans",
     # temporary issues
-    "fallenangels",
+
 ]
 # enable selective testing for direct calls
 if __name__ == '__main__' and len(sys.argv) > 1:
