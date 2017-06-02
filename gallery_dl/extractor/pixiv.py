e78952a1525346a0eb117a827cfad386794bf3ba
aiasdfd <aiasdfdlgkd@gmail.com>
2017-06-02 13:28:55 -0600

    [pixiv] fix the fix

--- gallery_dl/extractor/pixiv.py
+++ gallery_dl/extractor/pixiv.py
@@ -36,7 +36,7 @@ class PixivUserExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self)
         self.artist_id = match.group(1)
-        if (len(match.groups) > 2):
+        if (len(match.groups()) > 2):
             self.tag = match.group(2)
         else:
             self.tag = None
