561f256f7965f966142357dc4bb13c925ffbcf92
Mike Fährmann <mike_faehrmann@web.de>
2017-07-14 08:44:31 +0200

    [2chan] add thread extractor

---
+++ gallery_dl/extractor/2chan.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images from https://www.2chan.net/"""
+
+from .common import Extractor, Message
+from .. import text
+
+
+class FutabaThreadExtractor(Extractor):
+    """Extractor for images from threads on www.2chan.net"""
+    category = "2chan"
+    subcategory = "thread"
+    directory_fmt = ["{category}", "{board-name}", "{thread}"]
+    pattern = [r"(?:https?://)?(([^.]+)\.2chan\.net/([^/]+)/res/(\d+))"]
+    urlfmt = "https://{server}.2chan.net/{board}/src/{filename}"
+    test = [("http://dec.2chan.net/70/res/947.htm", {
+        "url": "c5c12b80b290e224b6758507b3bb952044f4595b",
+        "keyword": "e1295c0a96f733898e92742bcc1a4c4b320e3748",
+    })]
+
+    def __init__(self, match):
+        Extractor.__init__(self)
+        url, self.server, self.board, self.thread = match.groups()
+        self.url = "https://" + url + ".htm"
+
+    def items(self):
+        page = self.request(self.url).text
+        data = self.get_metadata(page)
+        yield Message.Version, 1
+        yield Message.Directory, data
+        for post in self.posts(page):
+            if "filename" not in post:
+                continue
+            post.update(data)
+            url = self.urlfmt.format_map(post)
+            yield Message.Url, url, post
+
+    def get_metadata(self, page):
+        """Collect metadata for extractor-job"""
+        title = text.extract(page, "<title>", "</title>")[0]
+        title, _, boardname = title.rpartition(" - ")
+        return {
+            "server": self.server,
+            "title": title,
+            "board": self.board,
+            "board-name": boardname[:-4],
+            "thread": self.thread,
+        }
+
+    def posts(self, page):
+        """Build a list of all post-objects"""
+        page = text.extract(
+            page, '<div class="thre">', '<div style="clear:left"></div>')[0]
+        return [
+            self.parse(post)
+            for post in page.split('<table border=0>')
+        ]
+
+    def parse(self, post):
+        """Build post-object by extracting data from an HTML post"""
+        data = self._extract_post(post)
+        if '<a href="/' in post:
+            self._extract_image(post, data)
+            data["tim"], _, data["extension"] = data["filename"].partition(".")
+            data["time"] = data["tim"][:-3]
+            data["ext"] = "." + data["extension"]
+        return data
+
+    @staticmethod
+    def _extract_post(post):
+        return text.extract_all(post, (
+            ("no"      , 'name="', '"'),
+            ("post"    , '<b>', '</b>'),
+            ("name"    , '<b>', ' </b>'),
+            ("now"     , '</font> ', ' '),
+            (None      , '<blockquote', ''),
+            ("com"     , '>', '</blockquote>'),
+        ))[0]
+
+    @staticmethod
+    def _extract_image(post, data):
+        text.extract_all(post, (
+            ("filename", '_blank">', '<'),
+            ("fsize"   , '(', ' '),
+        ), 0, data)
