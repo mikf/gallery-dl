390eeded4c0ceb645396cf4adb7c6d0f0ccef899
Mike Fährmann <mike_faehrmann@web.de>
2017-07-21 16:25:56 +0200

    [mangazuki] support 'raws.…' subdomain

--- gallery_dl/extractor/mangazuki.py
+++ gallery_dl/extractor/mangazuki.py
@@ -9,7 +9,7 @@
 """Extract manga-chapters and entire manga from https://mangazuki.co/"""
 
 from .common import Extractor, MangaExtractor, Message
-from .. import text, exception
+from .. import text, util, exception
 
 
 class MangazukiChapterExtractor(Extractor):
@@ -18,11 +18,15 @@ class MangazukiChapterExtractor(Extractor):
     subcategory = "chapter"
     directory_fmt = ["{category}", "{manga}", "c{chapter:>03}"]
     filename_fmt = ("{manga}_c{chapter:>03}_{page:>03}.{extension}")
-    pattern = [r"(?:https?://)?(?:www\.)?(mangazuki\.co/read/[^/]+/\d+)"]
+    pattern = [r"(?:https?://)?((raws\.)?mangazuki\.co/read/[^/]+/\d+)"]
     test = [
         ("https://mangazuki.co/read/Double-Casting/59", {
             "url": "47b4102da3ee1bd28459848904e8b7ff48e1ac5e",
             "keyword": "b4cac50b3a3a1dd06451d1917a0faae2c6a51b95",
+        }),
+        ("https://raws.mangazuki.co/read/Rakujitsu-no-Pathos/25", {
+            "url": "c1c95d9954185f53a8449f497239aee181bea4af",
+            "keyword": "7554ca641f10e84fa5594e5ff8bd3925a5ff2e80",
         }),
         ("https://mangazuki.co/read/Double-Casting/999", {
             "exception": exception.NotFoundError,
@@ -32,6 +36,7 @@ class MangazukiChapterExtractor(Extractor):
     def __init__(self, match):
         Extractor.__init__(self)
         self.url = "https://" + match.group(1)
+        self.lang = "" if match.group(2) else "en"
 
     def items(self):
         response = self.request(self.url)
@@ -49,10 +54,15 @@ class MangazukiChapterExtractor(Extractor):
 
     def get_metadata(self, page):
         """Collect metadata for extractor-job"""
+        data = {
+            "lang": self.lang,
+            "language": util.code_to_language(self.lang, self.lang),
+        }
         return text.extract_all(page, (
-            ("manga"  , "<title>Mangazuki - ", " - Chapter "),
+            (None     , "<title>Mangazuki", ""),
+            ("manga"  , " - ", " - Chapter "),
             ("chapter", "", "</title>"),
-        ), values={"lang": "en", "language": "English"})[0]
+        ), values=data)[0]
 
     @staticmethod
     def get_images(page):
@@ -63,11 +73,16 @@ class MangazukiChapterExtractor(Extractor):
 class MangazukiMangaExtractor(MangaExtractor):
     """Extractor for manga from mangazuki.co"""
     category = "mangazuki"
-    pattern = [r"(?:https?://)?(?:www\.)?(mangazuki\.co/series/[^/?&#]+)"]
+    pattern = [r"(?:https?://)?((?:raws\.)?mangazuki\.co/series/[^/?&#]+)"]
     scheme = "https"
-    test = [("https://mangazuki.co/series/Double-Casting", {
-        "url": "aab747414191b14e768f4a1eb148448d83ef2e14",
-    })]
+    test = [
+        ("https://mangazuki.co/series/Double-Casting", {
+            "url": "aab747414191b14e768f4a1eb148448d83ef2e14",
+        }),
+        ("https://raws.mangazuki.co/series/Rakujitsu-no-Pathos", {
+            "url": "4c5fcee4ad306faa3cfe952f7474293a99d11787",
+        }),
+    ]
 
     def chapters(self, page):
         params = {"page": 1}
