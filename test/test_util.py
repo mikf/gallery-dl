d74a635e4150441211903ae505332974846cc7b1
Mike Fährmann <mike_faehrmann@web.de>
2017-08-08 19:22:04 +0200

    [util] update 'default' values and improve test coverage
    for 'code_to_language()' and 'language_to_code()'

--- test/test_util.py
+++ test/test_util.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Mike Fährmann
+# Copyright 2015-2017 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -14,16 +14,38 @@ import gallery_dl.util as util
 class TestISO639_1(unittest.TestCase):
 
     def test_code_to_language(self):
-        self.assertEqual(util.code_to_language("en"), "English")
-        self.assertEqual(util.code_to_language("FR"), "French")
-        self.assertEqual(util.code_to_language("xx"), "English")
-        self.assertEqual(util.code_to_language("xx", default=None), None)
+        d = "default"
+        self._run_test(util.code_to_language, {
+            ("en",): "English",
+            ("FR",): "French",
+            ("xx",): None,
+            (""  ,): None,
+            (None,): None,
+            ("en", d): "English",
+            ("FR", d): "French",
+            ("xx", d): d,
+            (""  , d): d,
+            (None, d): d,
+        })
 
     def test_language_to_code(self):
-        self.assertEqual(util.language_to_code("English"), "en")
-        self.assertEqual(util.language_to_code("fRENch"), "fr")
-        self.assertEqual(util.language_to_code("xx"), "en")
-        self.assertEqual(util.language_to_code("xx", default=None), None)
+        d = "default"
+        self._run_test(util.language_to_code, {
+            ("English",): "en",
+            ("fRENch",): "fr",
+            ("xx",): None,
+            (""  ,): None,
+            (None,): None,
+            ("English", d): "en",
+            ("fRENch", d): "fr",
+            ("xx", d): d,
+            (""  , d): d,
+            (None, d): d,
+        })
+
+    def _run_test(self, func, tests):
+        for args, result in tests.items():
+            self.assertEqual(func(*args), result)
 
 
 if __name__ == '__main__':
