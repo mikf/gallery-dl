d70c66c5160691ae20b456bfb0e57f3c83f3a553
Mike Fährmann <mike_faehrmann@web.de>
2017-08-16 12:11:47 +0200

    fix "text:" downloader

--- test/test_extractors.py
+++ test/test_extractors.py
@@ -58,6 +58,7 @@ skip = [
     "exhentai", "kissmanga", "mangafox", "dynastyscans", "nijie",
     "archivedmoe", "archiveofsins", "thebarchive",
     # temporary issues
+    "imgtrex",  # 504
 ]
 # enable selective testing for direct calls
 if __name__ == '__main__' and len(sys.argv) > 1:
