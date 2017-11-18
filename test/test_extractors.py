b8cdd42cabbadf1562ede9476be49a37904e6ced
Mike Fährmann <mike_faehrmann@web.de>
2017-11-18 17:21:35 +0100

    [senmanga] fix extraction (again)
    this is basically a re-revert of 2ace5c7

--- test/test_extractors.py
+++ test/test_extractors.py
@@ -83,8 +83,6 @@ skip = [
     "archivedmoe", "archiveofsins", "thebarchive",
     # temporary issues
     "mangazuki",
-    "hentaifoundry",  # invalid SSL cert
-    "desuarchive", "rbt",  # server migration
 ]
 # enable selective testing for direct calls
 if __name__ == '__main__' and len(sys.argv) > 1:
