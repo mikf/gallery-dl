55c64cad4b83a3e9426fe1db97e6d68fa68b16bb
Mike Fährmann <mike_faehrmann@web.de>
2017-11-28 19:35:47 +0100

    [khinsider] fix filename extension and test-pattern

--- gallery_dl/extractor/rebeccablacktech.py
+++ gallery_dl/extractor/rebeccablacktech.py
@@ -19,9 +19,9 @@ class RebeccablacktechThreadExtractor(chan.FoolfuukaThreadExtractor):
                r"|rbt\.asia)/([^/]+)/thread/(\d+)"]
     test = [
         ("https://rbt.asia/g/thread/61487650/", {
-            "url": "484f20ea9b9b58f6abb0cd37eaeab22431ac22c2",
+            "url": "fadd274b25150a1bdf03a40c58db320fa3b617c4",
         }),
         ("https://archive.rebeccablacktech.com/g/thread/61487650/", {
-            "url": "484f20ea9b9b58f6abb0cd37eaeab22431ac22c2",
+            "url": "fadd274b25150a1bdf03a40c58db320fa3b617c4",
         }),
     ]
