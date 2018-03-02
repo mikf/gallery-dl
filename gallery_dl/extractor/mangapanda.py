6e38cf5aabfd32eb57224deeefbafc3f332c0e40
Mike Fährmann <mike_faehrmann@web.de>
2018-03-02 17:16:24 +0100

    [mangareader] use 'https://'
    The site now redirects from http://mangareader.net/
    to https://mangareader.net/

--- gallery_dl/extractor/mangapanda.py
+++ gallery_dl/extractor/mangapanda.py
@@ -6,7 +6,7 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-"""Extract manga-chapters and entire manga from http://www.mangapanda.com/"""
+"""Extract manga-chapters and entire manga from https://www.mangapanda.com/"""
 
 from .mangareader import MangareaderMangaExtractor, MangareaderChapterExtractor
 
@@ -14,14 +14,14 @@ from .mangareader import MangareaderMangaExtractor, MangareaderChapterExtractor
 class MangapandaBase():
     """Base class for mangapanda extractors"""
     category = "mangapanda"
-    root = "http://www.mangapanda.com"
+    root = "https://www.mangapanda.com"
 
 
 class MangapandaMangaExtractor(MangapandaBase, MangareaderMangaExtractor):
     """Extractor for manga from mangapanda.com"""
     pattern = [r"(?:https?://)?((?:www\.)?mangapanda\.com/[^/?&#]+)/?$"]
-    test = [("http://www.mangapanda.com/mushishi", {
-        "url": "50a1ba730b85426b904da256c80f68ba6a8a2566",
+    test = [("https://www.mangapanda.com/mushishi", {
+        "url": "357f965732371cac1990fee8b480f62e29141a42",
         "keyword": "031b3ea085921c552de017ecbb9b906e462229c9",
     })]
 
@@ -33,7 +33,7 @@ class MangapandaChapterExtractor(MangapandaBase, MangareaderChapterExtractor):
         (r"(?:https?://)?(?:www\.)?mangapanda\.com"
          r"(/\d+-\d+-\d+(/[^/]+)/chapter-(\d+)\.html)"),
     ]
-    test = [("http://www.mangapanda.com/red-storm/2", {
-        "url": "4bf4ddf6c50105ec8a37675495ab80c46608275d",
+    test = [("https://www.mangapanda.com/red-storm/2", {
+        "url": "1f633f776e950531ba9b1e81965316458e785261",
         "keyword": "32b5e84017c2bf5f122b339ecf40899e41f18cc9",
     })]
