ddb2579f784688ee6321f7e1f18378b3ce263af9
Mike Fährmann <mike_faehrmann@web.de>
2018-05-29 10:30:41 +0200

    [pixiv] fix filter for 'type=all'

--- gallery_dl/extractor/pixiv.py
+++ gallery_dl/extractor/pixiv.py
@@ -127,7 +127,8 @@ class PixivUserExtractor(PixivExtractor):
             qdict = text.parse_query(self.query)
             if "type" in qdict:
                 type_ = qdict["type"].lower()
-                works = filter(self._is_type(type_), works)
+                if type_ != "all":
+                    works = filter(self._is_type(type_), works)
             if "tag" in qdict:
                 tag = text.unquote(qdict["tag"]).lower()
                 works = filter(self._has_tag(tag), works)
