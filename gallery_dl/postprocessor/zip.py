97189e50cd96b8242dacf556fe9b9fd8f13e4cc1
Mike Fährmann <mike_faehrmann@web.de>
2018-06-06 20:49:52 +0200

    [pp:zip] use temppath; add options

--- gallery_dl/postprocessor/zip.py
+++ gallery_dl/postprocessor/zip.py
@@ -6,7 +6,7 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-"""Add files to ZIP archive"""
+"""Add files to ZIP archives"""
 
 from .common import PostProcessor
 import zipfile
@@ -14,12 +14,26 @@ import zipfile
 
 class ZipPP(PostProcessor):
 
+    COMPRESSION_ALGORITHMS = {
+        "store": zipfile.ZIP_STORED,
+        "zip": zipfile.ZIP_DEFLATED,
+        "bzip2": zipfile.ZIP_BZIP2,
+        "lzma": zipfile.ZIP_LZMA,
+    }
+
     def __init__(self, options):
         PostProcessor.__init__(self)
+        self.ext = "." + options.get("extension", "zip")
+
+        algorithm = options.get("compression", "store")
+        if algorithm not in self.COMPRESSION_ALGORITHMS:
+            algorithm = "store"
+        self.compression = self.COMPRESSION_ALGORITHMS[algorithm]
 
     def run(self, pathfmt):
-        with zipfile.ZipFile(pathfmt.realdirectory + ".zip", "a") as zfile:
-            zfile.write(pathfmt.realpath, pathfmt.filename)
+        archive = pathfmt.realdirectory + self.ext
+        with zipfile.ZipFile(archive, "a", self.compression, True) as zfile:
+            zfile.write(pathfmt.temppath, pathfmt.filename)
 
 
 __postprocessor__ = ZipPP
