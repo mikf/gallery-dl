9e188f6a213c834a00e852c374e986eff5c664ee
Mike Fährmann <mike_faehrmann@web.de>
2018-11-21 17:40:38 +0100

    [4chan] support 4channel.org domain

--- gallery_dl/extractor/4chan.py
+++ gallery_dl/extractor/4chan.py
@@ -15,12 +15,19 @@ from .. import text
 class FourchanThreadExtractor(chan.ChanThreadExtractor):
     """Extractor for images from threads from 4chan.org"""
     category = "4chan"
-    pattern = [r"(?:https?://)?boards\.4chan\.org/([^/]+)/thread/(\d+)"]
-    test = [("https://boards.4chan.org/tg/thread/15396072/", {
-        "url": "39082ad166161966d7ba8e37f2173a824eb540f0",
-        "keyword": "7ae2f4049adf0d2f835eb91b6b26b7f4ec882e0a",
-        "content": "20b7b51afa51c9c31a0020a0737b889532c8d7ec",
-    })]
+    pattern = [r"(?:https?://)?boards\.4chan(?:nel)?\.org"
+               r"/([^/]+)/thread/(\d+)"]
+    test = [
+        ("https://boards.4chan.org/tg/thread/15396072/", {
+            "url": "39082ad166161966d7ba8e37f2173a824eb540f0",
+            "keyword": "7ae2f4049adf0d2f835eb91b6b26b7f4ec882e0a",
+            "content": "20b7b51afa51c9c31a0020a0737b889532c8d7ec",
+        }),
+        ("https://boards.4channel.org/tg/thread/15396072/", {
+            "url": "39082ad166161966d7ba8e37f2173a824eb540f0",
+            "keyword": "7ae2f4049adf0d2f835eb91b6b26b7f4ec882e0a",
+        }),
+    ]
     api_url = "https://a.4cdn.org/{board}/thread/{thread}.json"
     file_url = "https://i.4cdn.org/{board}/{tim}{ext}"
 
