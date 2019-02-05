687cd1527722b5ddbe6a44fa549ee018f83e2dc0
Mike Fährmann <mike_faehrmann@web.de>
2019-02-05 10:22:52 +0100

    [hentaicafe] restore functionality

--- gallery_dl/extractor/hentaicafe.py
+++ gallery_dl/extractor/hentaicafe.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018 Mike Fährmann
+# Copyright 2018-2019 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -17,11 +17,13 @@ class HentaicafeChapterExtractor(foolslide.FoolslideChapterExtractor):
     """Extractor for manga-chapters from hentai.cafe"""
     category = "hentaicafe"
     directory_fmt = ["{category}", "{manga}"]
-    pattern = foolslide.chapter_pattern(r"(?:www\.)?hentai\.cafe/manga")
+    pattern = [r"(?:https?://)?(?:www\.)?hentai\.cafe"
+               r"(/manga/read/[^/?&#]+/[a-z-]+/\d+/\d+(?:/\d+)?)"]
     test = [("https://hentai.cafe/manga/read/saitom-box/en/0/1/", {
         "url": "8c6a8c56875ba3ed7ab0a74a64f9960077767fc2",
         "keyword": "1b24a3e8625b89d160d01ce3eb5e5eb12fbbf648",
     })]
+    root = "https://hentai.cafe"
 
     def get_metadata(self, page):
         info = text.unescape(text.extract(page, '<title>', '</title>')[0])
@@ -35,8 +37,8 @@ class HentaicafeChapterExtractor(foolslide.FoolslideChapterExtractor):
 class HentaicafeMangaExtractor(foolslide.FoolslideMangaExtractor):
     """Extractor for manga from hentai.cafe"""
     category = "hentaicafe"
-    pattern = [r"(?:https?://)?((?:www\.)?hentai\.cafe"
-               r"(?:/manga/series)?/[^/?&#]+/?$)"]
+    pattern = [r"(?:https?://)?" + r"(?:www\.)?hentai\.cafe"
+               r"((?:/manga/series)?/[^/?&#]+)/?$"]
     test = [
         # single chapter
         ("https://hentai.cafe/hazuki-yuuto-summer-blues/", {
@@ -52,6 +54,7 @@ class HentaicafeMangaExtractor(foolslide.FoolslideMangaExtractor):
             "keyword": "46012b857eb1a1394bc55c0efe7aa4e7f704d10d",
         }),
     ]
+    root = "https://hentai.cafe"
     reverse = False
 
     def chapters(self, page):
