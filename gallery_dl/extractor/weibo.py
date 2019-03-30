17c11393f5865e08c76554d9afa8df4e1ab88450
Mike Fährmann <mike_faehrmann@web.de>
2019-03-30 18:38:58 +0100

    [weibo] allow user-ids in status URLs

--- gallery_dl/extractor/weibo.py
+++ gallery_dl/extractor/weibo.py
@@ -113,7 +113,7 @@ class WeiboStatusExtractor(WeiboExtractor):
     """Extractor for images from a status on weibo.cn"""
     subcategory = "status"
     pattern = (r"(?:https?://)?(?:www\.|m\.)?weibo\.c(?:om|n)"
-               r"/(?:detail|status)/(\d+)")
+               r"/(?:detail|status|\d+)/(\d+)")
     test = (
         ("https://m.weibo.cn/detail/4323047042991618", {
             "pattern": r"https?://wx\d+.sinaimg.cn/large/\w+.jpg",
@@ -122,6 +122,7 @@ class WeiboStatusExtractor(WeiboExtractor):
             "pattern": r"https?://f.us.sinaimg.cn/\w+\.mp4\?label=mp4_hd",
         }),
         ("https://m.weibo.cn/status/4339748116375525"),
+        ("https://m.weibo.cn/5746766133/4339748116375525"),
     )
 
     def __init__(self, match):
