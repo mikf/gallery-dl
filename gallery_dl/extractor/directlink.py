db35c3b581f6a951cb26a8b112866402659ef3f3
Mike Fährmann <mike_faehrmann@web.de>
2019-11-28 23:39:35 +0100

    [directlink] separate filenames from paths
    With this, all default filename formats specify an '{extension}'
    and PathFormat.set_extension() reliably works for all files.

--- gallery_dl/extractor/directlink.py
+++ gallery_dl/extractor/directlink.py
@@ -15,25 +15,30 @@ from .. import text
 class DirectlinkExtractor(Extractor):
     """Extractor for direct links to images and other media files"""
     category = "directlink"
-    filename_fmt = "{domain}/{path}"
-    archive_fmt = "{domain}/{path}"
+    filename_fmt = "{domain}/{path}/{filename}.{extension}"
+    archive_fmt = filename_fmt
     pattern = (r"(?i)https?://(?P<domain>[^/?&#]+)/(?P<path>[^?&#]+\."
                r"(?:jpe?g|jpe|png|gif|web[mp]|mp4|mkv|og[gmv]|opus))"
                r"(?:\?(?P<query>[^/?#]*))?(?:#(?P<fragment>.*))?$")
     test = (
         (("https://en.wikipedia.org/static/images/project-logos/enwiki.png"), {
             "url": "18c5d00077332e98e53be9fed2ee4be66154b88d",
-            "keyword": "e81b9fe3022e971365dd859f38e4ef717a6c69ed",
+            "keyword": "105770a3f4393618ab7b811b731b22663b5d3794",
+        }),
+        # empty path
+        (("https://example.org/file.webm"), {
+            "url": "2d807ed7059d1b532f1bb71dc24b510b80ff943f",
+            "keyword": "29dad729c40fb09349f83edafa498dba1297464a",
         }),
         # more complex example
-        ("https://example.org/path/file.webm?que=1&ry=2#fragment", {
-            "url": "fd4aec8a32842343394e6078a06c3e6b647bf671",
-            "keyword": "ff75764b1ae66615b723a6357b8193fa2de84678",
+        ("https://example.org/path/to/file.webm?que=1&ry=2#fragment", {
+            "url": "114b8f1415cc224b0f26488ccd4c2e7ce9136622",
+            "keyword": "06014abd503e3b2b58aa286f9bdcefdd2ae336c0",
         }),
         # percent-encoded characters
         ("https://example.org/%27%3C%23/%23%3E%27.jpg?key=%3C%26%3E", {
             "url": "2627e8140727fdf743f86fe18f69f99a052c9718",
-            "keyword": "4d19dc12e41ffcb4cbec2013e335cf482377c35e",
+            "keyword": "831790fddda081bdddd14f96985ab02dc5b5341f",
         }),
         # upper case file extension (#296)
         ("https://post-phinf.pstatic.net/MjAxOTA1MjlfMTQ4/MDAxNTU5MTI2NjcyNTkw"
@@ -46,11 +51,14 @@ class DirectlinkExtractor(Extractor):
         self.data = match.groupdict()
 
     def items(self):
-        text.nameext_from_url(self.url, self.data)
-        for key, value in self.data.items():
+        data = self.data
+        for key, value in data.items():
             if value:
-                self.data[key] = text.unquote(value)
+                data[key] = text.unquote(value)
+        data["path"], _, name = data["path"].rpartition("/")
+        data["filename"], _, ext = name.rpartition(".")
+        data["extension"] = ext.lower()
 
         yield Message.Version, 1
-        yield Message.Directory, self.data
-        yield Message.Url, self.url, self.data
+        yield Message.Directory, data
+        yield Message.Url, self.url, data
