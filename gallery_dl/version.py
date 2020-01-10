b9cbf932b426f82d5e84c9d3ca5b391c4090c3bc
Mike Fährmann <mike_faehrmann@web.de>
2020-01-10 13:31:06 +0100

    [pixiv] update URL patterns (fixes #568)
    Pixiv now uses new URLs for
    - user profiles and illustration listings:
      - https://www.pixiv.net/en/users/<ID>
      - https://www.pixiv.net/en/users/<ID>/artworks
    - bookmarks:
      - https://www.pixiv.net/en/users/<ID>/bookmarks/artworks

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2016-2019 Mike Fährmann
+# Copyright 2016-2020 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.12.2"
+__version__ = "1.12.3-dev"
