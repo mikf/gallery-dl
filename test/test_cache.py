b6388baaa89bd7a9e6e2aade132fe984880fb6ea
Mike Fährmann <mike_faehrmann@web.de>
2021-10-13 03:10:55 +0200

    run tests without using 'nose'
    run_tests.sh -> run_tests.py

--- test/test_cache.py
+++ test/test_cache.py
@@ -20,6 +20,7 @@ from gallery_dl import config, util  # noqa E402
 dbpath = tempfile.mkstemp()[1]
 config.set(("cache",), "file", dbpath)
 from gallery_dl import cache  # noqa E402
+cache._init()
 
 
 #  def tearDownModule():
