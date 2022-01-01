mikf/gallery-dl

commit 3be656bed592c27e99e375937ded0ba50acdec55

use Extractor._check_cookies() for all cookie checks

gallery_dl/extractor/patreon.py
@@ -29,7 +29,7 @@ class PatreonExtractor(Extractor):
     def items(self):
 
         if self._warning:
-            if "session_id" not in self.session.cookies:
+            if not self._check_cookies(("session_id",)):
                 self.log.warning("no 'session_id' cookie set")
             PatreonExtractor._warning = False
         generators = self._build_file_generators(self.config("files"))

gallery_dl/extractor/fanbox.py
@@ -33,7 +33,7 @@ class FanboxExtractor(Extractor):
     def items(self):
 
         if self._warning:
-            if "FANBOXSESSID" not in self.session.cookies:
+            if not self._check_cookies(("FANBOXSESSID",)):
                 self.log.warning("no 'FANBOXSESSID' cookie set")
             FanboxExtractor._warning = False
 

gallery_dl/extractor/fantia.py
@@ -22,7 +22,7 @@ class FantiaExtractor(Extractor):
     def items(self):
 
         if self._warning:
-            if "_session_id" not in self.session.cookies:
+            if not self._check_cookies(("_session_id",)):
                 self.log.warning("no '_session_id' cookie set")
             FantiaExtractor._warning = False
 

commit c9a36ba6c41deec7bc247cb97b77d4a69051cd99

fix cookie checks for patreon, fanbox, fantia
The changes in 3be656be caused a warning about missing cookies to be
displayed even if those cookies were present, because _check_cookies()
did not account for an empty cookiedomain.

gallery_dl/extractor/common.py
@@ -336,7 +336,8 @@ class Extractor():
         now = time.time()
 
         for cookie in self._cookiejar:
-            if cookie.name in names and cookie.domain == domain:
+            if cookie.name in names and (
+                    not domain or cookie.domain == domain):
                 if cookie.expires and cookie.expires < now:
                     self.log.warning("Cookie '%s' has expired", cookie.name)
                 else:

gallery_dl/extractor/patreon.py
@@ -20,6 +20,7 @@ class PatreonExtractor(Extractor):
     """Base class for patreon extractors"""
     category = "patreon"
     root = "https://www.patreon.com"
+    cookiedomain = ".patreon.com"
     directory_fmt = ("{category}", "{creator[full_name]}")
     filename_fmt = "{id}_{title}_{num:>02}.{extension}"
     archive_fmt = "{id}_{num}"