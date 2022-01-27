mikf/gallery-dl

commit 19096cc6460fb95352782c4649857dc7791649d8

[gelbooru] handle changed API response format (#2157)

gallery_dl/extractor/gelbooru.py
@@ -10,7 +10,7 @@
 
 from .common import Extractor, Message
 from . import gelbooru_v02
-from .. import text, exception
+from .. import text, util, exception
 import binascii
 
 
@@ -19,6 +19,42 @@ class GelbooruBase():
     category = "gelbooru"
     basecategory = "booru"
     root = "https://gelbooru.com"
+
+    def _api_request(self, params):
+        url = self.root + "/index.php?page=dapi&s=post&q=index&json=1"
+        data = self.request(url, params=params).json()
+        if "post" not in data:
+            return ()
+        posts = data["post"]
+        if not isinstance(posts, list):
+            return (posts,)
+        return posts
+
+    def _pagination(self, params):
+        params["pid"] = self.page_start
+        params["limit"] = self.per_page
+
+        post = None
+        while True:
+            try:
+                posts = self._api_request(params)
+            except ValueError:
+                if "tags" not in params or post is None:
+                    raise
+                taglist = [tag for tag in params["tags"].split()
+                           if not tag.startswith("id:<")]
+                taglist.append("id:<" + str(post.attrib["id"]))
+                params["tags"] = " ".join(taglist)
+                params["pid"] = 0
+                continue
+
+            post = None
+            for post in posts:
+                yield post
+
+            if len(posts) < self.per_page:
+                return
+            params["pid"] += 1
 
     @staticmethod
     def _file_url(post):
@@ -81,6 +117,11 @@ class GelbooruPoolExtractor(GelbooruBase,
             "pool": text.parse_int(self.pool_id),
             "pool_name": text.unescape(name),
         }
+
+    def posts(self):
+        params = {}
+        for params["id"] in util.advance(self.post_ids, self.page_start):
+            yield from self._api_request(params)
 
 
 class GelbooruPostExtractor(GelbooruBase,

commit 5cc1115f4cf781b352783f7474fd8b7233afeb3f

[gelbooru] improve and fix pagination (#2230, #2232)
Use 'id:<POSTID' as a tag instead of going through pages with 'pid'.

Something similar was already implemented in 0fd85908,
but that got broken again in 19096cc6.

gallery_dl/extractor/gelbooru.py
@@ -33,28 +33,20 @@ class GelbooruBase():
     def _pagination(self, params):
         params["pid"] = self.page_start
         params["limit"] = self.per_page
+        limit = self.per_page // 2
 
-        post = None
         while True:
-            try:
-                posts = self._api_request(params)
-            except ValueError:
-                if "tags" not in params or post is None:
-                    raise
-                taglist = [tag for tag in params["tags"].split()
-                           if not tag.startswith("id:<")]
-                taglist.append("id:<" + str(post.attrib["id"]))
-                params["tags"] = " ".join(taglist)
-                params["pid"] = 0
-                continue
+            posts = self._api_request(params)
 
-            post = None
             for post in posts:
                 yield post
 
-            if len(posts) < self.per_page:
+            if len(posts) < limit:
                 return
-            params["pid"] += 1
+
+            if "pid" in params:
+                del params["pid"]
+            params["tags"] = "{} id:<{}".format(self.tags, post["id"])
 
     @staticmethod
     def _file_url(post):
@@ -81,9 +73,12 @@ class GelbooruTagExtractor(GelbooruBase,
         ("https://gelbooru.com/index.php?page=post&s=list&tags=bonocho", {
             "count": 5,
         }),
-        ("https://gelbooru.com/index.php?page=post&s=list&tags=bonocho", {
-            "options": (("api", False),),
-            "count": 5,
+        ("https://gelbooru.com/index.php?page=post&s=list&tags=meiya_neon", {
+            "range": "196-204",
+            "url": "845a61aa1f90fb4ced841e8b7e62098be2e967bf",
+            "pattern": r"https://img\d\.gelbooru\.com"
+                       r"/images/../../[0-9a-f]{32}\.jpg",
+            "count": 9,
         }),
     )
 

gallery_dl/version.py
@@ -6,4 +6,4 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.20.3"
+__version__ = "1.20.4-dev"