dffefbc3546c1b0801e0bb720958e9ff839c37da
v-delta <v-delta@posteo.de>
2022-02-09 13:42:24 +0100

    Wrap line

--- gallery_dl/extractor/imgbox.py
+++ gallery_dl/extractor/imgbox.py
@@ -54,7 +54,8 @@ class ImgboxExtractor(Extractor):
     def get_image_url(page):
         """Extract download-url"""
         pos = page.index(">Image</a>")
-        return 'https://images'+text.extract(page, '<a href="https://images', '?', pos)[0]
+        return 'https://images'+\
+               text.extract(page, '<a href="https://images', '?', pos)[0]
 
 
 class ImgboxGalleryExtractor(AsynchronousMixin, ImgboxExtractor):
