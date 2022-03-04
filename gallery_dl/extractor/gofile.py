f9da2b11bf2fe1940776553b9040f6d4fbb09d94
loragja <32179844+loragja@users.noreply.github.com>
2022-03-04 21:07:33 +0100

    add support for tiny monitors and ancient python versions

--- gallery_dl/extractor/gofile.py
+++ gallery_dl/extractor/gofile.py
@@ -15,23 +15,27 @@ class GofileFolderExtractor(Extractor):
         self.content_id = match.group(1)
 
     def _create_account(self):
-        response = self.request("https://api.gofile.io/createAccount", method="GET")
+        response = self.request(
+            "https://api.gofile.io/createAccount", method="GET")
         json_response = response.json()
         if json_response["status"] != "ok":
-            raise Exception(f"Error creating account! Status: {json_response['status']}")
+            raise Exception("Error creating account! Status: {}"
+                            .format(json_response['status']))
         return json_response["data"]["token"]
 
     def _get_content(self, token, content_id):
-        response = self.request(f"https://api.gofile.io/getContent?contentId={content_id}"
-                                f"&token={token}"
-                                "&websiteToken=websiteToken", method="GET")
+        response = self.request(
+            f"https://api.gofile.io/getContent?contentId={content_id}"
+            f"&token={token}"
+            "&websiteToken=websiteToken",
+            method="GET")
         json_response = response.json()
         if json_response["status"] != "ok":
-            raise Exception(f"Error getting content! Status: {json_response['status']}")
+            raise Exception("Error getting content! Status: {}"
+                            .format(json_response['status']))
         return json_response
 
     def items(self):
-
         token = self._create_account()
         self._update_cookies({"accountToken": token})
         content = self._get_content(token, self.content_id)
@@ -42,7 +46,8 @@ class GofileFolderExtractor(Extractor):
         for file in content["data"]["contents"].values():
             name = file["name"]
             if file["type"] != "file":
-                self.log.warning(f"Warning! Content {file['name']} is not a file!")
+                self.log.warning(
+                    "Warning! Content {} is not a file!".format(file['name']))
                 continue
             link = file["link"]
             data["filename"], _, data["extension"] = name.rpartition(".")
