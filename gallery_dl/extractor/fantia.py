b75309537e77cec73eb7f0241f010c8b87f0239e
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2022-03-07 22:43:52 +0300

    Fixed line to fit PEP8 line size limit

--- gallery_dl/extractor/fantia.py
+++ gallery_dl/extractor/fantia.py
@@ -29,8 +29,9 @@ class FantiaExtractor(Extractor):
         for post_id in self.posts():
             full_response, post = self._get_post_data(post_id)
             yield Message.Directory, post
-            for count, (url, url_data) in enumerate(self._get_urls_from_post(full_response, post), start=1):
-                post["num"] = count
+            post["num"] = 0
+            for url, url_data in self._get_urls_from_post(full_response, post):
+                post["num"] += 1
                 fname = url_data["content_filename"] or url
                 text.nameext_from_url(fname, url_data)
                 url_data["file_url"] = url
