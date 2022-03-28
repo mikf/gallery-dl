8117fa602c4f4ae463f9d19ac68766e16b8ab668
Mike Fährmann <mike_faehrmann@web.de>
2022-03-28 19:08:00 +0200

    [telegraph] update and improve
    - use GalleryExtractor capabilities
    - extract more metadata
    - add tests

--- gallery_dl/extractor/telegraph.py
+++ gallery_dl/extractor/telegraph.py
@@ -6,9 +6,7 @@
 
 """Extractor for https://telegra.ph/"""
 
-from urllib.parse import urlparse
-
-from .common import GalleryExtractor, Message
+from .common import GalleryExtractor
 from .. import text
 
 
@@ -17,39 +15,81 @@ class TelegraphGalleryExtractor(GalleryExtractor):
 
     category = "telegraph"
     root = "https://telegra.ph"
-    directory_fmt = ("{category}", "{id}")
-    filename_fmt = "{number_formatted}_{filename}.{extension}"
-    pattern = r"((?:https?://)(?:www\.)??telegra\.ph/.+)"
-
-    def __init__(self, match):
-        super().__init__(match, match.group(1))
+    directory_fmt = ("{category}", "{slug}")
+    filename_fmt = "{num_formatted}_{filename}.{extension}"
+    archive_fmt = "{slug}_{num}"
+    pattern = r"(?:https?://)(?:www\.)??telegra\.ph(/[^/?#]+)"
+    test = (
+        ("https://telegra.ph/Telegraph-Test-03-28", {
+            "pattern": r"https://telegra\.ph/file/[0-9a-f]+\.png",
+            "keyword": {
+                "author": "mikf",
+                "caption": r"re:test|",
+                "count": 2,
+                "date": "dt:2022-03-28 16:01:36",
+                "description": "Just a test",
+                "post_url": "https://telegra.ph/Telegraph-Test-03-28",
+                "slug": "Telegraph-Test-03-28",
+                "title": "Telegra.ph Test",
+            },
+        }),
+        ("https://telegra.ph/森-03-28", {
+            "pattern": "https://telegra.ph/file/3ea79d23b0dd0889f215a.jpg",
+            "count": 1,
+            "keyword": {
+                "author": "&",
+                "caption": "kokiri",
+                "count": 1,
+                "date": "dt:2022-03-28 16:31:26",
+                "description": "コキリの森",
+                "extension": "jpg",
+                "filename": "3ea79d23b0dd0889f215a",
+                "num": 1,
+                "num_formatted": "1",
+                "post_url": "https://telegra.ph/森-03-28",
+                "slug": "森-03-28",
+                "title": '"森"',
+                "url": "https://telegra.ph/file/3ea79d23b0dd0889f215a.jpg",
+            },
+        }),
+    )
 
     def metadata(self, page):
-        return {
-            "title": text.extract(page, "<h1>", "</h1>")[0],
-            "id": urlparse(self.url).path.split("/")[-1],
+        extr = text.extract_from(page)
+        data = {
+            "title": text.unescape(extr(
+                'property="og:title" content="', '"')),
+            "description": text.unescape(extr(
+                'property="og:description" content="', '"')),
+            "date": text.parse_datetime(extr(
+                'property="article:published_time" content="', '"'),
+                "%Y-%m-%dT%H:%M:%S%z"),
+            "author": text.unescape(extr(
+                'property="article:author" content="', '"')),
+            "post_url": text.unescape(extr(
+                'rel="canonical" href="', '"')),
         }
+        data["slug"] = data["post_url"][19:]
+        return data
 
-    def items(self):
-        page = self.request(self.url).text
-
-        data = self.metadata(page)
-        yield Message.Directory, data
-
+    def images(self, page):
         figures = tuple(text.extract_iter(page, "<figure>", "</figure>"))
         num_zeroes = len(str(len(figures)))
+        num = 0
 
-        for i, figure in enumerate(figures):
-            image_url = text.extract(figure, '<img src="', '">')
-            caption = text.extract(figure, ">", "</figcaption>", image_url[1])
+        result = []
+        for figure in figures:
+            src, pos = text.extract(figure, 'src="', '"')
+            if src.startswith("/embed/"):
+                continue
+            caption, pos = text.extract(figure, "<figcaption>", "<", pos)
+            url = self.root + src
+            num += 1
 
-            image = {
-                "url": self.root + image_url[0],
-                "caption": caption[0],
-                "number": i + 1,
-                "number_formatted": str(i + 1).zfill(num_zeroes),
-            }
-            image.update(data)
-            image = text.nameext_from_url(image["url"], image)
-
-            yield Message.Url, image["url"], image
+            result.append((url, {
+                "url"          : url,
+                "caption"      : text.unescape(caption),
+                "num"          : num,
+                "num_formatted": str(num).zfill(num_zeroes),
+            }))
+        return result
