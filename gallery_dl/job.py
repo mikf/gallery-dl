fed46520041e1a931f1019cb854f246f18eb33d3
KennethSamael <58892009+KennethSamael@users.noreply.github.com>
2022-04-08 12:29:46 +0200

    Revert "check if file in archive before trying to build path"
    This reverts commit c33c808232a17b7421a3c9af0628b782bdb0e89b.

--- gallery_dl/job.py
+++ gallery_dl/job.py
@@ -213,18 +213,17 @@ class DownloadJob(Job):
         pathfmt = self.pathfmt
         archive = self.archive
 
-        if archive and archive.check(kwdict):
-            pathfmt._create_directory = False
-            pathfmt.fix_extension()
-            self.handle_skip()
-            return
-
         # prepare download
         pathfmt.set_filename(kwdict)
 
         if "prepare" in hooks:
             for callback in hooks["prepare"]:
                 callback(pathfmt)
+
+        if archive and archive.check(kwdict):
+            pathfmt.fix_extension()
+            self.handle_skip()
+            return
 
         if pathfmt.exists():
             if archive:
