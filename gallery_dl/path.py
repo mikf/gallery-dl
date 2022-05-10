mikf/gallery-dl

commit 99cb28724f7576aa49a3e8313bf12c8ae19c16ff

delay directory creation (#2461, #2474)
Only call os.makedirs() when an open() call
fails with a FileNotFoundError

Might cause errors with some post processors etc,
but for now I was unable to find any.

gallery_dl/path.py
@@ -74,7 +74,7 @@ class PathFormat():
         self.directory = self.realdirectory = \
             self.filename = self.extension = self.prefix = \
             self.path = self.realpath = self.temppath = ""
-        self.delete = self._create_directory = False
+        self.delete = False
 
         extension_map = config("extension-map")
         if extension_map is None:
@@ -138,7 +138,11 @@ class PathFormat():
 
     def open(self, mode="wb"):
         """Open file and return a corresponding file object"""
-        return open(self.temppath, mode)
+        try:
+            return open(self.temppath, mode)
+        except FileNotFoundError:
+            os.makedirs(self.realdirectory, exist_ok=True)
+            return open(self.temppath, mode)
 
     def exists(self):
         """Return True if the file exists on disk"""
@@ -187,7 +191,6 @@ class PathFormat():
                 directory += sep
 
         self.realdirectory = directory
-        self._create_directory = True
 
     def set_filename(self, kwdict):
         """Set general filename data"""
@@ -279,9 +282,6 @@ class PathFormat():
 
     def build_path(self):
         """Combine directory and filename to full paths"""
-        if self._create_directory:
-            os.makedirs(self.realdirectory, exist_ok=True)
-            self._create_directory = False
         self.filename = filename = self.build_filename(self.kwdict)
         self.path = self.directory + filename
         self.realpath = self.realdirectory + filename

commit 9c8647a4fd83eeec181aa705ed1cf9b31ea595a7

fix 'part-directory' (#2576)
bug introduced in 99cb2872

gallery_dl/path.py
@@ -317,11 +317,18 @@ class PathFormat():
 
         if self.temppath != self.realpath:
             # Move temp file to its actual location
-            try:
-                os.replace(self.temppath, self.realpath)
-            except OSError:
-                shutil.copyfile(self.temppath, self.realpath)
-                os.unlink(self.temppath)
+            while True:
+                try:
+                    os.replace(self.temppath, self.realpath)
+                except FileNotFoundError:
+                    # delayed directory creation
+                    os.makedirs(self.realdirectory, exist_ok=True)
+                    continue
+                except OSError:
+                    # move across different filesystems
+                    shutil.copyfile(self.temppath, self.realpath)
+                    os.unlink(self.temppath)
+                break
 
         mtime = self.kwdict.get("_mtime")
         if mtime: