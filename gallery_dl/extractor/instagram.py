ce8eec708a0cbead56c3527a3a5f11a50b339d12
Mike Fährmann <mike_faehrmann@web.de>
2022-08-14 17:24:48 +0200

    [instagram] provide 'date' for directories (#2830)

--- gallery_dl/extractor/instagram.py
+++ gallery_dl/extractor/instagram.py
@@ -270,6 +270,7 @@ class InstagramExtractor(Extractor):
                 "post_shortcode": post["code"],
                 "likes": post["like_count"],
                 "pinned": post.get("timeline_pinned_user_ids", ()),
+                "date": text.parse_timestamp(post.get("taken_at")),
             }
 
             caption = post["caption"]
