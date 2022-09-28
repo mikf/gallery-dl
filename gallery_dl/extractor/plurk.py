dc03f6ec28409affbadba54a79fcf77b83c44f77
Mike Fährmann <mike_faehrmann@web.de>
2022-09-28 13:04:32 +0200

    [plurk] fix extraction (#2977)

--- gallery_dl/extractor/plurk.py
+++ gallery_dl/extractor/plurk.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019-2020 Mike Fährmann
+# Copyright 2019-2022 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -85,7 +85,7 @@ class PlurkTimelineExtractor(PlurkExtractor):
     def plurks(self):
         url = "{}/{}".format(self.root, self.user)
         page = self.request(url).text
-        user_id, pos = text.extract(page, '"user_id":', ',')
+        user_id, pos = text.extract(page, '"page_user": {"id":', ',')
         plurks = self._load(text.extract(page, "_PLURKS = ", ";\n", pos)[0])
 
         headers = {"Referer": url, "X-Requested-With": "XMLHttpRequest"}
