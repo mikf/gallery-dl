mikf/gallery-dl

commit b57015cf0a24aef2ff6663c96ec8022003965249

[postprocessor:metadata] assume 'mode: custom' when format is set
{"name": "metadata", "format": "foobar"}
will now implicitly use mode:custom and no longer mode:json like before

gallery_dl/postprocessor/metadata.py
@@ -20,14 +20,8 @@ class MetadataPP(PostProcessor):
         PostProcessor.__init__(self, job)
 
         mode = options.get("mode", "json")
-        if mode == "custom":
-            self.write = self._write_custom
-            cfmt = options.get("content-format") or options.get("format")
-            if isinstance(cfmt, list):
-                cfmt = "\n".join(cfmt) + "\n"
-            self._content_fmt = formatter.parse(cfmt).format_map
-            ext = "txt"
-        elif mode == "tags":
+        cfmt = options.get("content-format") or options.get("format")
+        if mode == "tags":
             self.write = self._write_tags
             ext = "txt"
         elif mode == "modify":
@@ -41,6 +35,12 @@ class MetadataPP(PostProcessor):
             self.run = self._run_delete
             self.fields = options.get("fields")
             ext = None
+        elif mode == "custom" or cfmt:
+            self.write = self._write_custom
+            if isinstance(cfmt, list):
+                cfmt = "\n".join(cfmt) + "\n"
+            self._content_fmt = formatter.parse(cfmt).format_map
+            ext = "txt"
         else:
             self.write = self._write_json
             self.indent = options.get("indent", 4)

test/test_postprocessor.py
@@ -267,6 +267,7 @@ class MetadataTest(BasePostprocessorTest):
         test({"mode": "custom", "content-format": "{foo}\n{missing}\n"})
         test({"mode": "custom", "content-format": ["{foo}", "{missing}"]})
         test({"mode": "custom", "format": "{foo}\n{missing}\n"})
+        test({"format": "{foo}\n{missing}\n"})
 
     def test_metadata_extfmt(self):
         pp = self._create({

commit 5a0b4cfab453344428fda3791c1399ec3b06c4bd

[postprocessor:metadata] don't ignore mode:json
fixes a bug introduced in b57015cf
where {"mode": "json", "format": "foobar"}
would behave like mode:custom

gallery_dl/postprocessor/metadata.py
@@ -19,7 +19,7 @@ class MetadataPP(PostProcessor):
     def __init__(self, job, options):
         PostProcessor.__init__(self, job)
 
-        mode = options.get("mode", "json")
+        mode = options.get("mode")
         cfmt = options.get("content-format") or options.get("format")
         if mode == "tags":
             self.write = self._write_tags
@@ -35,7 +35,7 @@ class MetadataPP(PostProcessor):
             self.run = self._run_delete
             self.fields = options.get("fields")
             ext = None
-        elif mode == "custom" or cfmt:
+        elif mode == "custom" or not mode and cfmt:
             self.write = self._write_custom
             if isinstance(cfmt, list):
                 cfmt = "\n".join(cfmt) + "\n"