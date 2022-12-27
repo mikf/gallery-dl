20aa271620219c64d4ebd12b3af9aba9404f90d6
Ion Chary <121480000+ionvanishchary@users.noreply.github.com>
2022-12-27 00:41:46 -0800

    Add copyright

--- gallery_dl/extractor/webtoonxyz.py
+++ gallery_dl/extractor/webtoonxyz.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# Copyright 2022 Ion Chary
+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
