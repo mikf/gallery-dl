mikf/gallery-dl

commit a383eca7f6ec0f08dcc854d594aecbfecc4f45be

decouple extractor initialization
Introduce an 'initialize()' function that does the actual init
(session, cookies, config options) and can called separately from
the constructor __init__().

This allows, for example, to adjust config access inside a Job
before most of it already happened when calling 'extractor.find()'.

gallery_dl/extractor/exhentai.py
@@ -31,17 +31,21 @@ class ExhentaiExtractor(Extractor):
     LIMIT = False
 
     def __init__(self, match):
-        # allow calling 'self.config()' before 'Extractor.__init__()'
-        self._cfgpath = ("extractor", self.category, self.subcategory)
+        Extractor.__init__(self, match)
+        self.version = match.group(1)
 
-        version = match.group(1)
+    def initialize(self):
         domain = self.config("domain", "auto")
         if domain == "auto":
-            domain = ("ex" if version == "ex" else "e-") + "hentai.org"
+            domain = ("ex" if self.version == "ex" else "e-") + "hentai.org"
         self.root = "https://" + domain
         self.cookies_domain = "." + domain
 
-        Extractor.__init__(self, match)
+        Extractor.initialize(self)
+
+        if self.version != "ex":
+            self.cookies.set("nw", "1", domain=self.cookies_domain)
+        self.session.headers["Referer"] = self.root + "/"
         self.original = self.config("original", True)
 
         limits = self.config("limits", False)
@@ -50,10 +54,6 @@ class ExhentaiExtractor(Extractor):
             self._remaining = 0
         else:
             self.limits = False
-
-        self.session.headers["Referer"] = self.root + "/"
-        if version != "ex":
-            self.cookies.set("nw", "1", domain=self.cookies_domain)
 
     def request(self, url, **kwargs):
         response = Extractor.request(self, url, **kwargs)
@@ -174,6 +174,7 @@ class ExhentaiGalleryExtractor(ExhentaiExtractor):
         self.image_token = match.group(4)
         self.image_num = text.parse_int(match.group(6), 1)
 
+    def _init(self):
         source = self.config("source")
         if source == "hitomi":
             self.items = self._items_hitomi

commit fd6b413f3c70ce1a365f5a22aec155cd30f05375

[exhentai] fix 'domain' option (#4458)
regression from a383eca7

gallery_dl/extractor/exhentai.py
@@ -482,7 +482,6 @@ class ExhentaiSearchExtractor(ExhentaiExtractor):
 
     def __init__(self, match):
         ExhentaiExtractor.__init__(self, match)
-        self.search_url = self.root
 
         _, query, tag = match.groups()
         if tag:
@@ -496,6 +495,9 @@ class ExhentaiSearchExtractor(ExhentaiExtractor):
             self.params = text.parse_query(query)
             if "next" not in self.params:
                 self.params["page"] = text.parse_int(self.params.get("page"))
+
+    def _init(self):
+        self.search_url = self.root
 
     def items(self):
         self.login()
@@ -542,6 +544,5 @@ class ExhentaiFavoriteExtractor(ExhentaiSearchExtractor):
          "&f_apply=Search+Favorites"),
     )
 
-    def __init__(self, match):
-        ExhentaiSearchExtractor.__init__(self, match)
+    def _init(self):
         self.search_url = self.root + "/favorites.php"