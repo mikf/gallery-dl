ceb59e176f3170cd054b02b6309d4f93966dc237
Mike Fährmann <mike_faehrmann@web.de>
2023-09-02 22:11:57 +0200

    fix default Firefox user agent string
    note to self: do not trust some random third-party website

--- gallery_dl/extractor/common.py
+++ gallery_dl/extractor/common.py
@@ -299,7 +299,7 @@ class Extractor():
             useragent = self.config("user-agent")
             if useragent is None:
                 useragent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; "
-                             "rv:115.0) Gecko/20100101 Firefox/115.0")
+                             "rv:109.0) Gecko/20100101 Firefox/115.0")
             elif useragent == "browser":
                 useragent = _browser_useragent()
             headers["User-Agent"] = useragent
@@ -831,8 +831,8 @@ _browser_cookies = {}
 
 HTTP_HEADERS = {
     "firefox": (
-        ("User-Agent", "Mozilla/5.0 ({}; rv:115.0) "
-                       "Gecko/20100101 Firefox/115.0"),
+        ("User-Agent", "Mozilla/5.0 ({}; "
+                       "rv:109.0) Gecko/20100101 Firefox/115.0"),
         ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,"
                    "image/avif,image/webp,*/*;q=0.8"),
         ("Accept-Language", "en-US,en;q=0.5"),
