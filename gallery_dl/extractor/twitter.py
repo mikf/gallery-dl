mikf/gallery-dl

commit d7e97c9a570cca48491b0c0736ef436b3b7e05fa

[twitter] add 'user' extractor and 'include' option (#4275)

gallery_dl/extractor/twitter.py
@@ -461,23 +461,18 @@ class TwitterExtractor(Extractor):
                 self._update_cookies(_login_impl(self, username, password))
 
 
-class TwitterTimelineExtractor(TwitterExtractor):
-    """Extractor for a Twitter user timeline"""
-    subcategory = "timeline"
+class TwitterUserExtractor(TwitterExtractor):
+    """Extractor for a Twitter user"""
+    subcategory = "user"
     pattern = (BASE_PATTERN + r"/(?!search)(?:([^/?#]+)/?(?:$|[?#])"
                r"|i(?:/user/|ntent/user\?user_id=)(\d+))")
     test = (
         ("https://twitter.com/supernaturepics", {
-            "range": "1-40",
-            "url": "c570ac1aae38ed1463be726cc46f31cac3d82a40",
-        }),
-        # suspended account (#2216)
-        ("https://twitter.com/OptionalTypo", {
-            "exception": exception.NotFoundError,
-        }),
-        # suspended account user ID
-        ("https://twitter.com/id:772949683521978368", {
-            "exception": exception.NotFoundError,
+            "options": (("include", "all"),),
+            "pattern": r"https://twitter\.com/supernaturepics"
+                       r"/(photo|header_photo|timeline|tweets"
+                       r"|media|with_replies|likes)$",
+            "count": 7,
         }),
         ("https://mobile.twitter.com/supernaturepics?p=i"),
         ("https://www.twitter.com/id:2976459548"),
@@ -492,6 +487,40 @@ class TwitterTimelineExtractor(TwitterExtractor):
         user_id = match.group(2)
         if user_id:
             self.user = "id:" + user_id
+
+    def items(self):
+        base = "{}/{}/".format(self.root, self.user)
+        return self._dispatch_extractors((
+            (TwitterAvatarExtractor    , base + "photo"),
+            (TwitterBackgroundExtractor, base + "header_photo"),
+            (TwitterTimelineExtractor  , base + "timeline"),
+            (TwitterTweetsExtractor    , base + "tweets"),
+            (TwitterMediaExtractor     , base + "media"),
+            (TwitterRepliesExtractor   , base + "with_replies"),
+            (TwitterLikesExtractor     , base + "likes"),
+        ), ("timeline",))
+
+
+class TwitterTimelineExtractor(TwitterExtractor):
+    """Extractor for a Twitter user timeline"""
+    subcategory = "timeline"
+    pattern = BASE_PATTERN + r"/(?!search)([^/?#]+)/timeline(?!\w)"
+    test = (
+        ("https://twitter.com/supernaturepics/timeline", {
+            "range": "1-40",
+            "url": "c570ac1aae38ed1463be726cc46f31cac3d82a40",
+        }),
+        # suspended account (#2216)
+        ("https://twitter.com/OptionalTypo/timeline", {
+            "exception": exception.NotFoundError,
+        }),
+        # suspended account user ID
+        ("https://twitter.com/id:772949683521978368/timeline", {
+            "exception": exception.NotFoundError,
+        }),
+        ("https://mobile.twitter.com/supernaturepics/timeline#t"),
+        ("https://www.twitter.com/id:2976459548/timeline"),
+    )
 
     def tweets(self):
         # yield initial batch of (media) tweets

commit 68b7b2232696713e37059fa3bd7e8c4de82adca4

[twitter] fix '_extractor' of following results (#4536)
regression from d7e97c9a

docs/gallery-dl.conf
@@ -335,7 +335,7 @@
             "text-tweets": false,
             "twitpic": false,
             "unique": true,
-            "users": "timeline",
+            "users": "user",
             "videos": true
         },
         "unsplash":

gallery_dl/extractor/twitter.py
@@ -406,9 +406,12 @@ class TwitterExtractor(Extractor):
 
     def _users_result(self, users):
         userfmt = self.config("users")
-        if not userfmt or userfmt == "timeline":
-            cls = TwitterTimelineExtractor
+        if not userfmt or userfmt == "user":
+            cls = TwitterUserExtractor
             fmt = (self.root + "/i/user/{rest_id}").format_map
+        elif userfmt == "timeline":
+            cls = TwitterTimelineExtractor
+            fmt = (self.root + "/id:{rest_id}/timeline").format_map
         elif userfmt == "media":
             cls = TwitterMediaExtractor
             fmt = (self.root + "/id:{rest_id}/media").format_map