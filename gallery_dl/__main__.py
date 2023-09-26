7150c4c76c6a64a9dc7a1ebf7137826bc9b73ee9
Mike Fährmann <mike_faehrmann@web.de>
2023-09-26 22:07:50 +0200

    fix imports when using the gallery_dl directory as argument (#4581)

--- gallery_dl/__main__.py
+++ gallery_dl/__main__.py
@@ -9,7 +9,7 @@
 
 import sys
 
-if __package__ is None and not hasattr(sys, "frozen"):
+if not __package__ and not hasattr(sys, "frozen"):
     import os.path
     path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.insert(0, os.path.realpath(path))
