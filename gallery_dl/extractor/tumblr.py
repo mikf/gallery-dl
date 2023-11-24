mikf/gallery-dl

commit 8fdab9fb23fd8e744887ad769cc1fcb87f01e516

decouple extractor initialization
Introduce an 'initialize()' function that does the actual init
(session, cookies, config options) and can called separately from
the constructor __init__().

This allows, for example, to adjust config access inside a Job
before most of it already happened when calling 'extractor.find()'.

gallery_dl/extractor/tumblr.py
@@ -42,6 +42,7 @@ class TumblrExtractor(Extractor):
         else:
             self.blog = match.group(1) or match.group(3)
 
+    def _init(self):
         self.api = TumblrAPI(self)
         self.types = self._setup_posttypes()
         self.avatar = self.config("avatar", False)

commit 7b643dcdefd611b0c7d52b5e301ae611b7ebccef

[tumblr] fix 'day' extractor
another bug caused by 8fdab9fb

gallery_dl/extractor/tumblr.py
@@ -322,12 +322,15 @@ class TumblrDayExtractor(TumblrExtractor):
     def __init__(self, match):
         TumblrExtractor.__init__(self, match)
         year, month, day = match.group(4).split("/")
-        self.date_min = (
-            # 719163 == date(1970, 1, 1).toordinal()
-            date(int(year), int(month), int(day)).toordinal() - 719163) * 86400
+        self.ordinal = date(int(year), int(month), int(day)).toordinal()
 
     def _init(self):
         TumblrExtractor._init(self)
+
+        self.date_min = (
+            # 719163 == date(1970, 1, 1).toordinal()
+            (self.ordinal - 719163) * 86400)
+
         self.api.before = self.date_min + 86400
 
     def posts(self):