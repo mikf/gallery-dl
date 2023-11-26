691e864099005e2c5926f3d54677f2024da90a42
Mike Fährmann <mike_faehrmann@web.de>
2023-11-26 18:03:13 +0100

    [cyberdrop] update to site layout changes

--- gallery_dl/extractor/cyberdrop.py
+++ gallery_dl/extractor/cyberdrop.py
@@ -7,6 +7,7 @@
 """Extractors for https://cyberdrop.me/"""
 
 from . import lolisafe
+from .common import Message
 from .. import text
 
 
@@ -16,24 +17,43 @@ class CyberdropAlbumExtractor(lolisafe.LolisafeAlbumExtractor):
     pattern = r"(?:https?://)?(?:www\.)?cyberdrop\.(?:me|to)/a/([^/?#]+)"
     example = "https://cyberdrop.me/a/ID"
 
+    def items(self):
+        files, data = self.fetch_album(self.album_id)
+
+        yield Message.Directory, data
+        for data["num"], file in enumerate(files, 1):
+            file.update(data)
+            text.nameext_from_url(file["name"], file)
+            file["name"], sep, file["id"] = file["filename"].rpartition("-")
+            yield Message.Url, file["url"], file
+
     def fetch_album(self, album_id):
-        url = self.root + "/a/" + self.album_id
-        extr = text.extract_from(self.request(url).text)
+        url = "{}/a/{}".format(self.root, album_id)
+        page = self.request(url).text
+        extr = text.extract_from(page)
 
-        files = []
-        append = files.append
-        while True:
-            url = text.unescape(extr('id="file" href="', '"'))
-            if not url:
-                break
-            append({"file": url,
-                    "_fallback": (self.root + url[url.find("/", 8):],)})
+        desc = extr('property="og:description" content="', '"')
+        if desc.startswith("A privacy-focused censorship-resistant file "
+                           "sharing platform free for everyone."):
+            desc = ""
+        extr('id="title"', "")
 
-        return files, {
+        album = {
             "album_id"   : self.album_id,
-            "album_name" : extr("name: '", "'"),
-            "date"       : text.parse_timestamp(extr("timestamp: ", ",")),
-            "album_size" : text.parse_int(extr("totalSize: ", ",")),
-            "description": extr("description: `", "`"),
-            "count"      : len(files),
+            "album_name" : text.unescape(extr('title="', '"')),
+            "album_size" : text.parse_bytes(extr(
+                '<p class="title">', "B")),
+            "date"       : text.parse_datetime(extr(
+                '<p class="title">', '<'), "%d.%m.%Y"),
+            "description": text.unescape(text.unescape(  # double
+                desc.rpartition(" [R")[0])),
         }
+
+        file_ids = list(text.extract_iter(page, 'id="file" href="/f/', '"'))
+        album["count"] = len(file_ids)
+        return self._extract_files(file_ids), album
+
+    def _extract_files(self, file_ids):
+        for file_id in file_ids:
+            url = "{}/api/f/{}".format(self.root, file_id)
+            yield self.request(url).json()
