c473a8251f9a757ea6041a2464aa25202f3ec0b6
Mike Fährmann <mike_faehrmann@web.de>
2023-12-25 15:15:59 +0100

    fix and update zsh completion (#4972)
    - backslash-escape '[' and ']'
    - replace single quotes with '\'' instead of "
    - ignore SUPPRESSed options

--- scripts/completion_zsh.py
+++ scripts/completion_zsh.py
@@ -10,8 +10,8 @@
 """Generate zsh completion script from gallery-dl's argument parser"""
 
 import util
+import argparse
 from gallery_dl import option
-
 
 TEMPLATE = """#compdef gallery-dl
 
@@ -19,23 +19,30 @@ local curcontext="$curcontext"
 typeset -A opt_args
 
 local rc=1
-_arguments -C -S \\
+_arguments -s -S \\
 %(opts)s && rc=0
 
 return rc
 """
 
+TR = str.maketrans({
+    "'": "'\\''",
+    "[": "\\[",
+    "]": "\\]",
+})
+
+
 opts = []
 for action in option.build_parser()._actions:
 
-    if not action.option_strings:
+    if not action.option_strings or action.help == argparse.SUPPRESS:
         continue
     elif len(action.option_strings) == 1:
         opt = action.option_strings[0]
     else:
         opt = "{" + ",".join(action.option_strings) + "}"
 
-    opt += "'[" + action.help.replace("'", '"') + "]'"
+    opt += "'[" + action.help.translate(TR) + "]'"
 
     if action.metavar:
         opt += ":'<" + action.metavar.lower() + ">'"
