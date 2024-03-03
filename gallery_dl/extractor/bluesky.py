mikf/gallery-dl

commit aabfd833407829490642cd313364c626f270a42c

[bluesky] add 'reposts' option (#4438, #5248)

gallery_dl/extractor/bluesky.py
@@ -39,12 +39,18 @@ class BlueskyExtractor(Extractor):
         self._metadata_facets = ("facets" in meta)
 
         self.api = BlueskyAPI(self)
-        self._user = None
+        self._user = self._user_did = None
 
     def items(self):
         for post in self.posts():
             if "post" in post:
                 post = post["post"]
+
+            pid = post["uri"].rpartition("/")[2]
+            if self._user_did and post["author"]["did"] != self._user_did:
+                self.log.debug("Skipping %s (repost)", pid)
+                continue
+
             post.update(post["record"])
             del post["record"]
 
@@ -75,7 +81,7 @@ class BlueskyExtractor(Extractor):
             if self._metadata_user:
                 post["user"] = self._user or post["author"]
 
-            post["post_id"] = post["uri"].rpartition("/")[2]
+            post["post_id"] = pid
             post["count"] = len(images)
             post["date"] = text.parse_datetime(
                 post["createdAt"][:19], "%Y-%m-%dT%H:%M:%S")
@@ -385,8 +391,11 @@ class BlueskyAPI():
         else:
             did = self.resolve_handle(actor)
 
-        if self.extractor._metadata_user:
-            self.extractor._user = self.get_profile(did)
+        extr = self.extractor
+        if not extr.config("reposts", False):
+            extr._user_did = did
+        if extr._metadata_user:
+            extr._user = self.get_profile(did)
 
         return did
 

commit e4a063f8d70ea3afa63cd52e403b31412a7f4295

[bluesky] fix feeds and lists
bug introduced in aabfd833

gallery_dl/extractor/bluesky.py
@@ -321,7 +321,7 @@ class BlueskyAPI():
         endpoint = "app.bsky.feed.getFeed"
         params = {
             "feed" : "at://{}/app.bsky.feed.generator/{}".format(
-                self._did_from_actor(actor), feed),
+                self._did_from_actor(actor, False), feed),
             "limit": "100",
         }
         return self._pagination(endpoint, params)
@@ -338,7 +338,7 @@ class BlueskyAPI():
         endpoint = "app.bsky.feed.getListFeed"
         params = {
             "list" : "at://{}/app.bsky.graph.list/{}".format(
-                self._did_from_actor(actor), list),
+                self._did_from_actor(actor, False), list),
             "limit": "100",
         }
         return self._pagination(endpoint, params)
@@ -385,14 +385,14 @@ class BlueskyAPI():
         }
         return self._pagination(endpoint, params, "posts")
 
-    def _did_from_actor(self, actor):
+    def _did_from_actor(self, actor, user_did=True):
         if actor.startswith("did:"):
             did = actor
         else:
             did = self.resolve_handle(actor)
 
         extr = self.extractor
-        if not extr.config("reposts", False):
+        if user_did and not extr.config("reposts", False):
             extr._user_did = did
         if extr._metadata_user:
             extr._user = self.get_profile(did)