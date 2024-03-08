cb4e2f631e212a5f04da240f9f4dd27f6adba64b
Mike Fährmann <mike_faehrmann@web.de>
2024-03-08 23:13:53 +0100

    [workflows] release standalone executables in gdl-org/builds
    needs some form of release notes,
    probably just git commits since last stable release

--- scripts/pyinstaller.py
+++ scripts/pyinstaller.py
@@ -4,16 +4,34 @@
 """Build a standalone executable using PyInstaller"""
 
 import PyInstaller.__main__
+import argparse
 import util
-import os
+import sys
 
-PyInstaller.__main__.run([
-    "--onefile",
-    "--console",
-    "--name", "gallery-dl." + ("exe" if os.name == "nt" else "bin"),
-    "--additional-hooks-dir", util.path("scripts"),
-    "--distpath", util.path("dist"),
-    "--workpath", util.path("build"),
-    "--specpath", util.path("build"),
-    util.path("gallery_dl", "__main__.py"),
-])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--os")
+    parser.add_argument("-a", "--arch")
+    args = parser.parse_args()
+
+    name = "gallery-dl"
+    if args.os:
+        name = "{}_{}".format(name, args.os.partition("-")[0].lower())
+    if args.arch == "x86":
+        name += "_x86"
+
+    PyInstaller.__main__.run([
+        "--onefile",
+        "--console",
+        "--name", name,
+        "--additional-hooks-dir", util.path("scripts"),
+        "--distpath", util.path("dist"),
+        "--workpath", util.path("build"),
+        "--specpath", util.path("build"),
+        util.path("gallery_dl", "__main__.py"),
+    ])
+
+
+if __name__ == "__main__":
+    sys.exit(main())
