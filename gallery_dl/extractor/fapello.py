eb673a7204c9c70eb7d74905c79bfd7b2eaacea8
wankio <31354933+wankio@users.noreply.github.com>
2024-03-19 02:48:53 +0700

    Update fapello.py
    get fullsize image instead resized

--- gallery_dl/extractor/fapello.py
+++ gallery_dl/extractor/fapello.py
@@ -42,7 +42,7 @@ class FapelloPostExtractor(Extractor):
             "type" : "video" if 'type="video' in page else "photo",
             "thumbnail": text.extr(page, 'poster="', '"'),
         }
-        url = text.extr(page, 'src="', '"')
+        url = text.extr(page, 'src="', '"').replace(".md", "")
         yield Message.Directory, data
         yield Message.Url, url, text.nameext_from_url(url, data)
 
