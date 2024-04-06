mikf/gallery-dl

commit f4c4d1cdd6bbb142d6bf71933e4134d44a699d60

[twitter] add 'quotes' extractor (#5262)
https://github.com/mikf/gallery-dl/issues/5262#issuecomment-1981571924

It's implemented as a search for 'quoted_tweet_id:…' on Twitter.

gallery_dl/extractor/twitter.py
@@ -731,9 +731,9 @@ class TwitterEventExtractor(TwitterExtractor):
 
 
 class TwitterTweetExtractor(TwitterExtractor):
-    """Extractor for images from individual tweets"""
+    """Extractor for individual tweets"""
     subcategory = "tweet"
-    pattern = BASE_PATTERN + r"/([^/?#]+|i/web)/status/(\d+)"
+    pattern = BASE_PATTERN + r"/([^/?#]+|i/web)/status/(\d+)/?$"
     example = "https://twitter.com/USER/status/12345"
 
     def __init__(self, match):
@@ -808,6 +808,18 @@ class TwitterTweetExtractor(TwitterExtractor):
             return buffer
 
         return itertools.chain(buffer, tweets)
+
+
+class TwitterQuotesExtractor(TwitterExtractor):
+    """Extractor for quotes of a Tweet"""
+    subcategory = "quotes"
+    pattern = BASE_PATTERN + r"/(?:[^/?#]+|i/web)/status/(\d+)/quotes"
+    example = "https://twitter.com/USER/status/12345/quotes"
+
+    def items(self):
+        url = "{}/search?q=quoted_tweet_id:{}".format(self.root, self.user)
+        data = {"_extractor": TwitterSearchExtractor}
+        yield Message.Queue, url, data
 
 
 class TwitterAvatarExtractor(TwitterExtractor):

test/results/twitter.py
@@ -218,7 +218,7 @@ __tests__ = (
     "#category": ("", "twitter", "hashtag"),
     "#class"   : twitter.TwitterHashtagExtractor,
     "#pattern" : twitter.TwitterSearchExtractor.pattern,
-    "#sha1_url": "3571c3a53b7647ea35517041fdc17f77ec5b2cb9",
+    "#urls"    : "https://twitter.com/search?q=%23nature",
 },
 
 {
@@ -535,6 +535,14 @@ You’ll be able to receive four Galarian form Pokémon with Hidden Abilities, p
     "content": """BREAKING - DEADLY LIES: Independent researchers at Texas A&M University have just contradicted federal government regulators, saying that toxic air pollutants in East Palestine, Ohio, could pose long-term risks. \n
 The Washington Post writes, "Three weeks after the toxic train derailment in Ohio, an analysis of Environmental Protection Agency data has found nine air pollutants at levels that could raise long-term health concerns in and around East Palestine, according to an independent analysis. \n
 "The analysis by Texas A&M University seems to contradict statements by state and federal regulators that air near the crash site is completely safe, despite residents complaining about rashes, breathing problems and other health effects." Your reaction.""",
+},
+
+{
+    "#url"     : "https://twitter.com/playpokemon/status/1263832915173048321/quotes",
+    "#category": ("", "twitter", "quotes"),
+    "#class"   : twitter.TwitterQuotesExtractor,
+    "#pattern" : twitter.TwitterSearchExtractor.pattern,
+    "#urls"    : "https://twitter.com/search?q=quoted_tweet_id:1263832915173048321",
 },
 
 {

commit 741e94bd12a81947311c73b18982fb4845353e0f

[twitter] match '/photo/' Tweet URLs (#5443)
fixes regression introduced in f4c4d1cd

gallery_dl/extractor/twitter.py
@@ -741,7 +741,8 @@ class TwitterEventExtractor(TwitterExtractor):
 class TwitterTweetExtractor(TwitterExtractor):
     """Extractor for individual tweets"""
     subcategory = "tweet"
-    pattern = BASE_PATTERN + r"/([^/?#]+|i/web)/status/(\d+)/?(?:$|[?#])"
+    pattern = (BASE_PATTERN + r"/([^/?#]+|i/web)/status/(\d+)"
+               r"/?(?:$|\?|#|photo/)")
     example = "https://twitter.com/USER/status/12345"
 
     def __init__(self, match):

test/results/twitter.py
@@ -527,6 +527,13 @@ You’ll be able to receive four Galarian form Pokémon with Hidden Abilities, p
 },
 
 {
+    "#url"     : "https://twitter.com/supernaturepics/status/604341487988576256/photo/1",
+    "#comment" : "/photo/ URL (#5443)",
+    "#category": ("", "twitter", "tweet"),
+    "#class"   : twitter.TwitterTweetExtractor,
+},
+
+{
     "#url"     : "https://twitter.com/morino_ya/status/1392763691599237121",
     "#comment" : "retweet with missing media entities (#1555)",
     "#category": ("", "twitter", "tweet"),