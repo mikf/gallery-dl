162756b684891a275f57512e548ef3cdfc028ad6
Mike Fährmann <mike_faehrmann@web.de>
2024-07-04 00:46:58 +0200

    [toyhouse] fix Content Warning bypass (#5820)

--- gallery_dl/extractor/toyhouse.py
+++ gallery_dl/extractor/toyhouse.py
@@ -77,22 +77,26 @@ class ToyhouseExtractor(Extractor):
                 cnt += 1
                 yield self._parse_post(post)
 
-            if cnt == 0 and params["page"] == 1:
-                token, pos = text.extract(
-                    page, '<input name="_token" type="hidden" value="', '"')
-                if not token:
-                    return
-                data = {
-                    "_token": token,
-                    "user"  : text.extract(page, 'value="', '"', pos)[0],
-                }
-                self.request(self.root + "/~account/warnings/accept",
-                             method="POST", data=data, allow_redirects=False)
-                continue
+            if not cnt and params["page"] == 1:
+                if self._accept_content_warning(page):
+                    continue
+                return
 
             if cnt < 18:
                 return
             params["page"] += 1
+
+    def _accept_content_warning(self, page):
+        pos = page.find(' name="_token"') + 1
+        token, pos = text.extract(page, ' value="', '"', pos)
+        user , pos = text.extract(page, ' value="', '"', pos)
+        if not token or not user:
+            return False
+
+        data = {"_token": token, "user": user}
+        self.request(self.root + "/~account/warnings/accept",
+                     method="POST", data=data, allow_redirects=False)
+        return True
 
 
 class ToyhouseArtExtractor(ToyhouseExtractor):
