mikf/gallery-dl

commit b413d2a8c928e59e70b6c9d4903a6501b2e79253

[nijie] support downloading videos (#5707, #5617)

gallery_dl/extractor/nijie.py
@@ -56,7 +56,7 @@ class NijieExtractor(AsynchronousMixin, BaseExtractor):
                 data["user_id"] = data["artist_id"]
                 data["user_name"] = data["artist_name"]
 
-            urls = list(self._extract_images(image_id, page))
+            urls = self._extract_images(image_id, page)
             data["count"] = len(urls)
 
             yield Message.Directory, data
@@ -113,11 +113,14 @@ class NijieExtractor(AsynchronousMixin, BaseExtractor):
             # multiple images
             url = "{}/view_popup.php?id={}".format(self.root, image_id)
             page = self.request(url).text
-            yield from text.extract_iter(
-                page, 'href="javascript:void(0);"><img src="', '"')
+            return [
+                text.extr(media, ' src="', '"')
+                for media in text.extract_iter(
+                    page, 'href="javascript:void(0);"><', '>')
+            ]
         else:
             pos = page.find('id="view-center"') + 1
-            yield text.extract(page, 'itemprop="image" src="', '"', pos)[0]
+            return (text.extr(page, 'itemprop="image" src="', '"', pos),)
 
     @staticmethod
     def _extract_user_name(page):

test/results/nijie.py
@@ -166,6 +166,18 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://nijie.info/view.php?id=385585",
+    "#comment" : "video (#5707)",
+    "#category": ("Nijie", "nijie", "image"),
+    "#class"   : nijie.NijieImageExtractor,
+    "#urls"    : (
+        "https://pic.nijie.net/01/nijie/20/82/59182/illust/0_0_162270ef49e2ee28_fab5ae.mp4",
+        "https://pic.nijie.net/04/nijie/20/82/59182/illust/385585_0_ff2d5d19129530d5_b2821e.jpg",
+        "https://pic.nijie.net/01/nijie/20/82/59182/illust/385585_1_7ee1a2a67bed2f84_212d67.jpg",
+    ),
+},
+
+{
     "#url"     : "https://nijie.info/view.php?id=70724",
     "#category": ("Nijie", "nijie", "image"),
     "#class"   : nijie.NijieImageExtractor,

commit 538ea8e02d14aa1e5d48e3e889e5a27debf855d9

[nijie] fix image URLs for single image posts (#5842)
fixes regression introduced in b413d2a8

gallery_dl/extractor/nijie.py
@@ -120,7 +120,8 @@ class NijieExtractor(AsynchronousMixin, BaseExtractor):
             ]
         else:
             pos = page.find('id="view-center"') + 1
-            return (text.extr(page, 'itemprop="image" src="', '"', pos),)
+            # do NOT use text.extr() here, as it doesn't support a pos argument
+            return (text.extract(page, 'itemprop="image" src="', '"', pos)[0],)
 
     @staticmethod
     def _extract_user_name(page):