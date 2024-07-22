be1583d2d46d7aeff69b9e6e4277ebadc3d6e8d9
SpiffyChatterbox <168247181+SpiffyChatterbox@users.noreply.github.com>
2024-07-22 16:32:50 -0400

    Better debug logging

--- gallery_dl/extractor/nudostar.py
+++ gallery_dl/extractor/nudostar.py
@@ -13,8 +13,22 @@ class NudostarExtractor(Extractor):
 
     def items(self):
         url = text.ensure_http_scheme(self.url)
+        page = self.request(text.ensure_http_scheme(url)).text
+        self.log.debug(f"Variable url is currently set to {url}")
+        
+        testtext = text.extract(page, '<div class="block-video"', '#343">')
+        self.log.debug(f"Text extracted is {testtext}")
+    
+        extractedfurther = text.extract(testtext, '<img src=', 'alt=')
+        self.log.debug(f"extractedfurther is {extractedfurther}")
+
+        html_removed = text.remove_html(testtext)
+        self.log.debug(f"Text html_removed is {html_removed}")
+
         data = text.nameext_from_url(url, {"url": url})
-        print(f'*** Debug:: Data is {data}')
+        self.log.debug(f"Data is {data}")
+
+
         yield Message.Directory, data
         yield Message.Url, url, data
         
