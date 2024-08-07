mikf/gallery-dl

commit ab2b82f398e7a297d6da5c0d3fb355d4df5904c7

[hotleak] fix faulty image URLs (#5915)

gallery_dl/extractor/hotleak.py
@@ -23,6 +23,12 @@ class HotleakExtractor(Extractor):
 
     def items(self):
         for post in self.posts():
+            if self.type == "photo":
+                post["url"] = (
+                    post["url"]
+                    .replace("/storage/storage/", "/storage/")
+                    .replace("_thumb.", ".")
+                )
             post["_http_expected_status"] = (404,)
             yield Message.Directory, post
             yield Message.Url, post["url"], post

commit d52b62b4334df094ce85e55ad16eb0569cb7d00e

[hotleak] fix AttributeError (#5950)
fixes regression introduced in ab2b82f3

gallery_dl/extractor/hotleak.py
@@ -23,7 +23,7 @@ class HotleakExtractor(Extractor):
 
     def items(self):
         for post in self.posts():
-            if self.type == "photo":
+            if not post["url"].startswith("ytdl:"):
                 post["url"] = (
                     post["url"]
                     .replace("/storage/storage/", "/storage/")

test/results/hotleak.py
@@ -13,20 +13,20 @@ __tests__ = (
     "#url"     : "https://hotleak.vip/kaiyakawaii/photo/1617145",
     "#category": ("", "hotleak", "post"),
     "#class"   : hotleak.HotleakPostExtractor,
-    "#pattern" : r"https://hotleak\.vip/storage/images/3625/1617145/fefdd5988dfcf6b98cc9e11616018868\.jpg",
+    "#urls"    : "https://image-cdn.hotleak.vip/storage/images/e98/18ad68/18ad68.webp",
 
     "id"       : 1617145,
     "creator"  : "kaiyakawaii",
     "type"     : "photo",
-    "filename" : "fefdd5988dfcf6b98cc9e11616018868",
-    "extension": "jpg",
+    "filename" : "18ad68",
+    "extension": "webp",
 },
 
 {
     "#url"     : "https://hotleak.vip/lilmochidoll/video/1625538",
     "#category": ("", "hotleak", "post"),
     "#class"   : hotleak.HotleakPostExtractor,
-    "#pattern" : r"ytdl:https://cdn8-leak\.camhdxx\.com/1661/1625538/index\.m3u8",
+    "#pattern" : r"ytdl:https://cdn\d+-leak\.camhdxx\.com/.+,\d+/1661/1625538/index\.m3u8",
 
     "id"       : 1625538,
     "creator"  : "lilmochidoll",