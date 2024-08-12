c25ff2ea8724058c87cdaa711ebef669b676e47d
SpiffyChatterbox <168247181+SpiffyChatterbox@users.noreply.github.com>
2024-08-12 08:25:09 -0400

    Update nudostar.py
    Still tinkering

--- gallery_dl/extractor/nudostar.py
+++ gallery_dl/extractor/nudostar.py
@@ -8,30 +8,18 @@ BASE_PATTERN = r"(?:https?://)?nudostar\.tv"
 
 class NudostarExtractor(Extractor):
     category = "nudostar"
-    pattern = BASE_PATTERN
+    pattern = BASE_PATTERN + r"/models/([^&#/]+)*/(\w*)/"
     # sampleurl = "https://nudostar.tv/models/megan-bitchell/343/"
 
     def items(self):
         url = text.ensure_http_scheme(self.url)
-        page = self.request(text.ensure_http_scheme(url)).text
-        self.log.debug(f"Variable url is currently set to {url}")
-        
-        testtext = text.extract(page, '<div class="block-video"', '#343">')
-        self.log.debug(f"Text extracted is {testtext}")
-    
-        extractedfurther = text.extract(testtext, '<img src=', 'alt=')
-        self.log.debug(f"extractedfurther is {extractedfurther}")
 
-        html_removed = text.remove_html(testtext)
-        self.log.debug(f"Text html_removed is {html_removed}")
+      <img src = ("https://nudostar.tv/contents/"
 
-        data = text.nameext_from_url(url, {"url": url})
-        self.log.debug(f"Data is {data}")
-
+                  "l/e/leena-1/1000/leena-1_0107.jpg")
 
         yield Message.Directory, data
         yield Message.Url, url, data
-        
-        
-        
-        
+
+
+
