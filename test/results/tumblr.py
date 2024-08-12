mikf/gallery-dl

commit 540eaa5addf0e8489deccc9f14c2d84c49802aa7

[tumblr] implement 'pagination' option (#5880)
restore pagination behavior from before
de670bd7de8600a1481ee6366680d3e08659f0f4

gallery_dl/extractor/tumblr.py
@@ -386,7 +386,7 @@ class TumblrAPI(oauth.OAuth1API):
     def posts(self, blog, params):
         """Retrieve published posts"""
         params["offset"] = self.extractor.config("offset")
-        params["limit"] = "50"
+        params["limit"] = 50
         params["reblog_info"] = "true"
         params["type"] = self.posts_type
         params["before"] = self.before
@@ -398,8 +398,14 @@ class TumblrAPI(oauth.OAuth1API):
 
     def likes(self, blog):
         """Retrieve liked posts"""
+        endpoint = "/v2/blog/{}/likes".format(blog)
         params = {"limit": "50", "before": self.before}
-        return self._pagination(blog, "/likes", params, key="liked_posts")
+        while True:
+            posts = self._call(endpoint, params)["liked_posts"]
+            if not posts:
+                return
+            yield from posts
+            params["before"] = posts[-1]["liked_timestamp"]
 
     def _call(self, endpoint, params, **kwargs):
         url = self.ROOT + endpoint
@@ -474,6 +480,7 @@ class TumblrAPI(oauth.OAuth1API):
         if self.api_key:
             params["api_key"] = self.api_key
 
+        strategy = self.extractor.config("pagination")
         while True:
             data = self._call(endpoint, params)
 
@@ -481,13 +488,31 @@ class TumblrAPI(oauth.OAuth1API):
                 self.BLOG_CACHE[blog] = data["blog"]
                 cache = False
 
-            yield from data[key]
+            posts = data[key]
+            yield from posts
 
-            try:
-                endpoint = data["_links"]["next"]["href"]
-            except KeyError:
-                return
+            if strategy == "api":
+                try:
+                    endpoint = data["_links"]["next"]["href"]
+                except KeyError:
+                    return
 
-            params = None
-            if self.api_key:
-                endpoint += "&api_key=" + self.api_key
+                params = None
+                if self.api_key:
+                    endpoint += "&api_key=" + self.api_key
+
+            elif strategy == "before":
+                if not posts:
+                    return
+                timestamp = posts[-1]["timestamp"] + 1
+                if params["before"] and timestamp >= params["before"]:
+                    return
+                params["before"] = timestamp
+                params["offset"] = None
+
+            else:  # offset
+                params["offset"] = \
+                    text.parse_int(params["offset"]) + params["limit"]
+                params["before"] = None
+                if params["offset"] >= data["total_posts"]:
+                    return

commit 785e6f291191156db21204dcaac07dec3505c4cf

[tumblr] fix 401 Unauthorized for likes when using api-key (#5994)
fixes regression introduced in 540eaa5a

gallery_dl/extractor/tumblr.py
@@ -400,6 +400,9 @@ class TumblrAPI(oauth.OAuth1API):
         """Retrieve liked posts"""
         endpoint = "/v2/blog/{}/likes".format(blog)
         params = {"limit": "50", "before": self.before}
+        if self.api_key:
+            params["api_key"] = self.api_key
+
         while True:
             posts = self._call(endpoint, params)["liked_posts"]
             if not posts:

test/results/tumblr.py
@@ -335,6 +335,14 @@ __tests__ = (
 },
 
 {
+    "#url"     : "http://mikf123.tumblr.com/likes",
+    "#category": ("", "tumblr", "likes"),
+    "#class"   : tumblr.TumblrLikesExtractor,
+    "#options" : {"api-secret": None},
+    "#count"   : 1,
+},
+
+{
     "#url"     : "https://www.tumblr.com/blog/view/mikf123/likes",
     "#category": ("", "tumblr", "likes"),
     "#class"   : tumblr.TumblrLikesExtractor,