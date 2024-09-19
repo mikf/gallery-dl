b44f0cdab05ffc4e50f984f1dfe2ec13c850c903
Mike Fährmann <mike_faehrmann@web.de>
2024-09-19 13:28:59 +0200

    [formatter] allow accessing 'util.NONE' via global '_nul'

--- gallery_dl/formatter.py
+++ gallery_dl/formatter.py
@@ -476,6 +476,7 @@ _GLOBALS = {
     "_env": lambda: os.environ,
     "_lit": lambda: _literal,
     "_now": datetime.datetime.now,
+    "_nul": lambda: util.NONE,
 }
 _CONVERSIONS = {
     "l": str.lower,
