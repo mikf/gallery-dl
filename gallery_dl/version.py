84f5c51a2c28a2d68bb819d7f4e94cd1652d3c6c
Mike Fährmann <mike_faehrmann@web.de>
2024-09-28 16:48:21 +0200

    [build] remove setuptools and requests version restrictions

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.27.5"
+__version__ = "1.27.6-dev"
 __variant__ = None
