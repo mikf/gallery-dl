80c7246732c115ddc5abd79060b321370872b957
Delphox <smugleaf@live.com>
2024-11-02 13:04:32 -0300

    [bluesky] match cbsky.app, bskye.app, bskyx.app and bsyy.app urls

--- gallery_dl/extractor/bluesky.py
+++ gallery_dl/extractor/bluesky.py
@@ -12,7 +12,7 @@ from .common import Extractor, Message
 from .. import text, util, exception
 from ..cache import cache, memcache
 
-BASE_PATTERN = r"(?:https?://)?bsky\.app"
+BASE_PATTERN = r"(?:https?://)?c?bs[ky]y[ex]?\.app"
 USER_PATTERN = BASE_PATTERN + r"/profile/([^/?#]+)"
 
 
