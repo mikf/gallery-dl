03658fab6133492ccfe3ca464656342b43658157
Mike Fährmann <mike_faehrmann@web.de>
2024-12-02 14:09:38 +0100

    [e621] fix 'TypeError' when 'metadata' is enabled (#6587)
    fixes regression introduced in 9e2ab8b1

--- gallery_dl/extractor/danbooru.py
+++ gallery_dl/extractor/danbooru.py
@@ -26,16 +26,7 @@ class DanbooruExtractor(BaseExtractor):
     def _init(self):
         self.ugoira = self.config("ugoira", False)
         self.external = self.config("external", False)
-
-        includes = self.config("metadata")
-        if includes:
-            if isinstance(includes, (list, tuple)):
-                includes = ",".join(includes)
-            elif not isinstance(includes, str):
-                includes = "artist_commentary,children,notes,parent,uploader"
-            self.includes = includes + ",id"
-        else:
-            self.includes = False
+        self.includes = False
 
         threshold = self.config("threshold")
         if isinstance(threshold, int):
@@ -56,6 +47,16 @@ class DanbooruExtractor(BaseExtractor):
         return pages * self.per_page
 
     def items(self):
+        # 'includes' initialization must be done here and not in '_init()'
+        # or it'll cause an exception with e621 when 'metadata' is enabled
+        includes = self.config("metadata")
+        if includes:
+            if isinstance(includes, (list, tuple)):
+                includes = ",".join(includes)
+            elif not isinstance(includes, str):
+                includes = "artist_commentary,children,notes,parent,uploader"
+            self.includes = includes + ",id"
+
         data = self.metadata()
         for post in self.posts():
 
