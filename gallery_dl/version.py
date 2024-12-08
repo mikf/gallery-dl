3b028866c76be2b7657d8341be96070dd6284488
Mike Fährmann <mike_faehrmann@web.de>
2024-12-08 20:57:12 +0100

    [cyberdrop] add extractor for media URLs (#2496)
    https://github.com/mikf/gallery-dl/issues/2496#issuecomment-2495467133

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.28.1"
+__version__ = "1.28.2-dev"
 __variant__ = None
