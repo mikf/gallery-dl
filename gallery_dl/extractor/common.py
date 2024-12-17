986f1302619caa85fde46e74d804097f252e4ca5
Mike Fährmann <mike_faehrmann@web.de>
2024-12-17 21:40:20 +0100

    [common] use random unused port for '"user-agent": "browser"'

--- gallery_dl/extractor/common.py
+++ gallery_dl/extractor/common.py
@@ -922,10 +922,11 @@ def _browser_useragent():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(("127.0.0.1", 6414))
+    server.bind(("127.0.0.1", 0))
     server.listen(1)
 
-    webbrowser.open("http://127.0.0.1:6414/user-agent")
+    host, port = server.getsockname()
+    webbrowser.open("http://{}:{}/user-agent".format(host, port))
 
     client = server.accept()[0]
     server.close()
