mikf/gallery-dl

commit e69956598c568f11f5faf8d7c348835fa0d5e913

[erome] extract 'tags' metadata (#7076)

gallery_dl/extractor/erome.py
@@ -44,6 +44,8 @@ class EromeExtractor(Extractor):
             pos = page.index('<div class="user-profile', pos)
             user, pos = text.extract(
                 page, 'href="https://www.erome.com/', '"', pos)
+            tags, pos = text.extract(
+                page, '<p class="mt-10"', '</p>', pos)
 
             urls = []
             date = None
@@ -59,11 +61,13 @@ class EromeExtractor(Extractor):
                         date = text.parse_timestamp(ts)
 
             data = {
-                "album_id"     : album_id,
-                "title"        : text.unescape(title),
-                "user"         : text.unquote(user),
-                "count"        : len(urls),
-                "date"         : date,
+                "album_id": album_id,
+                "title"   : text.unescape(title),
+                "user"    : text.unquote(user),
+                "count"   : len(urls),
+                "date"    : date,
+                "tags"    : [t.replace("+", " ")
+                             for t in text.extract_iter(tags, "?q=", '"')],
                 "_http_headers": {"Referer": url},
             }
 

commit a0eb9fd87b231fc8d8f2b2831676223e653f2f99

[erome] fix AttributeError for albums without tags (#7076)
fixes regression introduced in e69956598c568f11f5faf8d7c348835fa0d5e913

gallery_dl/extractor/erome.py
@@ -66,8 +66,9 @@ class EromeExtractor(Extractor):
                 "user"    : text.unquote(user),
                 "count"   : len(urls),
                 "date"    : date,
-                "tags"    : [t.replace("+", " ")
-                             for t in text.extract_iter(tags, "?q=", '"')],
+                "tags"    : ([t.replace("+", " ")
+                              for t in text.extract_iter(tags, "?q=", '"')]
+                             if tags else ()),
                 "_http_headers": {"Referer": url},
             }
 

gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.29.0"
+__version__ = "1.29.1-dev"
 __variant__ = None