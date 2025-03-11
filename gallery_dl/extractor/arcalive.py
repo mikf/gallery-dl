c534d0f4f9d5f12604a8e5c9660e800fec8b2f0e
Mike Fährmann <mike_faehrmann@web.de>
2025-03-11 17:45:56 +0100

    [arcalive] update names and formatting

--- gallery_dl/extractor/arcalive.py
+++ gallery_dl/extractor/arcalive.py
@@ -9,100 +9,100 @@
 from .common import Extractor, Message
 from .. import text, util, exception
 
+BASE_PATTERN = r"(?:https?://)?(?:www\.)?arca\.live"
+
 
 class ArcaliveExtractor(Extractor):
     """Base class for Arca.live extractors"""
-
     category = "arcalive"
     root = "https://arca.live"
 
     def _init(self):
         self.api = ArcaliveAPI(self)
-        self.session.headers["User-Agent"] = "net.umanle.arca.android.playstore/0.9.75"
-        self.session.headers["X-Device-Token"] = util.generate_token(64)
 
 
 class ArcalivePostExtractor(ArcaliveExtractor):
     """Extractor for an arca.live post"""
-
     subcategory = "post"
-    pattern = r"(?:https?://)?(?:www\.)?arca\.live/b/(?:\w+)/(\d+)"
-    example = "https://arca.live/b/breaking/123456789"
     directory_fmt = ("{category}", "{boardSlug}")
     filename_fmt = "{id}_{num}.{extension}"
     archive_fmt = "{id}_{num}"
+    pattern = BASE_PATTERN + r"/b/(?:\w+)/(\d+)"
+    example = "https://arca.live/b/breaking/123456789"
 
     def items(self):
-        (post_id,) = self.groups
-        post = self.api.post(post_id)
-
-        image_urls = self.parse_content(post["content"])
+        post = self.api.post(self.groups[0])
+        files = self._extract_files(post)
 
         post["date"] = text.parse_datetime(post["createdAt"])
-        post["count"] = len(image_urls)
+        post["count"] = len(files)
 
         yield Message.Directory, post
-        for post["num"], url in enumerate(image_urls, 1):
+        for post["num"], url in enumerate(files, 1):
             yield Message.Url, url, text.nameext_from_url(url, post)
 
-    def parse_content(self, content):
+    def _extract_files(self, post):
         # post["images"] doen't always have all the media
         # therefore, parse the content ourselves
         raise NotImplementedError
 
 
 class ArcaliveBoardExtractor(ArcaliveExtractor):
-    """Extractor for an arca.live board posts"""
-
+    """Extractor for an arca.live board's posts"""
     subcategory = "board"
-    pattern = r"(?:https?://)?(?:www\.)?arca\.live/b/(\w+)(?:\?([^#]+))?"
+    pattern = BASE_PATTERN + r"/b/(\w+)(?:/?\?([^#]+))?"
     example = "https://arca.live/b/breaking"
 
     def items(self):
-        board_slug, query_str = self.groups
-        query = text.parse_query(query_str)
-        articles = self.api.board(board_slug, query)
+        board, query = self.groups
+        params = text.parse_query(query)
+        articles = self.api.board(board, params)
 
         for article in articles:
             article["_extractor"] = ArcalivePostExtractor
-            url = "{}/b/{}/{}".format(self.root, board_slug, article["id"])
+            url = "{}/b/{}/{}".format(self.root, board, article["id"])
             yield Message.Queue, url, article
 
 
-class ArcaliveAPI:
+class ArcaliveAPI():
+
     def __init__(self, extractor):
         self.extractor = extractor
+        self.log = extractor.log
+        self.root = extractor.root + "/api/app"
+
+        headers = extractor.session.headers
+        headers["User-Agent"] = "net.umanle.arca.android.playstore/0.9.75"
+        headers["X-Device-Token"] = util.generate_token(64)
 
     def board(self, board_slug, params):
-        endpoint = "/api/app/list/channel/" + str(board_slug)
+        endpoint = "/list/channel/" + board_slug
         return self._pagination(endpoint, params, "articles")
 
     def post(self, post_id):
-        endpoint = "/api/app/view/article/breaking/" + str(post_id)
+        endpoint = "/view/article/breaking/" + str(post_id)
         return self._call(endpoint)
 
     def _call(self, endpoint, params=None):
-        url = self.extractor.root + endpoint
+        url = self.root + endpoint
         response = self.extractor.request(url, params=params)
+
         data = response.json()
+        if response.status_code == 200:
+            return data
 
-        if response.status_code != 200:
-            self.extractor.log.debug("Server response: %s", data)
-            if data.get("message"):
-                raise exception.StopExtraction(
-                    "API request failed: %s", data.get("message")
-                )
-            else:
-                raise exception.StopExtraction("API request failed")
-
-        return data
+        self.log.debug("Server response: %s", data)
+        msg = data.get("message")
+        raise exception.StopExtraction(
+            "API request failed%s", ": " + msg if msg else "")
 
     def _pagination(self, endpoint, params, key):
         while True:
             data = self._call(endpoint, params)
-            posts = data[key]
+
+            posts = data.get(key)
             if not posts:
                 break
-
             yield from posts
+
             params.update(data["next"])
