a2366b4c4a4a7f79b824a0cd2002bc46f6982efa
Mike Fährmann <mike_faehrmann@web.de>
2025-03-16 19:37:04 +0100

    [config] fix using same key multiple times with 'apply' (#7127)

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.29.2"
+__version__ = "1.29.3-dev"
 __variant__ = None
