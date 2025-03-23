8e32db507808da57d059de8f5a85eaba62b3ec09
Mike Fährmann <mike_faehrmann@web.de>
2025-03-23 15:20:12 +0100

    [bbc] extract more metadata (#6582)
    https://github.com/mikf/gallery-dl/issues/6582#issuecomment-2745905375
    
    - title
    - title_image
    - description
    - synopsis

--- test/results/bbc.py
+++ test/results/bbc.py
@@ -15,11 +15,49 @@ __tests__ = (
     "#pattern" : r"https://ichef\.bbci\.co\.uk/images/ic/1920xn/\w+\.jpg",
     "#count"   : 37,
 
-    "programme": "p084qtzs",
-    "path"     : [
+    "count"      : 37,
+    "num"        : range(1, 37),
+    "description": "The Cybermen attack. And for the Doctor, nothing will ever be the same.",
+    "programme"  : "p084qtzs",
+    "synopsis"   : "The Cybermen attack. And for the Doctor, nothing will ever be the same.",
+    "title"      : "The Timeless Children",
+    "title_image": {"The Timeless Children", ": The Timeless Children"},
+    "path"       : [
         "BBC One",
         "Doctor Who (2005–2022)",
         "The Timeless Children",
+    ],
+},
+
+{
+    "#url"     : "https://www.bbc.co.uk/programmes/p086f8yf/p086f8j6",
+    "#category": ("", "bbc", "gallery"),
+    "#class"   : bbc.BbcGalleryExtractor,
+    "#pattern" : r"https://ichef\.bbci\.co\.uk/images/ic/1920xn/\w+\.jpg",
+    "#range"   : "1-2",
+    "#count"   : 2,
+
+    "count"      : 9,
+    "num"        : {1, 2},
+    "description": "Continuing his journey, Colin gives unique insights into the unique animals he finds.",
+    "extension"  : "jpg",
+    "filename"   : {"p086f7yn", "p086f80n"},
+    "programme"  : "p086f8yf",
+    "title"      : "Wild Cuba: A Caribbean Journey - Part 2",
+    "title_image": {
+        "Cuba is home to many unique birds",
+        "A Cuban pygmy owl looks out of its tree hole",
+    },
+    "synopsis"   : {
+        "This vibrant Cuban tody is just one of more than 300 species of bird found in Cuba.",
+        "Cuban pygmy owls nest in abandoned holes carved out by woodpeckers.",
+    },
+    "path"       : [
+        "BBC Two",
+        "Natural World",
+        "2019-2020",
+        "Wild Cuba: A Caribbean Journey - Part 2",
+        "Wildlife camera operator Colin Stafford-Johnson has loved Cuba since he was a little boy"
     ],
 },
 
