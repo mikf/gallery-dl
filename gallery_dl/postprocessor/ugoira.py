mikf/gallery-dl

commit a08c35ad7990a3c532fbe327cd30ace3b416eaa7

[pp:ugoira] preserve 'extension' when using "mode": "archive" (#7304)

gallery_dl/postprocessor/ugoira.py
@@ -156,12 +156,7 @@ class UgoiraPP(PostProcessor):
                     return self.log.debug("", exc_info=exc)
 
             if self.convert(pathfmt, tempdir):
-                if self.delete:
-                    pathfmt.delete = True
-                elif pathfmt.extension != "zip":
-                    self.log.info(pathfmt.filename)
-                    pathfmt.set_extension("zip")
-                    pathfmt.build_path()
+                pathfmt.delete = self.delete
 
     def convert_from_files(self, pathfmt):
         if not self._convert_files:

commit fc561c2936a8e3f6955f1b9ba4af02139a0e1e11

[pp:ugoira] restore 'keep-files' functionality (#7304)
fixes regression introduced in a08c35ad7990a3c532fbe327cd30ace3b416eaa7

gallery_dl/postprocessor/ugoira.py
@@ -135,6 +135,7 @@ class UgoiraPP(PostProcessor):
         if not self._convert_zip:
             return
         self._zip_source = True
+        ext = pathfmt.extension
 
         with self._tempdir() as tempdir:
             if tempdir:
@@ -153,7 +154,12 @@ class UgoiraPP(PostProcessor):
                     return self.log.debug("", exc_info=exc)
 
             if self.convert(pathfmt, tempdir):
-                pathfmt.delete = self.delete
+                if self.delete:
+                    pathfmt.delete = True
+                elif pathfmt.extension != ext:
+                    self.log.info(pathfmt.filename)
+                    pathfmt.set_extension(ext)
+                    pathfmt.build_path()
 
     def convert_from_files(self, pathfmt):
         if not self._convert_files: