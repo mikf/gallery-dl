mikf/gallery-dl

commit 29d315e495a4965dad2ae3ff55488fa26588c645

[dl:ytdl] improve temp/part file handling (#6949 #7494)
- fixes files not being moved from 'part-directory' (#6949)
- fixes 'skip: enumerate' overwriting files (#7494)

gallery_dl/downloader/ytdl.py
@@ -130,18 +130,27 @@ class YoutubeDLDownloader(DownloaderBase):
         if pathfmt.exists():
             pathfmt.temppath = ""
             return True
-        if self.part and self.partdir:
-            pathfmt.temppath = os.path.join(
-                self.partdir, pathfmt.filename)
-
-        self._set_outtmpl(ytdl_instance, pathfmt.temppath.replace("%", "%%"))
 
         self.out.start(pathfmt.path)
+        if self.part:
+            pathfmt.kwdict["extension"] = pathfmt.prefix + "part"
+            filename = pathfmt.build_filename(pathfmt.kwdict)
+            pathfmt.kwdict["extension"] = info_dict["ext"]
+            if self.partdir:
+                path = os.path.join(self.partdir, filename)
+            else:
+                path = pathfmt.realdirectory + filename
+        else:
+            path = pathfmt.realpath
+
+        self._set_outtmpl(ytdl_instance, path.replace("%", "%%"))
         try:
             ytdl_instance.process_info(info_dict)
         except Exception as exc:
             self.log.debug("", exc_info=exc)
             return False
+
+        pathfmt.temppath = info_dict["filepath"]
         return True
 
     def _download_playlist(self, ytdl_instance, pathfmt, info_dict):

commit a798b8971ef16fd704eac42896b6d204df9fc104

[dl:ytdl] fix postprocessing/merge errors (#7581)
don't use a hardcoded '.part' filename extension

fixes regression introduced in 29d315e495a4965dad2ae3ff55488fa26588c645

gallery_dl/downloader/ytdl.py
@@ -134,17 +134,18 @@ class YoutubeDLDownloader(DownloaderBase):
 
         self.out.start(pathfmt.path)
         if self.part:
-            pathfmt.kwdict["extension"] = pathfmt.prefix + "part"
+            pathfmt.kwdict["extension"] = pathfmt.prefix
             filename = pathfmt.build_filename(pathfmt.kwdict)
             pathfmt.kwdict["extension"] = info_dict["ext"]
             if self.partdir:
                 path = os.path.join(self.partdir, filename)
             else:
                 path = pathfmt.realdirectory + filename
+            path = path.replace("%", "%%") + "%(ext)s"
         else:
-            path = pathfmt.realpath
+            path = pathfmt.realpath.replace("%", "%%")
 
-        self._set_outtmpl(ytdl_instance, path.replace("%", "%%"))
+        self._set_outtmpl(ytdl_instance, path)
         try:
             ytdl_instance.process_info(info_dict)
         except Exception as exc: