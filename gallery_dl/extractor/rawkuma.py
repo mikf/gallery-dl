a5b6ec444ee3be949eaff1f12a55ec79d42a3048
Mike Fährmann <mike_faehrmann@web.de>
2025-06-02 18:41:33 +0200

    fix 'chapter' extractor

--- gallery_dl/extractor/rawkuma.py
+++ gallery_dl/extractor/rawkuma.py
@@ -8,7 +8,6 @@
 
 from .common import MangaExtractor, ChapterExtractor
 from .. import text, util
-import re
 
 BASE_PATTERN = r"(?:https?://)?rawkuma\.(?:net|com)"
 
@@ -18,51 +17,41 @@ class RawkumaBase():
     category = "rawkuma"
     root = "https://rawkuma.net"
 
-    def get_title(self, page):
-        title = text.extr(page, 'property="og:title" content="', '"')
-        title = text.unescape(title).strip()
-        m = re.search(
-            r"(.+) (?:Manga|Chapter \d+) (?:Raw - Rawkuma)", title)
-        if m:
-            title = m.group(1)
-        return title
-
 
 class RawkumaChapterExtractor(RawkumaBase, ChapterExtractor):
     """Extractor for manga chapters from rawkuma.net"""
     archive_fmt = "{chapter_id}_{page}"
-    pattern = BASE_PATTERN + r"/([^/?#]+)-chapter-(\d+)"
-    example = "https://rawkuma.net/TITLE-chapter-123"
+    pattern = BASE_PATTERN + r"/([^/?#]+-chapter-\d+(?:-\d+)?)"
+    example = "https://rawkuma.net/TITLE-chapter-123/"
 
     def __init__(self, match):
-        url = match.group(0)
-        self.gid, self.chapter = match.groups()
+        url = "{}/{}/".format(self.root, match.group(1))
         ChapterExtractor.__init__(self, match, url)
 
     def metadata(self, page):
-        title = self.get_title(page)
-        chapter, sep, minor = self.chapter.partition(".")
+        item = util.json_loads(text.extr(page, ',"item":', "}};"))
+        title = text.rextr(
+            page, '<h1 class="entry-title', "</h1>").partition(" &#8211; ")[2]
+        date = text.extr(page, 'datetime="', '"')
+        chapter, sep, minor = item["c"].partition(".")
+
         return {
-            "manga": title,
-            "manga_id": self.gid,
-            "chapter": text.parse_int(chapter),
+            "manga"        : item["s"],
+            "manga_id"     : text.parse_int(item["mid"]),
+            "chapter"      : text.parse_int(chapter),
             "chapter_minor": sep + minor,
-            "chapter_id": "%s-chapter-%s" % (self.gid, self.chapter),
+            "chapter_id"   : text.parse_int(item["cid"]),
+            "title"        : text.unescape(title),
+            "date"         : text.parse_datetime(
+                date, "%Y-%m-%dWIB%H:%M:%S%z"),
+            "thumbnail"    : item.get("t"),
+            "lang"         : "ja",
+            "language"     : "Japanese",
         }
 
     def images(self, page):
-        results = []
-        pos = 0
-        json, pos = text.extract(page, "<script>ts_reader.run(",
-                                 ");</script>", pos)
-        json_data = util.json_loads(json)
-        source = json_data.get("sources", [{}])[0]
-        images = source.get("images", [])
-
-        for url in images:
-            results.append((url, None))
-
-        return results
+        images = util.json_loads(text.extr(page, '","images":', '}'))
+        return [(url, None) for url in images]
 
 
 class RawkumaMangaExtractor(RawkumaBase, MangaExtractor):
