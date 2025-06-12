ec6005d14e3947ad2a547791882f63643ec91044
Mike Fährmann <mike_faehrmann@web.de>
2025-06-12 23:38:39 +0200

    update copyright notices

--- gallery_dl/extractor/wikimedia.py
+++ gallery_dl/extractor/wikimedia.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2022 Ailothaen
-# Copyright 2024 Mike Fährmann
+# Copyright 2024-2025 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
