36549bce1e6f20f34e6a0b837c0fe4984abcf09e
Mike Fährmann <mike_faehrmann@web.de>
2025-06-15 22:22:12 +0200

    add tests

--- test/results/redbust.py
+++ test/results/redbust.py
@@ -10,7 +10,61 @@ from gallery_dl.extractor import redbust
 __tests__ = (
 {
     "#url"     : "https://redbust.com/sadie-gray-wearing-black-nighty/",
-    "#category": ("redbust"),
-    "#class"   : RedbustGalleryExtractor,
+    "#class"   : redbust.RedbustGalleryExtractor,
+    "#pattern" : r"https://redbust.com/stuff/sadie-gray-wearing-black-nighty/sadie-gray-morning-ritual-bathroom-naked-boobs-nighty-playboy-\d+\.jpg",
+    "#count"   : 35,
+
+    "count"       : 35,
+    "num"         : range(1, 35),
+    "date"        : "dt:2024-03-19 00:00:00",
+    "extension"   : "jpg",
+    "filename"    : str,
+    "gallery_id"  : "373920",
+    "gallery_slug": "sadie-gray-wearing-black-nighty",
+    "title"       : "Sadie Gray wearing black nighty",
+    "views"       : range(38_000, 50_000),
+    "categories": [
+        "Super Chicks",
+    ],
+    "tags"        : [
+        "bathroom",
+        "nighty",
+        "playboy",
+        "redhead",
+        "sadie gray",
+    ],
 },
+
+{
+    "#url"     : "https://redbust.com/girls-in-carwash/",
+    "#comment" : "legacy",
+    "#class"   : redbust.RedbustGalleryExtractor,
+    "#pattern" : r"https://redbust.com/wp-content/uploads/girls_in_carwash/girls_in_carwash_\d+\.jpg",
+    "#count"   : 27,
+
+    "count"       : 27,
+    "num"         : range(1, 27),
+    "date"        : "dt:2010-09-05 00:00:00",
+    "extension"   : "jpg",
+    "filename"    : str,
+    "gallery_id"  : "6517",
+    "gallery_slug": "girls-in-carwash",
+    "title"       : "Girls in carwash",
+    "views"       : range(9_000, 20_000),
+    "categories": [
+        "Automotive",
+    ],
+    "tags"        : [
+        "carwash",
+    ],
+},
+
+{
+    "#url"     : "https://redbust.com/other/",
+    "#comment" : "'category' listing",
+    "#class"   : redbust.RedbustGalleryExtractor,
+    "#pattern" : redbust.RedbustGalleryExtractor.pattern,
+    "#count"   : 28,
+},
+
 )
