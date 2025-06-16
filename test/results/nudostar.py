d56f86928224bf1087c074d160a0652c53e506c8
Mike Fährmann <mike_faehrmann@web.de>
2025-06-16 19:10:34 +0200

    expand tests

--- test/results/nudostar.py
+++ test/results/nudostar.py
@@ -9,18 +9,45 @@ from gallery_dl.extractor import nudostar
 
 __tests__ = (
 {
-    "#url": "https://nudostar.tv/models/eva-joys/",
-    "#class"   : nudostar.NudostarGalleryExtractor,
-    "#count"   : ">= 30",
-    "category" : ("nudostar"),
+    "#url"    : "https://nudostar.tv/models/eva-joys/",
+    "#class"  : nudostar.NudostarModelExtractor,
+    "#pattern": r"https://nudostar.tv/contents/e/v/eva-joys/1000/eva-joys_\d{4}\.jpg",
+    "#count"  : range(50, 80),
+
+    "count"      : 54,
+    "num"        : range(1, 54),
+    "extension"  : "jpg",
+    "filename"   : r"re:eva-joys_00\d\d",
+    "gallery_id" : "eva-joys",
+    "model"      : "eva_joy",
+    "model_slug" : "eva-joys",
+    "model_names": [
+        "eva_joy",
+        "eva_joys",
+    ],
 },
 
 {
-    "#url": "https://nudostar.tv/models/thebigtittiecommittee/148/",
-    "#class"    : nudostar.NudostarExtractor,
-    "category"  : "nudostar",
-    "filename" : "thebigtittiecommittee-148",
-    'extension': 'jpg',
-    "user_id": "thebigtittiecommittee",
+    "#url"    : "https://nl.nudostar.tv/models/eva-joys/",
+    "#class"  : nudostar.NudostarModelExtractor,
 },
+
+{
+    "#url"    : "https://nudostar.tv/models/thebigtittiecommittee/148/",
+    "#class"  : nudostar.NudostarImageExtractor,
+    "#results": "https://nudostar.tv/contents/t/h/thebigtittiecommittee/1000/thebigtittiecommittee_0148.jpg",
+
+    "extension"  : "jpg",
+    "filename"   : "thebigtittiecommittee_0148",
+    "gallery_id" : "thebigtittiecommittee",
+    "num"        : 148,
+    "url"        : "https://nudostar.tv/contents/t/h/thebigtittiecommittee/1000/thebigtittiecommittee_0148.jpg",
+    "model"      : "Hari Beavis",
+    "model_slug" : "thebigtittiecommittee",
+    "model_names": [
+        "Hari Beavis",
+        "Thebigtittiecommittee",
+    ],
+},
+
 )
