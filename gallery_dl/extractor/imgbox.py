mikf/gallery-dl

commit 811b665e33d807469b60b7984f3532201b2a2d4d

remove @staticmethod decorators
There might have been a time when calling a static method was faster
than a regular method, but that is no longer the case. According to
micro-benchmarks, it is 70% slower in CPython 3.13 and it also makes
executing the code of a class definition slower.

gallery_dl/extractor/imgbox.py
@@ -30,18 +30,15 @@ class ImgboxExtractor(Extractor):
                 text.nameext_from_url(imgdata["filename"], imgdata)
                 yield Message.Url, self.get_image_url(imgpage), imgdata
 
-    @staticmethod
-    def get_job_metadata():
+    def get_job_metadata(self):
         """Collect metadata for extractor-job"""
         return {}
 
-    @staticmethod
-    def get_image_keys():
+    def get_image_keys(self):
         """Return an iterable containing all image-keys"""
         return []
 
-    @staticmethod
-    def get_image_metadata(page):
+    def get_image_metadata(self, page):
         """Collect metadata for a downloadable file"""
         return text.extract_all(page, (
             ("num"      , '</a> &nbsp; ', ' of '),
@@ -49,8 +46,7 @@ class ImgboxExtractor(Extractor):
             ("filename" , ' title="', '"'),
         ))[0]
 
-    @staticmethod
-    def get_image_url(page):
+    def get_image_url(self, page):
         """Extract download-url"""
         return text.extr(page, 'property="og:image" content="', '"')
 
@@ -102,8 +98,7 @@ class ImgboxImageExtractor(ImgboxExtractor):
     def get_image_keys(self):
         return (self.image_key,)
 
-    @staticmethod
-    def get_image_metadata(page):
+    def get_image_metadata(self, page):
         data = ImgboxExtractor.get_image_metadata(page)
         if not data["filename"]:
             raise exception.NotFoundError("image")

commit 92ac35c932edf0e4f7ffebd2d8bc664047f76802

[imgbox] fix 'TypeError' when calling 'get_image_metadata()'
fixes regression introduced in 811b665e33d807469b60b7984f3532201b2a2d4d

gallery_dl/extractor/imgbox.py
@@ -99,7 +99,7 @@ class ImgboxImageExtractor(ImgboxExtractor):
         return (self.image_key,)
 
     def get_image_metadata(self, page):
-        data = ImgboxExtractor.get_image_metadata(page)
+        data = ImgboxExtractor.get_image_metadata(self, page)
         if not data["filename"]:
             raise exception.NotFoundError("image")
         return data