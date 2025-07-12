mikf/gallery-dl

commit 25c63deebf82dd4ce5bb1b7f79bc0d16f97ce2c6

[instagram] provide better 'video_dash_manifest' URLs (#7631)

gallery_dl/extractor/instagram.py
@@ -99,7 +99,7 @@ class InstagramExtractor(Extractor):
                         if videos_dash:
                             file["_fallback"] = (url,)
                             file["_ytdl_manifest"] = "dash"
-                            url = "ytdl:dash"
+                            url = f"ytdl:{post['post_url']}{file['num']}.mp4"
                         yield Message.Url, url, file
                     if previews:
                         file["media_id"] += "p"

commit 518c771e2150152cb7ed1446ae820c1e454288cc

[instagram] provide 'post_url' for stories and highlights (#7810)
fixes regression introduced in 25c63deebf82dd4ce5bb1b7f79bc0d16f97ce2c6

gallery_dl/extractor/instagram.py
@@ -165,10 +165,15 @@ class InstagramExtractor(Extractor):
             items = post["items"]
             reel_id = str(post["id"]).rpartition(":")[2]
             expires = post.get("expiring_at")
+            if expires:
+                post_url = f"{self.root}/stories/{post['user']['username']}/"
+            else:
+                post_url = f"{self.root}/stories/highlights/{reel_id}/"
             data = {
                 "expires": text.parse_timestamp(expires),
                 "post_id": reel_id,
                 "post_shortcode": shortcode_from_id(reel_id),
+                "post_url": post_url,
             }
             if "title" in post:
                 data["highlight_title"] = post["title"]