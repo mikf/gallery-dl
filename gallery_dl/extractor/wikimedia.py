mikf/gallery-dl

commit cd0cd5f3f2e56722267726dc88ec9b1598447272

remove @staticmethod decorators
There might have been a time when calling a static method was faster
than a regular method, but that is no longer the case. According to
micro-benchmarks, it is 70% slower in CPython 3.13 and it also makes
executing the code of a class definition slower.

gallery_dl/extractor/wikimedia.py
@@ -52,8 +52,7 @@ class WikimediaExtractor(BaseExtractor):
                 return url
         raise exception.StopExtraction("Unable to find API endpoint")
 
-    @staticmethod
-    def prepare(image):
+    def prepare(self, image):
         """Adjust the content of an image object"""
         image["metadata"] = {
             m["name"]: m["value"]

commit eb9e6d27e6a3a1273c552e9599dcd80fde3d8655

[wikimedia] add missing 'self' argument when calling 'prepare()' (#7835)
fixes regression introduced in cd0cd5f3f2e56722267726dc88ec9b1598447272

gallery_dl/extractor/wikimedia.py
@@ -238,7 +238,7 @@ class WikimediaArticleExtractor(WikimediaExtractor):
             }
 
     def prepare(self, image):
-        WikimediaExtractor.prepare(image)
+        WikimediaExtractor.prepare(self, image)
         image["page"] = self.title
 
 

test/results/bulbapedia.py
@@ -13,7 +13,7 @@ __tests__ = (
     "#category": ("wikimedia", "bulbapedia", "article"),
     "#class"   : wikimedia.WikimediaArticleExtractor,
     "#pattern" : r"https://archives\.bulbagarden\.net/media/upload/\w+/\w+/[^/?#]+",
-    "#count"   : range(10, 30),
+    "#count"   : range(8, 30),
 },
 
 {

test/results/fandom.py
@@ -35,12 +35,10 @@ __tests__ = (
     "extension"     : "png",
     "extmetadata"   : {
         "DateTime": {
-            "hidden": "",
             "source": "mediawiki-metadata",
             "value": "2015-01-28T05:22:55Z",
         },
         "ObjectName": {
-            "hidden": "",
             "source": "mediawiki-metadata",
             "value": "Yahagi",
         },