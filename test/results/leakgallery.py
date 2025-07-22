ee3a09682b721e5941fabb58120b2c454f68e2e2
Mike Fährmann <mike_faehrmann@web.de>
2025-07-22 21:31:50 +0200

    update test results
    and convert line endings to '\n'

--- test/results/leakgallery.py
+++ test/results/leakgallery.py
@@ -5,28 +5,43 @@
 # published by the Free Software Foundation.
 
 from gallery_dl.extractor import leakgallery
+FILE_PATTERN = r"https://cdn.leakgallery.com/content(-videos|\d+)?/[\w.-]+\.\w+"
+
 
 __tests__ = (
-    {
-        "#url": "https://leakgallery.com/sophieraiin/12240",
-        "#category": ("", "leakgallery", "post"),
-        "#class": leakgallery.LeakgalleryPostExtractor,
-        "id": 12240,
-        "creator": "sophieraiin",
-    },
-    {
-        "#url": "https://leakgallery.com/sophieraiin",
-        "#category": ("", "leakgallery", "user"),
-        "#class": leakgallery.LeakgalleryUserExtractor,
-    },
-    {
-        "#url": "https://leakgallery.com/trending-medias/Week",
-        "#category": ("", "leakgallery", "trending"),
-        "#class": leakgallery.LeakgalleryTrendingExtractor,
-    },
-    {
-        "#url": "https://leakgallery.com/most-liked",
-        "#category": ("", "leakgallery", "mostliked"),
-        "#class": leakgallery.LeakgalleryMostlikedExtractor,
-    },
+{
+    "#url"    : "https://leakgallery.com/sophieraiin/12240",
+    "#class"  : leakgallery.LeakgalleryPostExtractor,
+    "#results": "https://cdn.leakgallery.com/content-videos/watermark_745_sophieraiin_241.mp4",
+
+    "id"     : "12240",
+    "creator": "sophieraiin",
+},
+
+{
+    "#url"    : "https://leakgallery.com/sophieraiin",
+    "#class"  : leakgallery.LeakgalleryUserExtractor,
+    "#pattern": r"https://cdn.leakgallery.com/content3/(compressed_)?watermark_[0-9a-f]+_sophieraiin_\w+\.(jpg|png|mp4|mov)",
+    "#range"  : "1-100",
+    "#count"  : 100,
+
+    "creator": "sophieraiin",
+},
+
+{
+    "#url"    : "https://leakgallery.com/trending-medias/Week",
+    "#class"  : leakgallery.LeakgalleryTrendingExtractor,
+    "#pattern": FILE_PATTERN,
+    "#range"  : "1-100",
+    "#count"  : 100,
+},
+
+{
+    "#url"    : "https://leakgallery.com/most-liked",
+    "#class"  : leakgallery.LeakgalleryMostlikedExtractor,
+    "#pattern": FILE_PATTERN,
+    "#range"  : "1-100",
+    "#count"  : 100,
+},
+
 )
