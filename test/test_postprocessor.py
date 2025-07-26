b60fa63618291a0c3382096025831e12bc7b9bf1
cenodis <57576911+cenodis@users.noreply.github.com>
2025-07-26 16:58:15 +0200

    Update postprocessor tests

--- test/test_postprocessor.py
+++ test/test_postprocessor.py
@@ -211,7 +211,7 @@ class ExecTest(BasePostprocessorTest):
                 self.pathfmt.filename),
             shell=True,
             creationflags=0,
-            start_new_session=None,
+            start_new_session=False,
         )
         i.wait.assert_called_once_with()
 
@@ -235,7 +235,7 @@ class ExecTest(BasePostprocessorTest):
             ],
             shell=False,
             creationflags=0,
-            start_new_session=None,
+            start_new_session=False,
         )
 
     def test_command_many(self):
@@ -260,7 +260,7 @@ class ExecTest(BasePostprocessorTest):
                     self.pathfmt.filename),
                 shell=True,
                 creationflags=0,
-                start_new_session=None,
+                start_new_session=False,
             ),
             call(
                 [
@@ -270,7 +270,7 @@ class ExecTest(BasePostprocessorTest):
                 ],
                 shell=False,
                 creationflags=0,
-                start_new_session=None,
+                start_new_session=False,
             ),
         ])
 
