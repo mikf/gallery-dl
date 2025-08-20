mikf/gallery-dl

commit 5e37a9b98ca049409c6717c20dabb8dc1a08b48c

[tumblr] support URLs without subdomain (#7358)

gallery_dl/extractor/tumblr.py
@@ -17,7 +17,7 @@ import re
 BASE_PATTERN = (
     r"(?:tumblr:(?:https?://)?([^/]+)|"
     r"(?:https?://)?"
-    r"(?:www\.tumblr\.com/(?:blog/(?:view/)?)?([\w-]+)|"
+    r"(?:(?:www\.)?tumblr\.com/(?:blog/(?:view/)?)?([\w-]+)|"
     r"([\w-]+\.tumblr\.com)))"
 )
 
@@ -357,7 +357,7 @@ class TumblrLikesExtractor(TumblrExtractor):
 class TumblrSearchExtractor(TumblrExtractor):
     """Extractor for a Tumblr search"""
     subcategory = "search"
-    pattern = (BASE_PATTERN + r"/search/([^/?#]+)"
+    pattern = (r"(?:https?://)?(?:www\.)?tumblr\.com/search/([^/?#]+)"
                r"(?:/([^/?#]+)(?:/([^/?#]+))?)?(?:/?\?([^#]+))?")
     example = "https://www.tumblr.com/search/QUERY"
 

test/results/tumblr.py
@@ -93,6 +93,13 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://tumblr.com/mikf123",
+    "#comment" : "no 'www.' subdomain (#7358)",
+    "#category": ("", "tumblr", "user"),
+    "#class"   : tumblr.TumblrUserExtractor,
+},
+
+{
     "#url"     : "https://donttrustthetits.tumblr.com/",
     "#comment" : "pagination with 'date-max' (#2191) and 'api-key'",
     "#category": ("", "tumblr", "user"),
@@ -274,6 +281,12 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://tumblr.com/smarties-art/686047436641353728",
+    "#category": ("", "tumblr", "post"),
+    "#class"   : tumblr.TumblrPostExtractor,
+},
+
+{
     "#url"     : "http://demo.tumblr.com/tagged/Times%20Square",
     "#category": ("", "tumblr", "tag"),
     "#class"   : tumblr.TumblrTagExtractor,
@@ -361,6 +374,12 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://tumblr.com/mikf123/likes",
+    "#category": ("", "tumblr", "likes"),
+    "#class"   : tumblr.TumblrLikesExtractor,
+},
+
+{
     "#url"     : "https://www.tumblr.com/search/nathan fielder",
     "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
@@ -377,4 +396,11 @@ __tests__ = (
     "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
 },
+
+{
+    "#url"     : "https://tumblr.com/search/nathan%20fielder?t=90",
+    "#category": ("", "tumblr", "search"),
+    "#class"   : tumblr.TumblrSearchExtractor,
+},
+
 )

commit 2633a77517876c279502b927db61d084d0ad68b0

[tumblr:search] fix 'ValueError: not enough values to unpack' (#8079)
fixes regression introduced in 5e37a9b98ca049409c6717c20dabb8dc1a08b48c

gallery_dl/extractor/tumblr.py
@@ -382,7 +382,7 @@ class TumblrSearchExtractor(TumblrExtractor):
     example = "https://www.tumblr.com/search/QUERY"
 
     def posts(self):
-        _, _, _, search, mode, post_type, query = self.groups
+        search, mode, post_type, query = self.groups
         params = text.parse_query(query)
         return self.api.search(text.unquote(search), params, mode, post_type)
 

test/results/tumblr.py
@@ -381,26 +381,31 @@ __tests__ = (
 
 {
     "#url"     : "https://www.tumblr.com/search/nathan fielder",
-    "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
 },
 
 {
     "#url"     : "https://www.tumblr.com/search/nathan fielder/recent/quote?src=typed_query",
-    "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
 },
 
 {
     "#url"     : "https://www.tumblr.com/search/nathan%20fielder?t=90",
-    "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
 },
 
 {
     "#url"     : "https://tumblr.com/search/nathan%20fielder?t=90",
-    "#category": ("", "tumblr", "search"),
     "#class"   : tumblr.TumblrSearchExtractor,
+},
+
+{
+    "#url"     : "https://www.tumblr.com/search/Sonic?src=typed_query",
+    "#class"   : tumblr.TumblrSearchExtractor,
+    "#options" : {"original": False},
+    "#pattern" : r"https://\w+\.media\.tumblr\.com/.{10,}.\w+",
+    "#range"   : "1-10",
+    "count"    : range(1, 10),
 },
 
 {