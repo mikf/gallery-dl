392259107d8215d0b34f7c19e6b378803a13e31f
Mike Fährmann <mike_faehrmann@web.de>
2025-09-02 22:03:37 +0200

    [fansly] add initial support (#4401)
    - only posts and creator posts work
    - 'home' and 'list' extractors do nothing atm
    - needs 'authorization' header as 'token' config value
      to access locked content
    - needs yt-dlp to download 1080p videos

---
+++ test/results/fansly.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+from gallery_dl.extractor import fansly
+
+
+__tests__ = (
+{
+    "#url"     : "https://fansly.com/post/819035448046268416",
+    "#comment" : "video",
+    "#class"   : fansly.FanslyPostExtractor,
+},
+
+{
+    "#url"     : "https://fansly.com/post/815337432600821760",
+    "#comment" : "images",
+    "#class"   : fansly.FanslyPostExtractor,
+},
+
+{
+    "#url"     : "https://fansly.com/Oliviaus/posts",
+    "#class"   : fansly.FanslyCreatorPostsExtractor,
+},
+
+)
