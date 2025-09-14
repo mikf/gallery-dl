8fdc731fada2ed4ec7a7898214e0819654660d7b
Mike Fährmann <mike_faehrmann@web.de>
2025-09-14 09:37:23 +0200

    release version 1.30.7

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.30.7-dev"
+__version__ = "1.30.7"
 __variant__ = None
