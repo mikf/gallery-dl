9855b5b7f187e9d2d7776d094c7118721fed1ae5
Mike Fährmann <mike_faehrmann@web.de>
2025-10-03 22:26:45 +0200

    [instagram] support '/share/SHORTCODE' URLs (#8340)
    i.e. "bare" /share/ links

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -6,5 +6,5 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.30.9"
+__version__ = "1.31.0-dev"
 __variant__ = None
