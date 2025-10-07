mikf/gallery-dl

commit b49745d351081d702623925b782c5efb307e93a3

[instagram] ensure '_ytdl_manifest_data' exists before DASH DL (#8267)

gallery_dl/extractor/instagram.py
@@ -95,7 +95,7 @@ class InstagramExtractor(Extractor):
                     if videos:
                         file["_http_headers"] = videos_headers
                         text.nameext_from_url(url, file)
-                        if videos_dash:
+                        if videos_dash and "_ytdl_manifest_data" in post:
                             file["_fallback"] = (url,)
                             file["_ytdl_manifest"] = "dash"
                             url = f"ytdl:{post['post_url']}{file['num']}.mp4"

commit 338c69e5ed1b6a4ca78e96588452240d796cf9b3

[instagram] restore 'video_dash_manifest' downloads (#8364)
fixes regression introduced in b49745d351081d702623925b782c5efb307e93a3

gallery_dl/extractor/instagram.py
@@ -100,7 +100,7 @@ class InstagramExtractor(Extractor):
                     if videos:
                         file["_http_headers"] = videos_headers
                         text.nameext_from_url(url, file)
-                        if videos_dash and "_ytdl_manifest_data" in post:
+                        if videos_dash and "_ytdl_manifest_data" in file:
                             file["_fallback"] = (url,)
                             file["_ytdl_manifest"] = "dash"
                             url = f"ytdl:{post['post_url']}{file['num']}.mp4"