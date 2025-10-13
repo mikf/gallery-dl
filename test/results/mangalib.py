3289b230975ef55f76a22d2bd783dde164b79f5d
Vasiliy Mozhaev <mozhay2005@gmail.com>
2025-10-13 15:54:03 +0300

    [mangalib] more tests

--- test/results/mangalib.py
+++ test/results/mangalib.py
@@ -39,4 +39,57 @@ __tests__ = (
     "lang"     : "ru",
     "language" : "Russian",
 },
+
+{
+    "#url"     : "https://mangalib.me/ru/manga/21950--ashita-no-joe",
+    "#category": ("", "mangalib", "manga"),
+    "#class"   : mangalib.MangalibMangaExtractor,
+    "#pattern" : mangalib.MangalibChapterExtractor.pattern,
+    "#count"   : 126,
+
+    "manga"    : "ashita no joe",
+    "manga_slug": "21950--ashita-no-joe",
+    "volume"   : int,
+    "chapter"  : range(1, 126),
+    "chapter_minor": {"", ".5"},
+    "title"    : str,
+    "lang"     : "ru",
+    "language" : "Russian",
+},
+
+{
+    "#url"     : "https://mangalib.me/ru/21950--ashita-no-joe/read/v1/c5",
+    "#category": ("", "mangalib", "chapter"),
+    "#class"   : mangalib.MangalibChapterExtractor,
+    "#count"   : 24,
+
+    "manga"    : "ashita no joe",
+    "manga_slug": "21950--ashita-no-joe",
+    "volume"   : 1,
+    "chapter"  : 5,
+    "chapter_minor": "",
+    "title"    : str,
+    "lang"     : "ru",
+    "language" : "Russian",
+    "page"     : range(1, 25),
+},
+
+{
+    "#url"     : "https://mangalib.me/ru/21950--ashita-no-joe/read/v8/c80.5",
+    "#category": ("", "mangalib", "chapter"),
+    "#class"   : mangalib.MangalibChapterExtractor,
+    "#pattern" : r"https://img3\.mixlib\.me//manga/ashita-no-joe/chapters/2022662/[A-Za-z0-9._-]+\.png",
+    "#count"   : 14,
+
+    "manga"    : "ashita no joe",
+    "manga_slug": "21950--ashita-no-joe",
+    "volume"   : 8,
+    "chapter"  : 80,
+    "chapter_minor": ".5",
+    "title"    : str,
+    "lang"     : "ru",
+    "language" : "Russian",
+    "page"     : range(1, 15),
+},
+
 )
