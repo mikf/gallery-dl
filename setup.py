e2ad6465b7ce358922ac0dfbf6c5a0c9da12bc25
Mike Fährmann <mike_faehrmann@web.de>
2025-10-14 14:18:08 +0200

    [workflows:tests] include Python 3.14 & pypy3.11

--- setup.py
+++ setup.py
@@ -143,6 +143,7 @@ def build_setuptools():
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
             "Programming Language :: Python :: 3.13",
+            "Programming Language :: Python :: 3.14",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Internet :: WWW/HTTP",
