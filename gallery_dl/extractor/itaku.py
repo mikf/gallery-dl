7640991e04cf3cb9b3d6143ad7f96f2ee4e76de0
Mike Fährmann <mike_faehrmann@web.de>
2025-10-23 10:41:52 +0200

    [itaku] move API interface

--- gallery_dl/extractor/itaku.py
+++ gallery_dl/extractor/itaku.py
@@ -9,7 +9,6 @@
 """Extractors for https://itaku.ee/"""
 
 from .common import Extractor, Message, Dispatch
-from ..cache import memcache
 from .. import text, util
 
 BASE_PATTERN = r"(?:https?://)?itaku\.ee"
@@ -26,7 +25,7 @@ class ItakuExtractor(Extractor):
     request_interval = (0.5, 1.5)
 
     def _init(self):
-        self.api = ItakuAPI(self)
+        self.api = self.utils().ItakuAPI(self)
         self.videos = self.config("videos", True)
 
     def items(self):
@@ -240,91 +239,3 @@ class ItakuSearchExtractor(ItakuExtractor):
             "negative_tags": negative_tags,
             "optional_tags": optional_tags,
         })
-
-
-class ItakuAPI():
-
-    def __init__(self, extractor):
-        self.extractor = extractor
-        self.root = extractor.root + "/api"
-        self.headers = {
-            "Accept": "application/json, text/plain, */*",
-        }
-
-    def galleries_images(self, params, path=""):
-        endpoint = f"/galleries/images{path}/"
-        params = {
-            "cursor"    : None,
-            "date_range": "",
-            "maturity_rating": ("SFW", "Questionable", "NSFW"),
-            "ordering"  : "-date_added",
-            "page"      : "1",
-            "page_size" : "30",
-            "visibility": ("PUBLIC", "PROFILE_ONLY"),
-            **params,
-        }
-        return self._pagination(endpoint, params, self.image)
-
-    def posts(self, params):
-        endpoint = "/posts/"
-        params = {
-            "cursor"    : None,
-            "date_range": "",
-            "maturity_rating": ("SFW", "Questionable", "NSFW"),
-            "ordering"  : "-date_added",
-            "page"      : "1",
-            "page_size" : "30",
-            **params,
-        }
-        return self._pagination(endpoint, params)
-
-    def user_profiles(self, params):
-        endpoint = "/user_profiles/"
-        params = {
-            "cursor"   : None,
-            "ordering" : "-date_added",
-            "page"     : "1",
-            "page_size": "50",
-            "sfw_only" : "false",
-            **params,
-        }
-        return self._pagination(endpoint, params)
-
-    def image(self, image_id):
-        endpoint = f"/galleries/images/{image_id}/"
-        return self._call(endpoint)
-
-    def post(self, post_id):
-        endpoint = f"/posts/{post_id}/"
-        return self._call(endpoint)
-
-    @memcache(keyarg=1)
-    def user(self, username):
-        return self._call(f"/user_profiles/{username}/")
-
-    def user_id(self, username):
-        if username.startswith("id:"):
-            return int(username[3:])
-        return self.user(username)["owner"]
-
-    def _call(self, endpoint, params=None):
-        if not endpoint.startswith("http"):
-            endpoint = self.root + endpoint
-        return self.extractor.request_json(
-            endpoint, params=params, headers=self.headers)
-
-    def _pagination(self, endpoint, params, extend=None):
-        data = self._call(endpoint, params)
-
-        while True:
-            if extend is None:
-                yield from data["results"]
-            else:
-                for result in data["results"]:
-                    yield extend(result["id"])
-
-            url_next = data["links"].get("next")
-            if not url_next:
-                return
-
-            data = self._call(url_next)
