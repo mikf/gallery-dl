a1453afab049e409774670387f5a46f5fbbef5c1
Mike Fährmann <mike_faehrmann@web.de>
2025-10-23 17:02:18 +0200

    [blogger] move API interface

--- gallery_dl/extractor/blogger.py
+++ gallery_dl/extractor/blogger.py
@@ -27,7 +27,7 @@ class BloggerExtractor(BaseExtractor):
     archive_fmt = "{post[id]}_{num}"
 
     def _init(self):
-        self.api = BloggerAPI(self)
+        self.api = self.utils().BloggerAPI(self)
         self.blog = self.root.rpartition("/")[2]
         self.videos = self.config("videos", True)
 
@@ -48,7 +48,6 @@ class BloggerExtractor(BaseExtractor):
             r'blogger\.googleusercontent\.com/img|'
             r'lh\d+(?:-\w+)?\.googleusercontent\.com|'
             r'\d+\.bp\.blogspot\.com)/[^"]+)').findall
-        metadata = self.metadata()
 
         for post in self.posts(blog):
             content = post["content"]
@@ -65,36 +64,20 @@ class BloggerExtractor(BaseExtractor):
             post["author"] = post["author"]["displayName"]
             post["replies"] = post["replies"]["totalItems"]
             post["content"] = text.remove_html(content)
+            post["count"] = len(files)
             post["date"] = self.parse_datetime_iso(post["published"])
             del post["selfLink"]
             del post["blog"]
 
             data = {"blog": blog, "post": post}
-            if metadata:
-                data.update(metadata)
             yield Message.Directory, data
-
             for data["num"], url in enumerate(files, 1):
                 data["url"] = url
                 yield Message.Url, url, text.nameext_from_url(url, data)
 
-    def posts(self, blog):
-        """Return an iterable with all relevant post objects"""
-
-    def metadata(self):
-        """Return additional metadata"""
-
     def _extract_videos(self, files, post):
-        url = f"https://{self.blog}/feeds/posts/default/{post['id']}"
-        params = {
-            "alt"          : "json",
-            "v"            : "2",
-            "dynamicviews" : "1",
-            "rewriteforssl": "true",
-        }
-
-        data = self.request_json(url, params=params)
-        html = data["entry"]["content"]["$t"]
+        html = self.api.feeds_posts_default(
+            self.blog, post)["entry"]["content"]["$t"]
 
         for url in self.findall_video(html):
             page = self.request(url).text
@@ -140,12 +123,9 @@ class BloggerSearchExtractor(BloggerExtractor):
     pattern = rf"{BASE_PATTERN}/search/?\?q=([^&#]+)"
     example = "https://BLOG.blogspot.com/search?q=QUERY"
 
-    def metadata(self):
-        self.query = query = text.unquote(self.groups[-1])
-        return {"query": query}
-
     def posts(self, blog):
-        return self.api.blog_search(blog["id"], self.query)
+        self.kwdict["query"] = query = text.unquote(self.groups[-1])
+        return self.api.blog_search(blog["id"], query)
 
 
 class BloggerLabelExtractor(BloggerExtractor):
@@ -154,53 +134,6 @@ class BloggerLabelExtractor(BloggerExtractor):
     pattern = rf"{BASE_PATTERN}/search/label/([^/?#]+)"
     example = "https://BLOG.blogspot.com/search/label/LABEL"
 
-    def metadata(self):
-        self.label = label = text.unquote(self.groups[-1])
-        return {"label": label}
-
     def posts(self, blog):
-        return self.api.blog_posts(blog["id"], self.label)
-
-
-class BloggerAPI():
-    """Minimal interface for the Blogger API v3
-
-    https://developers.google.com/blogger
-    """
-    API_KEY = "AIzaSyCN9ax34oMMyM07g_M-5pjeDp_312eITK8"
-
-    def __init__(self, extractor):
-        self.extractor = extractor
-        self.api_key = extractor.config("api-key") or self.API_KEY
-
-    def blog_by_url(self, url):
-        return self._call("/blogs/byurl", {"url": url}, "blog")
-
-    def blog_posts(self, blog_id, label=None):
-        endpoint = f"/blogs/{blog_id}/posts"
-        params = {"labels": label}
-        return self._pagination(endpoint, params)
-
-    def blog_search(self, blog_id, query):
-        endpoint = f"/blogs/{blog_id}/posts/search"
-        params = {"q": query}
-        return self._pagination(endpoint, params)
-
-    def post_by_path(self, blog_id, path):
-        endpoint = f"/blogs/{blog_id}/posts/bypath"
-        return self._call(endpoint, {"path": path}, "post")
-
-    def _call(self, endpoint, params, notfound=None):
-        url = "https://www.googleapis.com/blogger/v3" + endpoint
-        params["key"] = self.api_key
-        return self.extractor.request_json(
-            url, params=params, notfound=notfound)
-
-    def _pagination(self, endpoint, params):
-        while True:
-            data = self._call(endpoint, params)
-            if "items" in data:
-                yield from data["items"]
-            if "nextPageToken" not in data:
-                return
-            params["pageToken"] = data["nextPageToken"]
+        self.kwdict["label"] = label = text.unquote(self.groups[-1])
+        return self.api.blog_posts(blog["id"], label)
