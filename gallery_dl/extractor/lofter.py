2d27d1e2b2db8053009c0667a8a2c8301f1a4130
Mike Fährmann <mike_faehrmann@web.de>
2025-10-24 20:10:54 +0200

    [lofter] move API interface

--- gallery_dl/extractor/lofter.py
+++ gallery_dl/extractor/lofter.py
@@ -7,7 +7,7 @@
 """Extractors for https://www.lofter.com/"""
 
 from .common import Extractor, Message
-from .. import text, util, exception
+from .. import text, util
 
 
 class LofterExtractor(Extractor):
@@ -19,7 +19,7 @@ class LofterExtractor(Extractor):
     archive_fmt = "{id}_{num}"
 
     def _init(self):
-        self.api = LofterAPI(self)
+        self.api = self.utils().LofterAPI(self)
 
     def items(self):
         for post in self.posts():
@@ -97,59 +97,3 @@ class LofterBlogPostsExtractor(LofterExtractor):
     def posts(self):
         blog_name = self.groups[0] or self.groups[1]
         return self.api.blog_posts(blog_name)
-
-
-class LofterAPI():
-
-    def __init__(self, extractor):
-        self.extractor = extractor
-
-    def blog_posts(self, blog_name):
-        endpoint = "/v2.0/blogHomePage.api"
-        params = {
-            "method": "getPostLists",
-            "offset": 0,
-            "limit": 200,
-            "blogdomain": blog_name + ".lofter.com",
-        }
-        return self._pagination(endpoint, params)
-
-    def post(self, blog_id, post_id):
-        endpoint = "/oldapi/post/detail.api"
-        params = {
-            "targetblogid": blog_id,
-            "postid": post_id,
-        }
-        return self._call(endpoint, params)["posts"][0]
-
-    def _call(self, endpoint, data):
-        url = "https://api.lofter.com" + endpoint
-        params = {
-            'product': 'lofter-android-7.9.10'
-        }
-        response = self.extractor.request(
-            url, method="POST", params=params, data=data)
-        info = response.json()
-
-        if info["meta"]["status"] == 4200:
-            raise exception.NotFoundError("blog")
-
-        if info["meta"]["status"] != 200:
-            self.extractor.log.debug("Server response: %s", info)
-            raise exception.AbortExtraction("API request failed")
-
-        return info["response"]
-
-    def _pagination(self, endpoint, params):
-        while True:
-            data = self._call(endpoint, params)
-            posts = data["posts"]
-
-            yield from posts
-
-            if data["offset"] < 0:
-                break
-
-            if params["offset"] + len(posts) < data["offset"]:
-                break
-            params["offset"] = data["offset"]
