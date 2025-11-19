992b7233a45fad1a9458095d2680b086937db025
Mike Fährmann <mike_faehrmann@web.de>
2025-11-19 10:22:05 +0100

    update & simplify
    - text.extract -> text.extr
    - self.request().json() -> self.request_json()
    - use self.groups
    - remove __init__
    - list comprehension

--- gallery_dl/extractor/sxypix.py
+++ gallery_dl/extractor/sxypix.py
@@ -12,29 +12,28 @@ from .. import text
 
 class SxypixGalleryExtractor(GalleryExtractor):
     """Extractor for image galleries from sxypix.com"""
-    root = "https://sxypix.com"
     category = "sxypix"
-    pattern = r"(?:https?://)?(?:www\.)?(sxypix\.com/w/(\w+))"
+    root = "https://sxypix.com"
+    pattern = r"(?:https?://)?(?:www\.)?sxypix\.com(/w/(\w+))"
     example = "https://sxypix.com/w/2bbaf1b24a5863d0e73436619bbaa7ee"
-
-    def __init__(self, match):
-        GalleryExtractor.__init__(self, match)
-        self.page_url = f"https://{match[1]}"
 
     def metadata(self, page):
         return {
-            "gallery_id": self.match[2],
-            "title": text.extract(page, '<meta name="keywords" content="', '" />')[0],
+            "gallery_id": self.groups[1],
+            "title": text.unescape(text.extr(
+                page, '<meta name="keywords" content="', '"')),
         }
 
     def images(self, page):
         data = {
-            "aid": text.extract(page, "data-aid='", "' ")[0],
-            "ghash": text.extract(page, "data-ghash='", "' ")[0],
+            "aid"  : text.extr(page, "data-aid='", "'"),
+            "ghash": text.extr(page, "data-ghash='", "'"),
         }
-        resp = self.request("https://sxypix.com/php/gall.php", method="POST", data=data)
+        gallery = self.request_json(
+            "https://sxypix.com/php/gall.php", method="POST", data=data)
 
-        for entry in resp.json()["r"]:
-            url = text.extract(entry, "img data-src=\'//.", "\'")[0]
-            url = f"https://x.{url}"
-            yield url, None
+        base = "https://x."
+        return [
+            (base + text.extr(entry, "data-src='//.", "'"), None)
+            for entry in gallery["r"]
+        ]
