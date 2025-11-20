dfbcaf99d07a8c2de98d4045fe3fbc4269031163
Mike Fährmann <mike_faehrmann@web.de>
2025-11-20 17:58:16 +0100

    update
    - generalize example URL
    - strip "Photo Gallery" from 'title'
    - extract 'id'

--- gallery_dl/extractor/eporner.py
+++ gallery_dl/extractor/eporner.py
@@ -4,7 +4,7 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-"""Extractors for https://eporner.com/"""
+"""Extractors for https://www.eporner.com/"""
 
 from .common import GalleryExtractor
 from .. import text
@@ -16,23 +16,22 @@ class EpornerGalleryExtractor(GalleryExtractor):
     root = "https://eporner.com"
     pattern = (r"(?:https?://)?(?:www\.)?eporner\.com"
                r"/gallery/(\w+)(?:/([\w-]+))?")
-    example = (
-        "https://www.eporner.com/gallery/mHNhErACQFE/"
-        "NaughtyAmerica-Lolly-Dames-My-Wife-s-Hot-Friend-"
-        "Big-Booty-Big-Tits-Lolly-Dames-Gets-Her-Pussy-Slammed-Hard/"
-    )
+    example = "https://www.eporner.com/gallery/GID/SLUG/"
 
     def __init__(self, match):
         url = f"{self.root}/gallery/{match[1]}/{match[2]}/"
         GalleryExtractor.__init__(self, match, url)
 
     def metadata(self, page):
-        gid, slug = self.groups
+        title = text.extr(page, "<title>", " - EPORNER</title>")
+        if title.endswith(" Photo Gallery"):
+            title = title[:-14]
+
         return {
-            "gallery_id": gid,
-            "slug"      : slug or "",
-            "title"     : text.unescape(text.extr(
-                page, "<title>", " - EPORNER</title>")),
+            "gallery_id": self.groups[0],
+            "title"     : text.unescape(title),
+            "slug"      : text.extr(
+                page, "/gallery/", '/"').rpartition("/")[2],
             "description": text.unescape(text.extr(
                 page, 'name="description" content="', '"')),
             "tags": text.extr(
@@ -40,15 +39,16 @@ class EpornerGalleryExtractor(GalleryExtractor):
         }
 
     def images(self, page):
-        album = text.extr(page, 'class="photosgrid gallerygrid"',
-                                "id='gallerySlideBox'")
-        urls = []
+        album = text.extr(
+            page, 'class="photosgrid gallerygrid"', "id='gallerySlideBox'")
+
+        results = []
         for url in text.extract_iter(album, ' src="', '"'):
             url, _, ext = url.rpartition(".")
             # Preview images have a resolution suffix.
             # E.g. "11208293-image-3_296x1000.jpg".
             # The same name, but without the suffix, leads to the full image.
-            main_url, _, _ = url.rpartition("_")
-            url = f"{main_url}.{ext}"
-            urls.append((url, None))
-        return urls
+            url = url[:url.rfind("_")]
+            name = url[url.rfind("/")+1:]
+            results.append((f"{url}.{ext}", {"id": name[:name.find("-")]}))
+        return results
