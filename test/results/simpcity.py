25ac5099cf4f2a500f8b473c271836729046f4f7
Mike Fährmann <mike_faehrmann@web.de>
2025-11-26 18:18:41 +0100

    [simpcity] extract attachment/inline files (#8560)

--- test/results/simpcity.py
+++ test/results/simpcity.py
@@ -13,7 +13,7 @@ __tests__ = (
     "#url"     : "https://simpcity.cr/threads/ririkana-rr_loveit.10731/post-1753131",
     "#class"   : simpcity.SimpcityPostExtractor,
     "#auth"    : True,
-    "#results" : "https://jpg5.su/img/coWRwo",
+    "#results" : "https://jpg6.su/img/coWRwo",
 
     "count" : 1,
     "num"   : 1,
@@ -25,7 +25,7 @@ __tests__ = (
         "date"      : "dt:2023-03-08 12:59:10",
         "id"        : "1753131",
         "content"   : """\
-<div class="bbWrapper"><a href="https://jpg5.su/img/coWRwo" target="_blank" class="link link--external" rel="noopener"><img src="https://simp6.jpg5.su/images/FqsNcNCaIAITBEL.md.jpg" data-url="https://simp6.jpg5.su/images/FqsNcNCaIAITBEL.md.jpg" class="bbImage " loading="lazy"
+<div class="bbWrapper"><a href="https://jpg6.su/img/coWRwo" target="_blank" class="link link--external" rel="noopener"><img src="https://simp6.jpg6.su/images/FqsNcNCaIAITBEL.md.jpg" data-url="https://simp6.jpg6.su/images/FqsNcNCaIAITBEL.md.jpg" class="bbImage " loading="lazy"
 \t\talt="FqsNcNCaIAITBEL.md.jpg" title="FqsNcNCaIAITBEL.md.jpg" style="" width="" height="" /></a></div>\
 """,
     },
@@ -65,7 +65,7 @@ __tests__ = (
     "#class"   : simpcity.SimpcityPostExtractor,
     "#auth"    : True,
     "#results" : (
-        "https://jpg5.su/img/NNFssUg",
+        "https://jpg6.su/img/NNFssUg",
         "https://saint2.cr/embed/nPy1kG3w55V",
         "https://saint2.cr/embed/c0KhPjU4-F3",
         "https://saint2.cr/embed/sZWnVZ_mQsV",
@@ -78,7 +78,10 @@ __tests__ = (
     "#comment" : "quote in post content (#8214)",
     "#class"   : simpcity.SimpcityPostExtractor,
     "#auth"    : True,
-    "#results" : ("/goto/post?id=13358068", "https://cyberdrop.me/a/Sh9GlG38"),
+    "#results" : (
+        "/goto/post?id=13358068",
+        "https://cyberdrop.cr/a/Sh9GlG38",
+    ),
 },
 
 {
@@ -87,10 +90,10 @@ __tests__ = (
     "#class"   : simpcity.SimpcityPostExtractor,
     "#auth"    : True,
     "#results" : (
-        "https://jpg5.su/img/aKroBJp",
-        "https://jpg5.su/img/aKroy2E",
-        "https://jpg5.su/img/aKrofqa",
-        "https://jpg5.su/img/aKroDgo",
+        "https://jpg6.su/img/aKroBJp",
+        "https://jpg6.su/img/aKroy2E",
+        "https://jpg6.su/img/aKrofqa",
+        "https://jpg6.su/img/aKroDgo",
         "https://bunkr.cr/v/6sErIc9pjrnQ3",
     ),
 
@@ -115,11 +118,99 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://simpcity.cr/threads/sophia-diamond.10049/post-10891",
+    "#class"   : simpcity.SimpcityPostExtractor,
+    "#auth"    : True,
+    "#results" : (
+        "https://brandarmy.com/SophiaDiamond",
+        "https://www.tiktok.com/@sophia.ilysm?lang=en",
+        "https://www.instagram.com/sophiadiamond/",
+        "https://simpcity.cr/attachments/sophiadiamond_239636842_558607608495946_5357173067872834144_n-jpg.65924/",
+    ),
+
+    "count"       : 4,
+    "num"         : range(1, 4),
+    "num_external": range(1, 3),
+    "num_internal": {0, 1},
+    "type"        : {"inline", "external"},
+    "post"        : {
+        "attachments": "",
+        "author"     : "inoncognito",
+        "author_id"  : "",
+        "author_url" : "",
+        "count"      : 4,
+        "date"       : "dt:2022-03-11 00:41:28",
+        "id"         : "10891",
+        "content"    : str,
+    },
+    "thread"      : {
+        "author"    : "inoncognito",
+        "author_id" : "53824",
+        "author_url": "https://simpcity.cr/members/inoncognito.53824/",
+        "date"      : "dt:2022-03-11 00:41:28",
+        "id"        : "10049",
+        "posts"     : range(1_000, 2_000),
+        "section"   : "TikTok",
+        "title"     : "Sophia Diamond",
+        "url"       : "https://simpcity.cr/threads/sophia-diamond.10049/",
+        "views"     : range(4_200_000, 6_000_000),
+        "tags"      : [
+            "busty",
+            "diamond",
+            "slut",
+            "sophia",
+            "sophiadiamond",
+            "tease",
+            "teen",
+            "tiktok",
+            "tits",
+        ],
+    },
+},
+
+{
+    "#url"     : "https://simpcity.cr/threads/sophia-diamond.10049/post-18744",
+    "#class"   : simpcity.SimpcityPostExtractor,
+    "#auth"    : True,
+    "#results" : "https://simpcity.cr/attachments/sophiadiamondcancunbikiniwp-png.36179/",
+
+    "count"       : 1,
+    "extension"   : "png",
+    "filename"    : "SophiaDiamondCancunBikiniWP",
+    "id"          : 36179,
+    "num"         : 1,
+    "num_external": 0,
+    "num_internal": 1,
+    "type"        : "inline",
+    "post"        : {
+        "author"     : "ElyseGooner",
+        "author_id"  : "65059",
+        "author_url" : "https://simpcity.cr/members/elysegooner.65059/",
+        "count"      : 1,
+        "date"       : "dt:2022-03-11 22:39:06",
+        "id"         : "18744",
+        "attachments": str,
+        "content"    : """\
+<div class="bbWrapper">Collage</div>
+
+
+            </div>\
+""",
+    },
+    "thread"      : {
+        "date"      : "dt:2022-03-11 00:41:28",
+        "id"        : "10049",
+        "section"   : "TikTok",
+        "title"     : "Sophia Diamond",
+    },
+},
+
+{
     "#url"     : "https://simpcity.cr/threads/alua-tatakai.89490/",
     "#class"   : simpcity.SimpcityThreadExtractor,
     "#auth"    : True,
-    "#pattern" : r"https://(jpg5\.su/img/\w+|bunkr\.\w+/[fiv]/\w+|pixeldrain.com/l/\w+|alua.com/tatakai)|/goto/post",
-    "#count"   : 29,
+    "#pattern" : r"https://(jpg6\.su/img/\w+|bunkr\.\w+/[fiv]/\w+|pixeldrain.com/l/\w+|alua.com/tatakai)|/goto/post",
+    "#count"   : range(100, 300),
 
     "count" : int,
     "num"   : int,
