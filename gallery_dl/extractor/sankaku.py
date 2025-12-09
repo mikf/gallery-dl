80a402f2673d0c8b5e5204be06fab706ae89e76a
Mike Fährmann <mike_faehrmann@web.de>
2025-12-09 10:02:43 +0100

    [sankaku][idolcomplex] fix download URLs (#8666)

--- gallery_dl/extractor/sankaku.py
+++ gallery_dl/extractor/sankaku.py
@@ -61,8 +61,8 @@ class SankakuExtractor(BooruExtractor):
                 self.log.warning(
                     "Login required to download 'contentious_content' posts")
                 SankakuExtractor._warning = False
-        elif url[8] == "v":
-            url = "https://s.sankakucomplex.com" + url[url.index("/", 8):]
+        elif url[4] != "s":
+            url = "https" + url[4:]
         return url
 
     def _prepare(self, post):
