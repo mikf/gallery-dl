mikf/gallery-dl

commit 4a4c1a1154af14bf39b74828b9710290029eb78b

[fanbox] yield data for inaccessible posts (#8643)

gallery_dl/extractor/fanbox.py
@@ -66,17 +66,16 @@ class FanboxExtractor(Extractor):
             if fee_max is not None and fee_max < item["feeRequired"]:
                 self.log.warning("Skipping post %s (feeRequired of %s > %s)",
                                  item["id"], item["feeRequired"], fee_max)
-                continue
+            else:
+                try:
+                    url = ("https://api.fanbox.cc/post.info?postId=" +
+                           item["id"])
+                    item = self.request_json(url, headers=self.headers)["body"]
+                except Exception as exc:
+                    self.log.warning("Skipping post %s (%s: %s)",
+                                     item["id"], exc.__class__.__name__, exc)
 
-            try:
-                url = "https://api.fanbox.cc/post.info?postId=" + item["id"]
-                body = self.request_json(url, headers=self.headers)["body"]
-                content_body, post = self._extract_post(body)
-            except Exception as exc:
-                self.log.warning("Skipping post %s (%s: %s)",
-                                 item["id"], exc.__class__.__name__, exc)
-                continue
-
+            content_body, post = self._extract_post(item)
             yield Message.Directory, post
             yield from self._get_urls_from_post(content_body, post)
 

commit 9133ee321f1b5fe6f3b9ab9c9193abc2736de209

[fanbox] make '_extract_post()' non-fatal (#8711)
fixes regression introduced in 4a4c1a1154af14bf39b74828b9710290029eb78b

gallery_dl/extractor/fanbox.py
@@ -127,15 +127,19 @@ class FanboxExtractor(Extractor):
                         if file.get("extension", "").lower() in exts
                     ]
 
-        post["date"] = self.parse_datetime_iso(post["publishedDatetime"])
+        try:
+            post["date"] = self.parse_datetime_iso(post["publishedDatetime"])
+        except Exception:
+            post["date"] = None
         post["text"] = content_body.get("text") if content_body else None
         post["isCoverImage"] = False
 
-        if self._meta_user:
-            post["user"] = self._get_user_data(post["creatorId"])
-        if self._meta_plan:
+        cid = post.get("creatorId")
+        if self._meta_user and cid is not None:
+            post["user"] = self._get_user_data(cid)
+        if self._meta_plan and cid is not None:
             plans = self._get_plan_data(post["creatorId"])
-            fee = post["feeRequired"]
+            fee = post.get("feeRequired") or 0
             try:
                 post["plan"] = plans[fee]
             except KeyError:
@@ -146,7 +150,7 @@ class FanboxExtractor(Extractor):
                     plan["fee"] = fee
                 post["plan"] = plans[fee] = plan
         if self._meta_comments:
-            if post["commentCount"]:
+            if post.get("commentCount"):
                 post["comments"] = list(self._get_comment_data(post["id"]))
             else:
                 post["commentd"] = ()

test/results/fanbox.py
@@ -191,6 +191,42 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://etlabsotwe.fanbox.cc/posts/11070192",
+    "#comment" : "(potentially?) missing 'publishedDatetime' (#8711)",
+    "#class"   : fanbox.FanboxPostExtractor,
+    "#results" : "https://pixiv.pximg.net/fanbox/public/images/post/11070192/cover/MGoWoTnJphuJSkD3owo3r7wG.jpeg",
+
+    "archives"       : (),
+    "creatorId"      : "etlabsotwe",
+    "date"           : "dt:2025-12-15 11:31:50",
+    "excerpt"        : "",
+    "extension"      : "jpeg",
+    "feeRequired"    : 200,
+    "fileUrl"        : "https://pixiv.pximg.net/fanbox/public/images/post/11070192/cover/MGoWoTnJphuJSkD3owo3r7wG.jpeg",
+    "filename"       : "MGoWoTnJphuJSkD3owo3r7wG",
+    "hasAdultContent": True,
+    "id"             : "11070192",
+    "imageForShare"  : "https://pixiv.pximg.net/c/1200x630_90_a2_g5/fanbox/public/images/post/11070192/cover/MGoWoTnJphuJSkD3owo3r7wG.jpeg",
+    "isCommentingRestricted": False,
+    "isCoverImage"   : True,
+    "isLiked"        : False,
+    "isPinned"       : False,
+    "isRestricted"   : True,
+    "likeCount"      : int,
+    "num"            : 0,
+    "publishedDatetime": "2025-12-15T20:31:50+09:00",
+    "tags"           : [],
+    "text"           : None,
+    "title"          : "Sketches 2025-12-15",
+    "type"           : "article",
+    "updatedDatetime": "iso:dt",
+    "user"           : {
+        "name"   : "ET",
+        "userId" : "74814193",
+    },
+},
+
+{
     "#url"     : "https://fanbox.cc/",
     "#category": ("", "fanbox", "home"),
     "#class"   : fanbox.FanboxHomeExtractor,