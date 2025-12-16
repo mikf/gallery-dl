dbeb0eb89fe031a0015007510c3ba82cd2513b28
Mike Fährmann <mike_faehrmann@web.de>
2025-12-16 18:46:23 +0100

    update tests

--- test/results/picazor.py
+++ test/results/picazor.py
@@ -1,16 +1,33 @@
 # -*- coding: utf-8 -*-
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
 from gallery_dl.extractor import picazor
 
+
 __tests__ = (
-    {
-        "#url": "https://picazor.com/en/kailey-mae",
+{
+    "#url"     : "https://picazor.com/en/kailey-mae",
+    "#class"   : picazor.PicazorUserExtractor,
+    "#range"   : "1-50",
+    "#pattern" : r"https://picazor\.com/uploads/.+\.jpg",
+    "#count"   : 50,
 
-        "#class": picazor.PicazorUserExtractor,
-
-
-        "user": "kailey-mae",
-        "path": "re:/uploads/.+",
-
+    "count"    : 278,
+    "num"      : range(200, 278),
+    "extension": "jpg",
+    "filename" : str,
+    "id"       : "re:[0-9a-f]+",
+    "mime"     : "photo",
+    "path"     : str,
+    "user"     : "kailey-mae",
+    "visible"  : True,
+    "subject"  : {
+        "id" : "66077ba2b64e3f3d178f39ac",
+        "uri": "kailey-mae",
     },
+},
+
 )
