fd6f19347a6369c7f2f8b64f0b8d56adf25a8f7c
Mike Fährmann <mike_faehrmann@web.de>
2025-12-26 14:07:48 +0100

    [actions] fix 'abort' (#8753)
    fixes regression introduced in 2eb5e5205590e64813456fc0d63ec7e55794d2f2
    
    forward the correct arguments to StopExtraction.__init__
    instead of logging message and 'extras'

--- gallery_dl/actions.py
+++ gallery_dl/actions.py
@@ -257,15 +257,21 @@ def action_raise(opts):
 
 
 def action_abort(opts):
-    return None, util.raises(exception.StopExtraction)
+    def _abort(_):
+        raise exception.StopExtraction(opts or None)
+    return None, _abort
 
 
 def action_terminate(opts):
-    return None, util.raises(exception.TerminateExtraction)
+    def _terminate(_):
+        raise exception.TerminateExtraction(opts)
+    return None, _terminate
 
 
 def action_restart(opts):
-    return None, util.raises(exception.RestartExtraction)
+    def _restart(_):
+        raise exception.RestartExtraction(opts)
+    return None, _restart
 
 
 def action_exit(opts):
@@ -274,7 +280,7 @@ def action_exit(opts):
     except ValueError:
         pass
 
-    def _exit(args):
+    def _exit(_):
         raise SystemExit(opts)
     return None, _exit
 
