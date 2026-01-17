fab907a4ed7f88e9320f61af1edac37754d988c7
braekja <braekja@gmail.com>
2026-01-17 12:05:05 -0600

    Add album support

--- gallery_dl/extractor/turbo.py
+++ gallery_dl/extractor/turbo.py
@@ -12,16 +12,83 @@ from gallery_dl.exception import HttpError
 from gallery_dl.extractor.message import Message
 from .common import Extractor
 
+from .. import text
+
 BASE_PATTERN = r"(?:https?://)?(?:turbo(?:vid)?\.cr)"
 
-
-class TurboMediaExtractor(Extractor):
-    """Extractor for turbo.cr videos"""
+class TurboAlbumMediaExtractor(Extractor):
+    """Extractor for turbo.cr album files"""
     category = "turbo"
     subcategory = "media"
     directory_fmt = ("{category}",)
 
-    pattern = r"https?://(?:www\.)?turbo(?:vid)?\.cr(/(embe)?d/([^/?#]+))"
+    pattern = r"https?://(?:www\.)?turbo(?:vid)?\.cr(/a/([^/?#]+))"
+    example = "https://turbo.cr/a/ID"
+
+    def items(self):
+        path, _ = self.groups
+
+        try:
+            # FIXME: self.root is empty by default
+            response = self.request(
+                self.url,
+            )
+
+            tbody, _ = text.extract(response.text, '<tbody id="fileTbody"', '</tbody>')
+            if not tbody:
+                raise Exception("Could not extract files from site")
+
+            ids = list(text.extract_iter(tbody, 'data-id="', '"'))
+
+            if not ids:
+                raise Exception("Could not extract files from site")
+
+        except Exception as ex:
+            self.log.error("%s: %s", ex.__class__.__name__, ex)
+            return (), {}
+
+        files = []
+        api_url = "https://turbo.cr/api/sign?v={}"
+
+        for v_id in ids:
+            try:
+                response = self.request(
+                    api_url.format(v_id),
+                ).json()
+            except HttpError as ex:
+                self.log.error("%s: %s", ex.__class__.__name__, ex)
+                return (), {}
+
+            video_url = response.get("url")
+
+            filename = response.get("filename")
+            filename = response.get("filename").split(".")[0]
+
+            files.append(
+                {
+                    "id"       : v_id,
+                    "url"       : video_url,
+                    "filename" : filename,
+                    "extension": "mp4",
+                    "category" : self.category,
+                    "_http_headers": {
+                        "Referer": self.root + path
+                    }
+                }
+            )
+
+        for data in files:
+            yield Message.Directory, "", data
+            yield Message.Url, data["url"], data
+
+
+class TurboMediaExtractor(Extractor):
+    """Extractor for turbo.cr single files"""
+    category = "turbo"
+    subcategory = "media"
+    directory_fmt = ("{category}",)
+
+    pattern = r"https?://(?:www\.)?turbo(?:vid)?\.cr(/(embe)?(v)?d/([^/?#]+))"
     example = "https://turbo.cr/embed/ID"
 
     def items(self):
@@ -31,7 +98,6 @@ class TurboMediaExtractor(Extractor):
         try:
             response = self.request(
                 api_url,
-                headers={"Referer": self.root + path}
             ).json()
         except HttpError as ex:
             self.log.error("%s: %s", ex.__class__.__name__, ex)
