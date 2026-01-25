bf3ee5e9f7a5c6b814a27fb23981abe2325c8af6
Mike Fährmann <mike_faehrmann@web.de>
2026-01-25 18:32:17 +0100

    [mangatown] fix & update
    - use BASE_PATTERN
    - fix manga, manga_id, chapter_id extraction
    - fix & extend 'manga' metadata results
    - extend test results

--- gallery_dl/extractor/mangatown.py
+++ gallery_dl/extractor/mangatown.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015-2025 Mike Fährmann
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
@@ -10,6 +8,8 @@
 
 from .common import ChapterExtractor, MangaExtractor
 from .. import text
+
+BASE_PATTERN = r"(?:https?://)?(?:www\.)?mangatown\.com"
 
 
 class MangatownBase():
@@ -20,34 +20,32 @@ class MangatownBase():
 
 class MangatownChapterExtractor(MangatownBase, ChapterExtractor):
     """Extractor for manga-chapters from mangatown.com"""
-    pattern = (r"(?:https?://)?(?:www\.)?mangatown\.com"
-               r"(/manga/[^/]+(?:/v0*(\d+))?/c(\d+[^/?#]*))")
+    pattern = BASE_PATTERN + r"(/manga/[^/?#]+(?:/v0*(\d+))?/c(\d+[^/?#]*))"
     example = "https://www.mangatown.com/manga/TITLE/c001/1.html"
 
     def __init__(self, match):
         self.part, self.volume, self.chapter = match.groups()
-        self.base = self.root + self.part + "/"
+        self.base = f"{self.root}{self.part}/"
         ChapterExtractor.__init__(self, match, self.base + "1.html")
 
     def metadata(self, page):
-        manga, pos = text.extract(page, '<title>', '</title>')
-        manga = manga.partition(" Manga")[0].replace("Read ", "", 1)
-
+        manga, pos = text.extract(
+            page, 'property="og:title" content="', '"')
         count     , pos = text.extract(page, "total_pages = ", ";", pos)
-        manga_id  , pos = text.extract(page, "series_id = ", ";", pos)
-        chapter_id, pos = text.extract(page, "chapter_id = ", ";", pos)
+        manga_id  , pos = text.extract(page, "series_id=", ";", pos)
+        chapter_id, pos = text.extract(page, "chapter_id=", ";", pos)
 
         chapter, dot, minor = self.chapter.partition(".")
 
         return {
-            "manga": text.unescape(manga),
+            "manga"   : text.unescape(manga),
             "manga_id": text.parse_int(manga_id),
-            "volume": text.parse_int(self.volume),
-            "chapter": text.parse_int(chapter),
+            "volume"  : text.parse_int(self.volume),
+            "chapter" : text.parse_int(chapter),
             "chapter_minor": dot + minor,
             "chapter_id": text.parse_int(chapter_id),
-            "count": text.parse_int(count),
-            "lang": "en",
+            "count"   : text.parse_int(count),
+            "lang"    : "en",
             "language": "English",
         }
 
@@ -67,8 +65,7 @@ class MangatownChapterExtractor(MangatownBase, ChapterExtractor):
 class MangatownMangaExtractor(MangatownBase, MangaExtractor):
     """Extractor for manga from mangatown.com"""
     chapterclass = MangatownChapterExtractor
-    pattern = (r"(?:https?://)?(?:www\.)?mangatown\.com"
-               r"(/manga/[^/?#]+)/?$")
+    pattern = BASE_PATTERN + r"(/manga/[^/?#]+)"
     example = "https://www.mangatown.com/manga/TITLE"
 
     def chapters(self, page):
@@ -78,23 +75,22 @@ class MangatownMangaExtractor(MangatownBase, MangaExtractor):
         manga = manga.partition(" Manga")[0].replace("Read ", "", 1)
         manga = text.unescape(manga)
 
-        page = text.extract(
-            page, 'class="chapter_list"', '</ul>', pos)[0]
+        page = text.extract(page, 'class="chapter_list"', '</ul>', pos)[0]
+        for ch in text.extract_iter(page, "<li>", "</li>"):
+            path , pos = text.extract(ch, '<a href="', '"')
+            title, pos = text.extract(ch, "<span>", "<", pos)
+            date , pos = text.extract(ch, 'class="time">', "<", pos)
 
-        pos = 0
-        while True:
-            url, pos = text.extract(page, '<a href="', '"', pos)
-            if not url:
-                return results
-            url = text.urljoin(self.root, url)
-            title, pos = text.extract(page, '>', '<', pos)
-            date , pos = text.extract(page, 'class="time"', '</span>', pos)
-            date = text.remove_html(date).strip()
+            chapter = text.extr(path, "/c", "/")
+            chapter, sep, minor = chapter.partition(".")
 
-            results.append((url, {
-                "manga": manga,
-                "title": text.unescape(title),
-                "date": date,
-                "lang": "en",
+            results.append((self.root + path, {
+                "manga"   : manga,
+                "chapter" : text.parse_int(chapter),
+                "chapter_minor": sep + minor,
+                "title"   : "" if title is None else text.unescape(title),
+                "date"    : date,
+                "lang"    : "en",
                 "language": "English",
             }))
+        return results
--- test/results/mangatown.py
+++ test/results/mangatown.py
@@ -10,36 +10,54 @@ from gallery_dl.extractor import mangatown
 __tests__ = (
 {
     "#url"     : "https://www.mangatown.com/manga/kimetsu_no_yaiba/c001/",
-    "#category": ("", "mangatown", "chapter"),
     "#class"   : mangatown.MangatownChapterExtractor,
     "#pattern" : r"https://zjcdn\.mangahere\.org/.*",
     "#count"   : ">= 20",
+
+    "chapter"      : 1,
+    "chapter_id"   : 368511,
+    "chapter_minor": "",
+    "count"        : 55,
+    "page"         : range(1, 55),
+    "extension"    : "jpg",
+    "filename"     : str,
+    "lang"         : "en",
+    "language"     : "English",
+    "manga"        : "Kimetsu no Yaiba",
+    "manga_id"     : 21437,
+    "volume"       : 0,
+
 },
 
 {
     "#url"     : "https://www.mangatown.com/manga/kimetsu_no_yaiba/c001/1.html",
-    "#category": ("", "mangatown", "chapter"),
     "#class"   : mangatown.MangatownChapterExtractor,
     "#pattern" : r"https://zjcdn\.mangahere\.org/.*",
 },
 
 {
     "#url"     : "http://www.mangatown.com/manga/kimetsu_no_yaiba/c001/",
-    "#category": ("", "mangatown", "chapter"),
     "#class"   : mangatown.MangatownChapterExtractor,
 },
 
 {
     "#url"     : "https://www.mangatown.com/manga/kimetsu_no_yaiba/",
-    "#category": ("", "mangatown", "manga"),
     "#class"   : mangatown.MangatownMangaExtractor,
     "#pattern" : mangatown.MangatownChapterExtractor.pattern,
     "#count"   : ">= 100",
+
+    "chapter"  : int,
+    "chapter_minor": {"", ".5", ".6"},
+    "date"     : str,
+    "lang"     : "en",
+    "language" : "English",
+    "manga"    : "Kimetsu no Yaiba",
+    "title"    : str,
+
 },
 
 {
     "#url"     : "http://www.mangatown.com/manga/kimetsu_no_yaiba/",
-    "#category": ("", "mangatown", "manga"),
     "#class"   : mangatown.MangatownMangaExtractor,
 },
 
