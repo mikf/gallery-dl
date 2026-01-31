7692d31a5773635eccb55c1a3b3c7b25f92b2866
Mike Fährmann <mike_faehrmann@web.de>
2026-01-31 20:09:26 +0100

    [twitter] move transaction_id.py

--- gallery_dl/extractor/utils/twitter_transaction_id.py
+++ gallery_dl/extractor/utils/twitter_transaction_id.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2025 Mike Fährmann
+# Copyright 2025-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -22,8 +22,8 @@ import random
 import hashlib
 import binascii
 import itertools
-from . import text, util
-from .cache import cache
+from ... import text, util
+from ...cache import cache
 
 
 class ClientTransaction():
