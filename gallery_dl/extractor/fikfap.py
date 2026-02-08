cd29ccadba588a4a441f506ac63e732ac5e64780
wise-immersion <wise_immersion@proton.me>
2026-02-08 00:52:47 +0000

    Update fikfap.py to allow extracting by hashtag
    Added functionality to extract by hashtag and save to directory named after the hashtag.

--- gallery_dl/extractor/fikfap.py
+++ gallery_dl/extractor/fikfap.py
@@ -103,3 +103,30 @@ class FikfapUserExtractor(FikfapExtractor):
             if len(data) < 21:
                 return
             params["afterId"] = data[-1]["postId"]
+
+
+class FikfapHashExtractor(FikfapExtractor):
+    
+    directory_fmt = ("{category}", "{hashtag}")
+    
+    subcategory = "hash"
+    pattern = BASE_PATTERN + r"/hash/([\w-]+)"
+    example = "https://fikfap.com/hash/HASH"
+    
+    def posts(self):
+        hashtag = self.groups[0]
+        
+        url = f"{self.root_api}/hashtags/label/{hashtag}/posts"
+        params = {"amount": "21"}
+        
+        while True:
+            data = self.request_api(url, params)
+            
+            for post in data:
+                post["hashtag"] = hashtag
+                
+                yield post
+            
+            if len(data) < 21:
+                return
+            params["afterId"] = data[-1]["postId"]
