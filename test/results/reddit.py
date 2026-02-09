mikf/gallery-dl

commit c16892a1503594d74c161559d4a1abcc1f7270f3

[reddit:user] provide 'user' metadata field (#8228)

gallery_dl/extractor/reddit.py
@@ -300,19 +300,24 @@ class RedditHomeExtractor(RedditSubredditExtractor):
 class RedditUserExtractor(RedditExtractor):
     """Extractor for URLs from posts by a reddit user"""
     subcategory = "user"
+    directory_fmt = ("{category}", "Users", "{user[name]}")
     pattern = (r"(?:https?://)?(?:\w+\.)?reddit\.com/u(?:ser)?/"
                r"([^/?#]+(?:/([a-z]+))?)/?(?:\?([^#]*))?$")
     example = "https://www.reddit.com/user/USER/"
 
     def __init__(self, match):
-        self.user, sub, params = match.groups()
-        self.params = text.parse_query(params)
-        if sub:
+        if sub := match[2]:
             self.subcategory += "-" + sub
         RedditExtractor.__init__(self, match)
 
     def submissions(self):
-        return self.api.submissions_user(self.user, self.params)
+        username, sub, qs = self.groups
+        username = text.unquote(username)
+        self.kwdict["user"] = user = self.api.user_about(username)
+
+        submissions = self.api.submissions_user(
+            user["name"], text.parse_query(qs))
+        return submissions
 
 
 class RedditSubmissionExtractor(RedditExtractor):
@@ -323,12 +328,8 @@ class RedditSubmissionExtractor(RedditExtractor):
                r"comments|gallery)|redd\.it)/([a-z0-9]+)")
     example = "https://www.reddit.com/r/SUBREDDIT/comments/id/"
 
-    def __init__(self, match):
-        RedditExtractor.__init__(self, match)
-        self.submission_id = match[1]
-
     def submissions(self):
-        return (self.api.submission(self.submission_id),)
+        return (self.api.submission(self.groups[0]),)
 
 
 class RedditImageExtractor(Extractor):
@@ -449,9 +450,9 @@ class RedditAPI():
         endpoint = subreddit + "/.json"
         return self._pagination(endpoint, params)
 
-    def submissions_user(self, user, params):
+    def submissions_user(self, username, params):
         """Collect all (submission, comments)-tuples posted by a user"""
-        endpoint = "/user/" + user + "/.json"
+        endpoint = f"/user/{username}/.json"
         return self._pagination(endpoint, params)
 
     def morechildren(self, link_id, children):
@@ -471,6 +472,10 @@ class RedditAPI():
                     children.extend(thing["data"]["children"])
                 else:
                     yield thing["data"]
+
+    def user_about(self, username):
+        endpoint = f"/user/{username}/about.json"
+        return self._call(endpoint, {})["data"]
 
     def authenticate(self):
         """Authenticate the application by requesting an access token"""

commit 52a5e39fc636a6152f8b01b55c27e8156a978e72

[reddit:user] fix user lookup when using sub view (#8228 #9032)
e.g. USER/submitted or USER/comments
fixes regression introduced in c16892a1503594d74c161559d4a1abcc1f7270f3

gallery_dl/extractor/reddit.py
@@ -302,12 +302,12 @@ class RedditUserExtractor(RedditExtractor):
     subcategory = "user"
     directory_fmt = ("{category}", "Users", "{user[name]}")
     pattern = (r"(?:https?://)?(?:\w+\.)?reddit\.com/u(?:ser)?/"
-               r"([^/?#]+(?:/([a-z]+))?)/?(?:\?([^#]*))?$")
+               r"([^/?#]+)(/[a-z]+)?/?(?:\?([^#]*))?")
     example = "https://www.reddit.com/user/USER/"
 
     def __init__(self, match):
         if sub := match[2]:
-            self.subcategory += "-" + sub
+            self.subcategory += "-" + sub[1:]
         RedditExtractor.__init__(self, match)
 
     def submissions(self):
@@ -316,7 +316,7 @@ class RedditUserExtractor(RedditExtractor):
         self.kwdict["user"] = user = self.api.user_about(username)
 
         submissions = self.api.submissions_user(
-            user["name"], text.parse_query(qs))
+            user["name"] + (sub or ""), text.parse_query(qs))
         if self.config("only", True):
             submissions = self._only(submissions, user)
         return submissions

test/results/reddit.py
@@ -92,6 +92,25 @@ __tests__ = (
 },
 
 {
+    "#url"     : "https://www.reddit.com/user/SeichiScout/submitted/?sort=hot",
+    "#category": ("", "reddit", "user-submitted"),
+    "#class"   : reddit.RedditUserExtractor,
+    "#pattern" : r"https://i\.redd\.it/\w+\.jpg",
+    "#range"   : "1-25",
+    "#count"   : 25,
+    "#archive" : False,
+
+    "author"          : "SeichiScout",
+    "author_fullname" : "t2_l8qpy6td6",
+    "user"            : {
+        "created_utc" : 1724480738.0,
+        "id"          : "l8qpy6td6",
+        "name"        : "SeichiScout",
+        "verified"    : True,
+    },
+},
+
+{
     "#url"     : "https://www.reddit.com/r/lavaporn/comments/8cqhub/",
     "#category": ("", "reddit", "submission"),
     "#class"   : reddit.RedditSubmissionExtractor,