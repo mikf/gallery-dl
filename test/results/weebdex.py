mikf/gallery-dl

commit 56168fbc8781e6283483b10e1c03912df185c506

[weebdex] add 'lang' option, support query params (#8957)
for example '?order=asc&group=j0fsj3oem3&tlang=en'

gallery_dl/extractor/weebdex.py
@@ -85,17 +85,21 @@ class WeebdexChapterExtractor(WeebdexBase, ChapterExtractor):
 class WeebdexMangaExtractor(WeebdexBase, MangaExtractor):
     """Extractor for weebdex manga"""
     chapterclass = WeebdexChapterExtractor
-    pattern = BASE_PATTERN + r"/title/(\w+)"
+    reverse = False
+    pattern = BASE_PATTERN + r"/title/(\w+)(?:/[^/?#]+/?\?([^#]+))?"
     example = "https://weebdex.org/title/ID/SLUG"
 
     def chapters(self, page):
-        mid = self.groups[0]
-        url = f"{self.root_api}/manga/{mid}/chapters"
-        params = {
-            "limit": 100,
-            "order": "asc" if self.config("chapter-reverse") else "desc",
-        }
+        mid, qs = self.groups
 
+        params = text.parse_query(qs)
+        params.setdefault("limit", 100)
+        if "tlang" not in params:
+            params["tlang"] = self.config("lang", "en")
+        if "order" not in params:
+            params["order"] = ("desc" if self.config("chapter-reverse") else
+                               "asc")
+        url = f"{self.root_api}/manga/{mid}/chapters"
         base = self.root + "/chapter/"
         manga = _manga_info(self, mid)
         results = []

test/results/weebdex.py
@@ -154,6 +154,7 @@ ___
     "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono",
     "#class"   : weebdex.WeebdexMangaExtractor,
     "#pattern" : weebdex.WeebdexChapterExtractor.pattern,
+    "#options" : {"lang": None},
     "#count"   : range(120, 300),
 
     "artist"       : ["Matsuda Minoru"],
@@ -209,6 +210,74 @@ ___
     "origin"     : "ja",
     "volume"     : int,
     "year"       : 2003,
+},
+
+{
+    "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono",
+    "#comment" : "'lang' option (#8957)",
+    "#class"   : weebdex.WeebdexMangaExtractor,
+    "#pattern" : weebdex.WeebdexChapterExtractor.pattern,
+    "#options" : {"lang": "vi"},
+    "#count"   : 105,
+
+    "artist"       : ["Matsuda Minoru"],
+    "author"       : ["Matsuda Minoru"],
+    "chapter"      : range(1, 105),
+    "chapter_minor": {"", ".5"},
+    "demographic"  : "seinen",
+    "description"  : str,
+    "language"     : "vi",
+    "manga"        : "Ani Datta Mono",
+    "manga_date"   : "dt:2025-10-09 19:02:04",
+    "manga_id"     : "3o0icxno26",
+    "origin"       : "ja",
+    "status"       : "ongoing",
+    "version"      : int,
+    "volume"       : int,
+    "year"         : 2021,
+    "tags"         : [
+        "genre:Drama",
+        "genre:Horror",
+        "genre:Psychological",
+        "genre:Romance",
+        "genre:Thriller",
+        "theme:Ghosts",
+        "theme:Supernatural",
+    ],
+    "relationships": {
+        "groups"  : [{
+            "id"  : "u7kmeka8v6",
+            "name": "BBB Translation (Big Beaming Bluewhale)",
+        }],
+    },
+},
+
+{
+    "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono?group=j0fsj3oem3&order=asc",
+    "#comment" : "query parameters (#8957)",
+    "#class"   : weebdex.WeebdexMangaExtractor,
+    "#range"   : "1-3",
+    "#results" : (
+        "https://weebdex.org/chapter/xv2cm9bj1a",
+        "https://weebdex.org/chapter/etoaxmxgcq",
+        "https://weebdex.org/chapter/24ew0mo562",
+    ),
+
+    "artist"       : ["Matsuda Minoru"],
+    "author"       : ["Matsuda Minoru"],
+    "chapter"      : range(1, 3),
+    "language"     : "en",
+    "manga"        : "Ani Datta Mono",
+    "manga_date"   : "dt:2025-10-09 19:02:04",
+    "manga_id"     : "3o0icxno26",
+    "version"      : 1,
+    "volume"       : 1,
+    "relationships": {
+        "groups"  : [{
+            "id"  : "j0fsj3oem3",
+            "name": "Rainbow D Translations",
+        }],
+    },
 },
 
 )

docs/gallery-dl.conf
@@ -967,7 +967,8 @@
         },
         "weebdex":
         {
-            "data-saver": false
+            "data-saver": false,
+            "lang"      : "en"
         },
         "weibo":
         {

commit 04905ff7a2753f94638e78f55a0a769a64d8ab52

[weebdex] fix 'chapter-reverse' (#9041)
fixes regression introduced in 56168fbc8781e6283483b10e1c03912df185c506

gallery_dl/extractor/weebdex.py
@@ -94,11 +94,10 @@ class WeebdexMangaExtractor(WeebdexBase, MangaExtractor):
 
         params = text.parse_query(qs)
         params.setdefault("limit", 100)
+        params.setdefault("order", "asc")
         if "tlang" not in params:
             params["tlang"] = self.config("lang", "en")
-        if "order" not in params:
-            params["order"] = ("desc" if self.config("chapter-reverse") else
-                               "asc")
+
         url = f"{self.root_api}/manga/{mid}/chapters"
         base = self.root + "/chapter/"
         manga = _manga_info(self, mid)

test/results/weebdex.py
@@ -30,7 +30,7 @@ __tests__ = (
     "width"        : range(800, 2800),
     "height"       : range(800, 2800),
     "lang"         : "en",
-    "manga"        : "Akuyaku Reijou Level 99 ~Watashi wa UraBoss desu ga Maou de wa Arimasen~",
+    "manga"        : "Akuyaku Reijou Level 99: Watashi wa UraBoss desu ga Maou de wa Arimasen",
     "manga_date"   : "dt:2025-10-09 07:32:07",
     "manga_id"     : "raa6dfy3da",
     "origin"       : "ja",
@@ -253,31 +253,44 @@ ___
 },
 
 {
-    "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono?group=j0fsj3oem3&order=asc",
+    "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono?group=j0fsj3oem3&order=desc",
     "#comment" : "query parameters (#8957)",
     "#class"   : weebdex.WeebdexMangaExtractor,
     "#range"   : "1-3",
     "#results" : (
-        "https://weebdex.org/chapter/xv2cm9bj1a",
-        "https://weebdex.org/chapter/etoaxmxgcq",
-        "https://weebdex.org/chapter/24ew0mo562",
+        "https://weebdex.org/chapter/u18zq7aclx",
+        "https://weebdex.org/chapter/2qb8jw9paz",
+        "https://weebdex.org/chapter/aht5ukglyv",
     ),
 
     "artist"       : ["Matsuda Minoru"],
     "author"       : ["Matsuda Minoru"],
-    "chapter"      : range(1, 3),
+    "chapter"      : range(30, 50),
     "language"     : "en",
     "manga"        : "Ani Datta Mono",
     "manga_date"   : "dt:2025-10-09 19:02:04",
     "manga_id"     : "3o0icxno26",
     "version"      : 1,
-    "volume"       : 1,
+    "volume"       : {3, 4, 5},
     "relationships": {
         "groups"  : [{
             "id"  : "j0fsj3oem3",
             "name": "Rainbow D Translations",
         }],
     },
+},
+
+{
+    "#url"     : "https://weebdex.org/title/3o0icxno26/ani-datta-mono?group=j0fsj3oem3",
+    "#comment" : "'chapter-reverse' option (#9041)",
+    "#class"   : weebdex.WeebdexMangaExtractor,
+    "#options" : {"chapter-reverse": True},
+    "#range"   : "1-3",
+    "#results" : (
+        "https://weebdex.org/chapter/xv2cm9bj1a",
+        "https://weebdex.org/chapter/etoaxmxgcq",
+        "https://weebdex.org/chapter/24ew0mo562",
+    ),
 },
 
 )