mikf/gallery-dl

commit 56f27906268a860b610d7e311faf9683876cf4d5

[options] add '--config-type' command-line option
can also be set via 'GDL_CONFIG_TYPE' environment variable
9fd732afe82f98bc1a592a5078909da366b44957

gallery_dl/config.py
@@ -21,48 +21,56 @@ log = logging.getLogger("config")
 
 _config = {}
 _files = []
-_type = os.environ.get("GDL_CONFIG_TYPE")
-
-if not _type or (_type := _type.lower()) == "json":
-    _type = "json"
-    _load = util.json_loads
-elif _type == "yaml":
-    from yaml import safe_load as _load
-elif _type == "toml":
-    try:
-        from tomllib import loads as _load
-    except ImportError:
-        from toml import loads as _load
-else:
-    raise ValueError(f"Unsupported config file type "
-                     f"'{os.environ['GDL_CONFIG_TYPE']}'")
-
-if util.WINDOWS:
-    _default_configs = [
-        r"%APPDATA%\gallery-dl\config." + _type,
-        r"%USERPROFILE%\gallery-dl\config." + _type,
-        r"%USERPROFILE%\gallery-dl.conf",
-    ]
-else:
-    _default_configs = [
-        "/etc/gallery-dl.conf",
-        "${XDG_CONFIG_HOME}/gallery-dl/config." + _type
-        if os.environ.get("XDG_CONFIG_HOME") else
-        "${HOME}/.config/gallery-dl/config." + _type,
-        "${HOME}/.gallery-dl.conf",
-    ]
-
-
-if util.EXECUTABLE:
-    # look for config file in PyInstaller executable directory (#682)
-    _default_configs.append(os.path.join(
-        os.path.dirname(sys.executable),
-        "gallery-dl.conf",
-    ))
+_type = "json"
+_load = util.json_loads
+_default_configs = ()
 
 
 # --------------------------------------------------------------------
 # public interface
+
+
+def default(type=None):
+    global _type
+    global _load
+    global _default_configs
+
+    if not type or (type := type.lower()) == "json":
+        _type = type = "json"
+        _load = util.json_loads
+    elif type == "yaml":
+        _type = "yaml"
+        from yaml import safe_load as _load
+    elif type == "toml":
+        _type = "toml"
+        try:
+            from tomllib import loads as _load
+        except ImportError:
+            from toml import loads as _load
+    else:
+        raise ValueError(f"Unsupported config file type '{type}'")
+
+    if util.WINDOWS:
+        _default_configs = [
+            r"%APPDATA%\gallery-dl\config." + type,
+            r"%USERPROFILE%\gallery-dl\config." + type,
+            r"%USERPROFILE%\gallery-dl.conf",
+        ]
+    else:
+        _default_configs = [
+            "/etc/gallery-dl.conf",
+            "${XDG_CONFIG_HOME}/gallery-dl/config." + type
+            if os.environ.get("XDG_CONFIG_HOME") else
+            "${HOME}/.config/gallery-dl/config." + type,
+            "${HOME}/.gallery-dl.conf",
+        ]
+
+    if util.EXECUTABLE:
+        # look for config file in PyInstaller executable directory (#682)
+        _default_configs.append(os.path.join(
+            os.path.dirname(sys.executable),
+            "gallery-dl.conf",
+        ))
 
 
 def initialize():
@@ -366,3 +374,6 @@ class apply():
                 unset(path, key)
             else:
                 set(path, key, value)
+
+
+default(os.environ.get("GDL_CONFIG_TYPE"))

gallery_dl/__init__.py
@@ -26,6 +26,11 @@ def main():
         log = output.initialize_logging(args.loglevel)
 
         # configuration
+        if args.config_type:
+            try:
+                config.default(args.config_type)
+            except Exception as exc:
+                config.log.error(exc)
         if args.config_load:
             config.load()
         if args.configs_json:

gallery_dl/option.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2017-2025 Mike Fährmann
+# Copyright 2017-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -632,6 +632,12 @@ def build_parser():
         "--config-open",
         dest="config", action="store_const", const="open",
         help="Open configuration file in external application",
+    )
+    configuration.add_argument(
+        "--config-type",
+        dest="config_type", metavar="TYPE",
+        help=("Set filetype of default configuration files "
+              "(json, yaml, toml)"),
     )
     configuration.add_argument(
         "--config-ignore",

commit cc529868112ad11cfd2feb00a241516472fce5bc

[config] fix 'JSONDecodeError' when using non-JSON config types (#9063)
fixes regression introduced in 56f27906268a860b610d7e311faf9683876cf4d5

gallery_dl/config.py
@@ -73,6 +73,9 @@ def default(type=None):
         ))
 
 
+default(os.environ.get("GDL_CONFIG_TYPE"))
+
+
 def initialize():
     paths = list(map(util.expand_path, _default_configs))
 
@@ -211,8 +214,11 @@ def remap_categories():
             opts[new] = opts[old]
 
 
-def load(files=None, strict=False, loads=_load, conf=_config):
-    """Load JSON configuration files"""
+def load(files=None, strict=False, loads=None, conf=_config):
+    """Load configuration files"""
+    if loads is None:
+        loads = _load
+
     for pathfmt in files or _default_configs:
         path = util.expand_path(pathfmt)
         try:
@@ -374,6 +380,3 @@ class apply():
                 unset(path, key)
             else:
                 set(path, key, value)
-
-
-default(os.environ.get("GDL_CONFIG_TYPE"))