71a0250bf4fc644c74049455febfe763f6f06998
Mike Fährmann <mike_faehrmann@web.de>
2026-02-24 14:47:08 +0100

    release version 1.31.7

--- gallery_dl/version.py
+++ gallery_dl/version.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2016-2025 Mike Fährmann
+# Copyright 2016-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-__version__ = "1.31.6"
+__version__ = "1.31.7"
 __variant__ = None
