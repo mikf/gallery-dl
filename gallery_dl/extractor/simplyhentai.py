228405a4987f696d21edbb79f420d439a6ec73bb
Mike Fährmann <mike_faehrmann@web.de>
2026-03-03 19:42:55 +0100

    [simplyhentai] rewrite (#9158)

--- gallery_dl/extractor/simplyhentai.py
+++ gallery_dl/extractor/simplyhentai.py
@@ -1,160 +1,175 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018-2025 Mike Fährmann
+# Copyright 2018-2026 Mike Fährmann
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-"""Extract hentai-manga from https://www.simply-hentai.com/"""
+"""Extractors for https://www.simply-hentai.com/"""
 
-from .common import GalleryExtractor, Extractor, Message
+from .common import Extractor, GalleryExtractor, Message
 from .. import text, util
 
+BASE_PATTERN = r"(?:https?://)?(?:www\.)?simply-hentai\.com"
 
-class SimplyhentaiGalleryExtractor(GalleryExtractor):
-    """Extractor for image galleries from simply-hentai.com"""
+
+class SimplyhentaiExtractor(Extractor):
+    """Base class for simplyhentai extractors"""
     category = "simplyhentai"
-    archive_fmt = "{image_id}"
-    pattern = (r"(?:https?://)?(?!videos\.)([\w-]+\.)?simply-hentai\.com"
-               r"(?!/(?:album|gifs?|images?|series)(?:/|$))"
-               r"((?:/(?!(?:page|all-pages)(?:/|\.|$))[^/?#]+)+)")
-    example = "https://www.simply-hentai.com/TITLE"
+    root = "https://www.simply-hentai.com"
+    root_api = "https://api-v3.simply-hentai.com"
 
-    def __init__(self, match):
-        subdomain, path = match.groups()
-        if subdomain and subdomain not in ("www.", "old."):
-            path = "/" + subdomain.rstrip(".") + path
-        url = "https://old.simply-hentai.com" + path
-        GalleryExtractor.__init__(self, match, url)
+    def items(self):
+        for gallery in self.galleries():
+            gallery["_extractor"] = SimplyhentaiGalleryExtractor
+            series = (s["slug"] if (s := gallery.get("series")) else
+                      "8-original-work")
+            url = f"{self.root}/{series}/{gallery['slug']}"
+            yield Message.Queue, url, gallery
 
-    def _init(self):
-        self.session.headers["Referer"] = self.page_url
+    def request_api(self, endpoint, params=None):
+        url = f"{self.root_api}/v3{endpoint}"
+        return self.request_json(url, params=params, headers={
+            "Referer": self.root + "/",
+            "Origin" : self.root,
+        })
 
-    def metadata(self, page):
-        extr = text.extract_from(page)
-        split = text.split_html
+    def _pagination(self, endpoint, params):
+        params["page"] = text.parse_int(params.get("page"), 1)
 
-        title = extr('<meta property="og:title" content="', '"')
-        image = extr('<meta property="og:image" content="', '"')
-        if not title:
-            raise self.exc.NotFoundError("gallery")
-        data = {
-            "title"     : text.unescape(title),
-            "gallery_id": text.parse_int(image.split("/")[-2]),
-            "parody"    : split(extr('box-title">Series</div>', '</div>')),
-            "language"  : text.remove_html(extr(
-                'box-title">Language</div>', '</div>')) or None,
-            "characters": split(extr('box-title">Characters</div>', '</div>')),
-            "tags"      : split(extr('box-title">Tags</div>', '</div>')),
-            "artist"    : split(extr('box-title">Artists</div>', '</div>')),
-            "date"      : self.parse_datetime(text.remove_html(
-                extr('Uploaded', '</div>')), "%d.%m.%Y"),
-        }
-        data["lang"] = util.language_to_code(data["language"])
+        while True:
+            data = self.request_api(endpoint, params)
+
+            if isinstance(data["data"], dict):
+                yield from data["data"]["albums"]
+            else:
+                yield from data["data"]
+
+            try:
+                if params["page"] >= data["pagination"]["pages"]:
+                    break
+            except Exception:
+                break
+            params["page"] += 1
+
+
+class SimplyhentaiSeriesExtractor(SimplyhentaiExtractor):
+    subcategory = "series"
+    pattern = (BASE_PATTERN + r"/series/([^/?#]+)"
+               r"(?:/tag-([^/?#]+))?(?:/sort-([^/?#]+))?(?:/page-(\d+))?"
+               r"(?:\?([^#]+))?")
+    example = "https://www.simply-hentai.com/series/SLUG"
+
+    def galleries(self):
+        slug, tag, sort, pnum, qs = self.groups
+        params = text.parse_query(qs)
+        if tag is not None:
+            params["tag_slug"] = tag
+        if sort is not None:
+            params["sort"] = sort
+        if pnum is not None:
+            params["page"] = pnum
+        return self._pagination(f"/series/{slug}", params)
+
+
+class SimplyhentaiMangaExtractor(SimplyhentaiExtractor):
+    subcategory = "manga"
+    pattern = (BASE_PATTERN + r"/2-mangas"
+               r"(?:/sort-([^/?#]+))?(?:/page-(\d+))?(?:\?([^#]+))?")
+    example = "https://www.simply-hentai.com/2-mangas"
+
+    def galleries(self):
+        sort, pnum, qs = self.groups
+        params = text.parse_query(qs)
+        if sort is not None:
+            params["sort"] = sort
+        if pnum is not None:
+            params["page"] = pnum
+        return self._pagination("/mangas", params)
+
+
+class SimplyhentaiTagExtractor(SimplyhentaiExtractor):
+    subcategory = "tag"
+    pattern = (BASE_PATTERN +
+               r"/(parody|tag|character|collection|artist|translator)"
+               r"/([^/?#]+)"
+               r"(?:/tag-([^/?#]+))?(?:/sort-([^/?#]+))?(?:/page-(\d+))?"
+               r"(?:\?([^#]+))?")
+    example = "https://www.simply-hentai.com/tag/TAG"
+
+    def galleries(self):
+        type, slug, tag, sort, pnum, qs = self.groups
+        params = text.parse_query(qs)
+        if type == "collection":
+            endpoint = "/collection/" + slug
+        else:
+            endpoint = "/tag/" + slug
+            params["type"] = type
+        if tag is not None:
+            params["tag_slug"] = tag
+        if sort is not None:
+            params["sort"] = sort
+        if pnum is not None:
+            params["page"] = pnum
+        return self._pagination(endpoint, params)
+
+
+class SimplyhentaiLanguageExtractor(SimplyhentaiExtractor):
+    subcategory = "language"
+    pattern = (BASE_PATTERN + r"/language/([^/?#]+)"
+               r"(?:/sort-([^/?#]+))?(?:/page-(\d+))?(?:\?([^#]+))?")
+    example = "https://www.simply-hentai.com/language/LANG"
+
+    def galleries(self):
+        language, sort, pnum, qs = self.groups
+        params = text.parse_query(qs)
+        params["type"] = "language"
+        if sort is not None:
+            params["sort"] = sort
+        if pnum is not None:
+            params["page"] = pnum
+        return self._pagination(f"/tag/{language}", params)
+
+
+class SimplyhentaiGalleryExtractor(GalleryExtractor, SimplyhentaiExtractor):
+    """Extractor for simplyhentai galleries"""
+    archive_fmt = "{id}"
+    pattern = BASE_PATTERN + r"/[^/?#]+/([^/?#]+)"
+    example = "https://www.simply-hentai.com/SERIES/SLUG"
+
+    def metadata(self, _):
+        endpoint = "/manga/" + self.groups[0]
+        data = self.request_api(endpoint)["data"]
+        data["gallery_id"] = data.pop("id")
+        data["series"] = (s := data.get("series")) and s.get("title")
+        data["date"] = self.parse_datetime_iso(data.get("created_at"))
+
+        for key in ("artists", "characters", "parodies",
+                    "tags", "translators"):
+            data[key] = [t["title"] for t in data.get(key) or ()]
+
+        data.pop("related", None)
+        self._images = data.pop("images", ())
+        if data.get("image_count", 32) > 12:
+            self._images = endpoint + "/pages"
+
+        try:
+            language = data["language"]["name"]
+            data["lang"] = util.language_to_code(language)
+            data["language"] = language
+        except Exception:
+            pass
+
+        try:
+            data["cover"] = data.pop("preview")["sizes"]["full"]
+        except Exception:
+            pass
+
         return data
 
     def images(self, _):
-        url = self.page_url + "/all-pages"
-        headers = {"Accept": "application/json"}
-        images = self.request_json(url, headers=headers)
-        return [
-            (
-                urls["full"].replace("/giant_thumb_", "/"),
-                {"image_id": text.parse_int(image_id)},
-            )
-            for image_id, urls in sorted(images.items())
-        ]
-
-
-class SimplyhentaiImageExtractor(Extractor):
-    """Extractor for individual images from simply-hentai.com"""
-    category = "simplyhentai"
-    subcategory = "image"
-    directory_fmt = ("{category}", "{type}s")
-    filename_fmt = "{category}_{token}{title:?_//}.{extension}"
-    archive_fmt = "{token}"
-    pattern = (r"(?:https?://)?(?:www\.)?(simply-hentai\.com"
-               r"/(image|gif)/[^/?#]+)")
-    example = "https://www.simply-hentai.com/image/NAME"
-
-    def __init__(self, match):
-        Extractor.__init__(self, match)
-        self.page_url = "https://old." + match[1]
-        self.type = match[2]
-
-    def items(self):
-        extr = text.extract_from(self.request(self.page_url).text)
-        title = extr('"og:title" content="'      , '"')
-        descr = extr('"og:description" content="', '"')
-        url = extr('&quot;image&quot;:&quot;'  , '&')
-        url = extr("&quot;content&quot;:&quot;", "&") or url
-
-        if tags := text.extr(descr, " tagged with ", " online for free "):
-            tags = tags.split(", ")
-            tags[-1] = tags[-1].partition(" ")[2]
-        else:
-            tags = []
-
-        if url.startswith("//"):
-            url = "https:" + url
-
-        data = text.nameext_from_url(url, {
-            "title": text.unescape(title) if title else "",
-            "tags": tags,
-            "type": self.type,
-        })
-        data["token"] = data["filename"].rpartition("_")[2]
-
-        yield Message.Directory, "", data
-        yield Message.Url, url, data
-
-
-class SimplyhentaiVideoExtractor(Extractor):
-    """Extractor for hentai videos from simply-hentai.com"""
-    category = "simplyhentai"
-    subcategory = "video"
-    directory_fmt = ("{category}", "{type}s")
-    filename_fmt = "{title}{episode:?_//>02}.{extension}"
-    archive_fmt = "{title}_{episode}"
-    pattern = r"(?:https?://)?(videos\.simply-hentai\.com/[^/?#]+)"
-    example = "https://videos.simply-hentai.com/TITLE"
-
-    def __init__(self, match):
-        Extractor.__init__(self, match)
-        self.page_url = "https://" + match[1]
-
-    def items(self):
-        page = self.request(self.page_url).text
-
-        title, pos = text.extract(page, "<title>", "</title>")
-        tags , pos = text.extract(page, ">Tags</div>", "</div>", pos)
-        date , pos = text.extract(page, ">Upload Date</div>", "</div>", pos)
-        title = title.rpartition(" - ")[0]
-
-        if "<video" in page:
-            video_url = text.extract(page, '<source src="', '"', pos)[0]
-            episode = 0
-        else:
-            # video url from myhentai.tv embed
-            pos = page.index('<div class="video-frame-container">', pos)
-            embed_url = text.extract(page, 'src="', '"', pos)[0].replace(
-                "embedplayer.php?link=", "embed.php?name=")
-            embed_page = self.request(embed_url).text
-            video_url = text.extr(embed_page, '"file":"', '"')
-            title, _, episode = title.rpartition(" Episode ")
-
-        if video_url.startswith("//"):
-            video_url = "https:" + video_url
-
-        data = text.nameext_from_url(video_url, {
-            "title": text.unescape(title),
-            "episode": text.parse_int(episode),
-            "tags": text.split_html(tags)[::2],
-            "type": "video",
-            "date": self.parse_datetime(text.remove_html(
-                date), "%B %d, %Y %H:%M"),
-        })
-
-        yield Message.Directory, "", data
-        yield Message.Url, video_url, data
+        imgs = self._images
+        if isinstance(imgs, str):
+            imgs = self.request_api(imgs)["data"]["pages"]
+        return [(img["sizes"]["full"], {"id": img["id"]}) for img in imgs]
