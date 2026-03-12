051271fff3711b6d633872b72116156780047a21
notsafeforgit <96166447+notsafeforgit@users.noreply.github.com>
2026-03-12 01:20:26 -0700

    [ytdl] propagate ControlException and handle ignoreerrors (#9229)
    Modified `gallery_dl/extractor/ytdl.py` to re-raise `self.exc.ControlException`
    before catching generic `Exception` blocks in `items()` and `_process_entries()`.
    This ensures that control flow mechanisms like `RestartExtraction` are not
    accidentally wrapped and treated as standard extraction errors.
    
    Updated `_process_entries()` to respect the `ignoreerrors` parameter when
    handling generic exceptions from `ytdl_instance.extract_info()`. If
    `ignoreerrors` is truthy, it will now continue iteration. Otherwise, it
    raises an `AbortExtraction`.
    
    Added regression tests to `test/test_ytdl.py` for control exception propagation
    and verifying the `ignoreerrors` logic paths.

--- test/test_ytdl.py
+++ test/test_ytdl.py
@@ -12,7 +12,8 @@ import sys
 import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from gallery_dl import ytdl, util, config  # noqa E402
+from gallery_dl import ytdl, util, config, exception  # noqa E402
+from gallery_dl.extractor.ytdl import YoutubeDLExtractor  # noqa E402
 
 
 class Test_CommandlineArguments(unittest.TestCase):
@@ -307,6 +308,68 @@ class Test_CommandlineArguments_YtDlp(Test_CommandlineArguments):
                 "firefox+keyring:profile::container"],
                "cookiesfrombrowser",
                ("firefox", "profile", "KEYRING", "container"))
+
+
+class MockYtdlModule:
+    class utils:
+        class YoutubeDLError(Exception):
+            pass
+
+
+class MockYtdlInstance:
+    def __init__(self, ignoreerrors):
+        self.params = {"ignoreerrors": ignoreerrors}
+
+
+class Test_YoutubeDLExtractor(unittest.TestCase):
+    def setUp(self):
+        class ExtractorMock(YoutubeDLExtractor):
+            def __init__(self):
+                self.exc = exception
+        self.extr = ExtractorMock.__new__(ExtractorMock)
+        self.extr.exc = exception
+
+    def test_process_entries_restart_propagation(self):
+        ytdl_module = MockYtdlModule()
+        ytdl_instance = MockYtdlInstance(ignoreerrors=False)
+
+        def mock_extract_info(url, dl, ie_key):
+            raise exception.RestartExtraction()
+        ytdl_instance.extract_info = mock_extract_info
+
+        entries = [{"_type": "url", "url": "test_url"}]
+        with self.assertRaises(exception.RestartExtraction):
+            list(self.extr._process_entries(
+                ytdl_module, ytdl_instance, entries))
+
+    def test_process_entries_ignoreerrors_true(self):
+        ytdl_module = MockYtdlModule()
+        ytdl_instance = MockYtdlInstance(ignoreerrors=True)
+
+        def mock_extract_info(url, dl, ie_key):
+            raise ytdl_module.utils.YoutubeDLError("Some Error")
+        ytdl_instance.extract_info = mock_extract_info
+
+        entries = [
+            {"_type": "url", "url": "test_url1"},
+            {"_type": "url", "url": "test_url2"}
+        ]
+        results = list(self.extr._process_entries(
+            ytdl_module, ytdl_instance, entries))
+        self.assertEqual(results, [])
+
+    def test_process_entries_ignoreerrors_false(self):
+        ytdl_module = MockYtdlModule()
+        ytdl_instance = MockYtdlInstance(ignoreerrors=False)
+
+        def mock_extract_info(url, dl, ie_key):
+            raise ytdl_module.utils.YoutubeDLError("Some Error")
+        ytdl_instance.extract_info = mock_extract_info
+
+        entries = [{"_type": "url", "url": "test_url"}]
+        with self.assertRaises(exception.AbortExtraction):
+            list(self.extr._process_entries(
+                ytdl_module, ytdl_instance, entries))
 
 
 if __name__ == "__main__":
