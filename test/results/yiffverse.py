710f35c337f2bae66da8cb8d3bf962e1956445e0
Mike Fährmann <mike_faehrmann@web.de>
2026-03-12 18:28:06 +0100

    [yiffverse] recognize 'system' & 'meta' tag types

--- test/results/yiffverse.py
+++ test/results/yiffverse.py
@@ -32,6 +32,7 @@ __tests__ = (
     "tags"        : list,
     "tags_general": list,
     "tags_artist" : ["imanika"],
+    "tags_meta"   : ["6:5"],
     "uploaderId"  : 2,
     "width"       : 950,
     "data"        : {
@@ -41,22 +42,13 @@ __tests__ = (
             "https://d.furaffinity.net/art/imanika/1733430246/1733430246.imanika_dream_girl_ych_slot1_web.jpg",
         ],
     },
-    "uploader"    : {
-        "created"      : "2021-07-04T15:01:03.110916Z",
-        "data"         : None,
-        "displayName"  : "agent.e621-uploader",
-        "emailVerified": False,
-        "id"           : 2,
-        "role"         : 3,
-        "userName"     : "agent.e621-uploader",
-    },
 },
 
 {
     "#url"    : "https://yiffverse.com/post/575680",
     "#comment": "video",
     "#class"  : yiffverse.YiffversePostExtractor,
-    "#results"     : "https://yiffverse.com/posts/575/575680/575680.mov.mp4",
+    "#results"     : "https://furry34com.b-cdn.net/posts/575/575680/575680.mov.mp4",
     "#sha1_content": "8952fc794e58c531b4e3b01cfe9e14b1c59ad9ef",
 },
 
@@ -72,7 +64,7 @@ __tests__ = (
     "#url"  : "https://yiffverse.com/playlist/6842",
     "#class": yiffverse.YiffversePlaylistExtractor,
     "#pattern": r"https://(yiffverse\.com|furry34com\.b-cdn\.net)/posts/\d+/\d+/\d+\.mov(720)?\.mp4",
-    "#count"  : range(30, 50),
+    "#count"  : range(40, 60),
 },
 
 )
