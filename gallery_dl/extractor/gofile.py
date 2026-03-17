56ca2b1454fc0829302d584b093d731030cdc09e
Mike Fährmann <mike_faehrmann@web.de>
2026-03-17 19:30:55 +0100

    [gofile] update 'salt' for WT generation

--- gallery_dl/extractor/gofile.py
+++ gallery_dl/extractor/gofile.py
@@ -90,7 +90,7 @@ class GofileFolderExtractor(Extractor):
                 f"{lang}::"
                 f"{self.api_token}::"
                 f"{int(time.time() / 14400)}::"
-                f"gf2026x")
+                f"f4s58gs6")
         return hashlib.sha256(data.encode()).hexdigest()
 
     def _get_content(self, content_id, password=None):
