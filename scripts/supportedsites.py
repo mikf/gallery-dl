bbe53ebb46a2da14f3593580fc684d29a6613591
Mike Fährmann <mike_faehrmann@web.de>
2026-03-18 11:15:18 +0100

    [wikimedia] support 'mgewiki.moe' (#9258)

--- scripts/supportedsites.py
+++ scripts/supportedsites.py
@@ -121,6 +121,7 @@ CATEGORY_MAP = {
     "mangafreak"     : "MangaFreak",
     "mangareader"    : "MangaReader",
     "mangataro"      : "MangaTaro",
+    "mgewiki"        : "Monster Girl Encyclopedia Wiki",
     "s3ndpics"       : "S3ND",
     "schalenetwork"  : "Schale Network",
     "leakgallery"    : "Leak Gallery",
