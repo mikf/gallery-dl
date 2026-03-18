fb3a4226323eaefedf0d0ee983d43fd9dfd8e4cf
Mike Fährmann <mike_faehrmann@web.de>
2026-03-18 23:09:20 +0100

    [pholder] fix 'Could not locate window.data JSON' (#9264)
    detect and solve JS challenges

--- gallery_dl/extractor/pholder.py
+++ gallery_dl/extractor/pholder.py
@@ -52,10 +52,32 @@ class PholderExtractor(Extractor):
 
         raise exception.AbortExtraction("Could not locate window.data JSON.")
 
+    def _solve_challenge(self, html):
+        extr = text.extract_from(html)
+        ts = text.parse_int(extr(" ts=", ";"))
+        ip = text.parse_int(extr(" ip=", ";"))
+        vl = text.parse_int(extr("^(", ")"))
+
+        # '& 0xFFFFFFFF' to replicate JS behavior
+        n = ts ^ ip ^ vl
+        n ^= (n & 0xFFFFFFFF) >> 16
+        n = (n ^ ((n << 7) & 0xFFFFFFFF)) & 0xFFFFFFFF
+        n ^= (n & 0xFFFFFFFF) >> 3
+        n = (n ^ ((n << 17) & 0xFFFFFFFF)) & 0xFFFFFFFF
+        n ^= (n & 0xFFFFFFFF) >> 11
+
+        ot = util.b36encode((ts ^ ip) & 0xFFFFFFFF)
+        return f"{ot}.{util.b36encode(n)}"
+
     def _posts(self, page_url):
         params = {"page": 1}
         while True:
             html = self.request(page_url, params=params).text
+
+            if len(html) < 4096:
+                value = self._solve_challenge(html)
+                self.cookies.set("_bcc", value, domain=self.root[8:])
+                continue
             window_data = self._parse_window_data(html)
 
             for item in window_data["media"]:
