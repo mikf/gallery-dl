6fd4ed249a0c2af17fe9be5c489a3dd9e35b32bd
Mike Fährmann <mike_faehrmann@web.de>
2026-04-19 16:26:17 +0200

    [scripts/requirements] add --architecture & --platform filters
    and add several shortcut options like '--windows'

--- scripts/requirements.py
+++ scripts/requirements.py
@@ -41,6 +41,10 @@ def package_hashes(pkg, args):
         "parents": [],
     }
 
+    def append(file):
+        files.append(((file["filename"], file["digests"]["sha256"])))
+
+    py3 = None
     wheel = (0, None)
     files = result["files"]
     for u in d["urls"]:
@@ -48,11 +52,20 @@ def package_hashes(pkg, args):
             continue
         if not args.sdist and u["packagetype"] == "sdist":
             continue
-        if not args.musllinux and "-musllinux" in u["filename"]:
-            continue
 
         v = u["python_version"]
-        if v in ("py3", "cp314"):
+        if v == "py3":
+            py3 = u
+            continue
+
+        if args.freethreaded and args.freethreaded(u["filename"]):
+            continue
+        if args.architecture and not args.architecture(u["filename"]):
+            continue
+        if args.platform and not args.platform(u["filename"]):
+            continue
+
+        if v == "cp314":
             wheel = (99, ())
         elif v.startswith("cp3"):
             if f"-{v}t-" in u["filename"]:
@@ -66,11 +79,13 @@ def package_hashes(pkg, args):
         else:
             continue
 
-        files.append(((u["filename"], u["digests"]["sha256"])))
+        append(u)
 
     if not files and wheel[0]:
         for u in wheel[1]:
-            files.append(((u["filename"], u["digests"]["sha256"])))
+            append(u)
+    if not files and py3:
+        append(py3)
 
     for d in i["requires_dist"] or ():
         name = re.sub(r"([\w-]+).+", r"\1", d).lower()
@@ -110,7 +125,7 @@ def collect(pkg, args, level=0, parent=None):
                 extras = info["extras"] if args.Extra else args.extra
             for extra in extras:
                 for dep in info["extras"][extra]:
-                    collect(dep, args, level+1, f"{pkg}[{extra}]")
+                    collect(dep, args, level+1, f"{pkgl}[{extra}]")
     if parent:
         results[pkgl]["parents"].append(parent)
 
@@ -141,6 +156,7 @@ def output(write, args):
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser(args)
+    parser.add_argument("-a", "--architecture", action="append", default=[])
     parser.add_argument("-d", "--dependencies", action="count", default=0)
     parser.add_argument("-D", "--Dependencies",
                         dest="dependencies", action="store_const", const=128)
@@ -148,22 +164,68 @@ def parse_args(args=None):
     parser.add_argument("-E", "--Extra", action="store_true")
     parser.add_argument("-f", "--freethreaded", action="store_true")
     parser.add_argument("-F", "--filenames", action="store_true")
-    parser.add_argument("-m", "--musllinux", action="store_true")
     parser.add_argument("-N", "--no-clobber", default="w",
                         dest="mode", action="store_const", const="x")
-    parser.add_argument("-o", "--output", )
+    parser.add_argument("-o", "--output")
     parser.add_argument("-O", "--Output")
+    parser.add_argument("-p", "--platform", action="append", default=[])
     parser.add_argument("-s", "--sdist", action="store_true")
     parser.add_argument("-x", "--exclude", action="append", default=[])
-    parser.add_argument("PKGS", nargs="*")
 
+    parser.add_argument("--x32", "--x86", action="store_true")
+    parser.add_argument("--x64", "--x86_64", "--x86-64", "--amd64",
+                        action="store_true")
+    parser.add_argument("--arm64", action="store_true")
+
+    parser.add_argument("--windows", action="store_true")
+    parser.add_argument("--linux", action="store_true")
+    parser.add_argument("--manylinux", "--glibc", action="store_true")
+    parser.add_argument("--musllinux", action="store_true")
+    parser.add_argument("--macosx", "--osx", action="store_true")
+
+    parser.add_argument("PKGS", nargs="*")
     args = parser.parse_args()
 
     if args.Output:
-        args.pkgs = (args.Output,)
+        if not args.pkgs:
+            args.pkgs = (args.Output,)
         args.output = util.path("requirements", args.Output.lower())
     else:
         args.pkgs = args.PKGS
+
+    if args.freethreaded:
+        args.freethreaded = False
+    else:
+        args.freethreaded = re.compile(
+            r"p\d+t-").search
+
+    if args.x32:
+        args.architecture.append("win32")
+    if args.x64:
+        args.architecture.append("x86_64")
+        args.architecture.append("amd64")
+    if args.arm64:
+        args.architecture.append("universal2")
+        args.architecture.append("aarch64")
+        args.architecture.append("arm64")
+    if args.architecture:
+        args.architecture = re.compile(
+            fr"_(?:{'|'.join(args.architecture)})\.").search
+
+    if args.windows:
+        args.platform.append("win")
+    if args.linux:
+        args.platform.append("manylinux\\d*")
+        args.platform.append("musllinux")
+    if args.manylinux:
+        args.platform.append("manylinux")
+    if args.musllinux:
+        args.platform.append("musllinux")
+    if args.macosx:
+        args.platform.append("macosx")
+    if args.platform:
+        args.platform = re.compile(
+            fr"-(?:{'|'.join(args.platform)})_").search
 
     return args
 
